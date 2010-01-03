bea772b8f9f49a2119e12c4e7124ca47f0c622f4
blueboy <djwebb@blueyonder.co.uk>
2010-01-03 10:11:08 +0000

    Fix for non-resurrectable bots

--- src/game/PlayerbotAI.cpp
+++ src/game/PlayerbotAI.cpp
@@ -1938,9 +1938,11 @@ void PlayerbotAI::UpdateAI(const uint32 p_time)
         else if( m_botState == BOTSTATE_DEAD )
         {
             // become ghost
-            if( m_bot->GetCorpse() )
+            if( m_bot->GetCorpse() ){
+                //sLog.outDebug( "[PlayerbotAI]: %s already has a corpse...", m_bot->GetName() );
+                SetState( BOTSTATE_DEADRELEASED );
                 return;
-
+            }
             m_bot->SetBotDeathTimer();
             m_bot->BuildPlayerRepop();
             // relocate ghost
