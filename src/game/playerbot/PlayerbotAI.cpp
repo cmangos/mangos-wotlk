cedf2b4e4f820302ba3a18a7d3c7073adb67edf9
chelobaka <chelobaka@rocketmail.com>
2010-08-01 12:00:19 +0400

    Add 'pet toggle' command
    This command toggle autocasting of bot's pet's spell.
    Syntax is similar to the 'cast' command.

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -220,6 +220,70 @@ uint32 PlayerbotAI::getSpellId(const char* args, bool master) const
     return foundSpellId;
 }
 
+
+uint32 PlayerbotAI::getPetSpellId(const char* args) const
+{
+    if (!*args)
+        return 0;
+
+    Pet* pet = m_bot->GetPet();
+    if (!pet)
+        return 0;
+
+    std::string namepart = args;
+    std::wstring wnamepart;
+
+    if (!Utf8toWStr(namepart, wnamepart))
+        return 0;
+
+    // converting string that we try to find to lower case
+    wstrToLower(wnamepart);
+
+    int loc = GetMaster()->GetSession()->GetSessionDbcLocale();
+
+    uint32 foundSpellId = 0;
+    bool foundExactMatch = false;
+    bool foundMatchUsesNoReagents = false;
+
+    for (PetSpellMap::iterator itr = pet->m_spells.begin(); itr != pet->m_spells.end(); ++itr)
+     {
+         uint32 spellId = itr->first;
+
+         if (itr->second.state == PETSPELL_REMOVED || IsPassiveSpell(spellId))
+             continue;
+
+         const SpellEntry* pSpellInfo = sSpellStore.LookupEntry(spellId);
+         if (!pSpellInfo)
+             continue;
+
+         const std::string name = pSpellInfo->SpellName[loc];
+         if (name.empty() || !Utf8FitTo(name, wnamepart))
+             continue;
+
+         bool isExactMatch = (name.length() == wnamepart.length()) ? true : false;
+         bool usesNoReagents = (pSpellInfo->Reagent[0] <= 0) ? true : false;
+
+         // if we already found a spell
+         bool useThisSpell = true;
+         if (foundSpellId > 0)
+         {
+             if (isExactMatch && !foundExactMatch) {}
+             else if (usesNoReagents && !foundMatchUsesNoReagents) {}
+             else if (spellId > foundSpellId) {}
+             else
+                 useThisSpell = false;
+         }
+         if (useThisSpell)
+         {
+             foundSpellId = spellId;
+             foundExactMatch = isExactMatch;
+             foundMatchUsesNoReagents = usesNoReagents;
+         }
+     }
+
+    return foundSpellId;
+}
+
 /*
  * Send a list of equipment that is in bot's inventor that is currently unequipped.
  * This is called when the master is inspecting the bot.
@@ -3211,6 +3275,48 @@ void PlayerbotAI::HandleCommand(const std::string& text, Player& fromPlayer)
         ch.SendSysMessage(posOut.str().c_str());
         SendWhisper("and here's my pet's attack spells:", fromPlayer);
         ch.SendSysMessage(negOut.str().c_str());
+    }
+
+    //handle pet spell toggle
+    else if (text.size() > 11 && text.substr(0, 11) == "pet toggle ")
+    {
+        Pet *pet = m_bot->GetPet();
+        if (!pet)
+        {
+            SendWhisper("I have no pet.", fromPlayer);
+            return;
+        }
+        else
+        {
+            std::string spellStr = text.substr(11);
+            uint32 spellId = (uint32) atol(spellStr.c_str());
+
+            // try and get spell ID by name
+            if (spellId == 0)
+                spellId = getPetSpellId(spellStr.c_str());
+
+            // try link if text NOT (spellid OR spellname)
+            if (spellId == 0)
+                extractSpellId(text, spellId);
+
+            if (spellId != 0 && pet->HasSpell(spellId))
+            {
+                PetSpellMap::iterator itr = pet->m_spells.find(spellId);
+                if (itr != pet->m_spells.end())
+                {
+                    if (itr->second.active == ACT_ENABLED)
+                    {
+                        pet->ToggleAutocast(spellId, false);
+                        if (pet->HasAura(spellId))
+                            pet->RemoveAurasByCasterSpell(spellId,pet->GetGUID());
+                    }
+                    else
+                    {
+                        pet->ToggleAutocast(spellId, true);
+                    }
+                }
+            }
+        }
     }
 
     else if (text == "spells")
