c4d5c2e85b9ccce271bc05547c5a299e402a79b4
blueboy <djwebb@blueyonder.co.uk>
2012-01-21 21:35:07 +0000

    Fix for 'skill learn [LINK]' crash issue

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -7497,7 +7497,12 @@ void PlayerbotAI::_HandleCommandSkill(std::string &text, Player &fromPlayer)
         // check present spell in trainer spell list
         TrainerSpellData const* cSpells = creature->GetTrainerSpells();
         TrainerSpellData const* tSpells = creature->GetTrainerTemplateSpells();
-        if (!cSpells && !tSpells)
+
+        TrainerSpellData const* all_trainer_spells = cSpells;
+        if (!all_trainer_spells)
+           all_trainer_spells = tSpells;
+
+	if (!all_trainer_spells)
         {
             SendWhisper("No spells can be learnt from this trainer", fromPlayer);
             return;
@@ -7521,10 +7526,7 @@ void PlayerbotAI::_HandleCommandSkill(std::string &text, Player &fromPlayer)
                 if (!spellId)
                     break;
 
-                TrainerSpell const* trainer_spell = cSpells->Find(spellId);
-                if (!trainer_spell)
-                    trainer_spell = tSpells->Find(spellId);
-
+                TrainerSpell const* trainer_spell = all_trainer_spells->Find(spellId);
                 if (!trainer_spell || !trainer_spell->learnedSpell)
                     continue;
 
@@ -7583,11 +7585,7 @@ void PlayerbotAI::_HandleCommandSkill(std::string &text, Player &fromPlayer)
         {
             msg << "The spells I can learn and their cost:\r";
 
-            TrainerSpellData const* trainer_spells = cSpells;
-            if (!trainer_spells)
-                trainer_spells = tSpells;
-
-            for (TrainerSpellMap::const_iterator itr =  trainer_spells->spellList.begin(); itr !=  trainer_spells->spellList.end(); ++itr)
+            for (TrainerSpellMap::const_iterator itr =  all_trainer_spells->spellList.begin(); itr !=  all_trainer_spells->spellList.end(); ++itr)
             {
                 TrainerSpell const* tSpell = &itr->second;
 
