9b0d888a95b80ad9d0b132fafafed13d282a9b7e
kid10 <kid_10@gmx.at>
2012-09-13 23:06:41 +0200

    [12163] Improve Initialisation of COMMAND_ATTACK field
    This moves the set command of COMMAND_ATTACK out of CharmInfo::InitPetAction bar, so that this function can be used more genericly for other purposes
    
    Signed-off-by: Schmoozerd <cmangos>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9577,7 +9577,6 @@ void CharmInfo::InitPetActionBar()
 
 void CharmInfo::InitEmptyActionBar()
 {
-    SetActionBar(ACTION_BAR_INDEX_START, COMMAND_ATTACK, ACT_COMMAND);
     for (uint32 x = ACTION_BAR_INDEX_START + 1; x < ACTION_BAR_INDEX_END; ++x)
         SetActionBar(x, 0, ACT_PASSIVE);
 }
@@ -9588,6 +9587,8 @@ void CharmInfo::InitPossessCreateSpells()
 
     if (m_unit->GetTypeId() == TYPEID_PLAYER)               // possessed players don't have spells, keep the action bar empty
         return;
+
+    SetActionBar(ACTION_BAR_INDEX_START, COMMAND_ATTACK, ACT_COMMAND);
 
     for (uint32 x = 0; x < CREATURE_MAX_SPELLS; ++x)
     {
