3493ad73365bcc4929a7294d65faf7e301d8d057
kid10 <kid_10@gmx.at>
2012-10-17 22:44:39 +0200

    [12217] Vehicle spell casting related fixes
    * Despawn Spell summoned vehicles when boarding fails
    * Implement TARGET_CONTROLLED_VEHICLE (based on long-history of vehicle-authors)
    * Improve Spell:CheckPetCast check to only check the sent unit-target if it has any meaning
    
    Signed-off-by: Schmoozerd <schmoozerd@cmangos>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2340,6 +2340,10 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 if (target->GetTypeId() == TYPEID_UNIT && ((Creature*)target)->IsPet() && ((Pet*)target)->getPetType() == MINI_PET)
                     targetUnitMap.push_back(target);
             break;
+        case TARGET_CONTROLLED_VEHICLE:
+            if (m_caster->IsBoarded() && m_caster->GetTransportInfo()->IsOnVehicle())
+                targetUnitMap.push_back((Unit*)m_caster->GetTransportInfo()->GetTransport());
+            break;
         case TARGET_CASTER_COORDINATES:
         {
             // Check original caster is GO - set its coordinates as src cast
@@ -6125,7 +6129,8 @@ SpellCastResult Spell::CheckPetCast(Unit* target)
 
         Unit* _target = m_targets.getUnitTarget();
 
-        if (_target)                                        // for target dead/target not valid
+        // for target dead/target not valid
+        if (_target && m_targets.m_targetMask & TARGET_FLAG_UNIT)
         {
             if (!_target->isTargetableForAttack())
                 return SPELL_FAILED_BAD_TARGETS;            // guessed error
