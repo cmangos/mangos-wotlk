f98a3a06b547fe21b799f436a1a6809af14b78d7
Schmoozerd <schmoozerd@scriptdev2.com>
2012-11-19 18:06:28 +0100

    [12273] Rewrite GetHeight functions to return a reasonable height if possible
    Thanks to Cyberium for problem research, suggestions and discussion

--- src/game/Map.h
+++ src/game/Map.h
@@ -264,7 +264,7 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>
         void PlayDirectSoundToMap(uint32 soundId, uint32 zoneId = 0);
 
         // Dynamic VMaps
-        float GetHeight(uint32 phasemask, float x, float y, float z, bool pCheckVMap = true, float maxSearchDist = DEFAULT_HEIGHT_SEARCH) const;
+        float GetHeight(uint32 phasemask, float x, float y, float z) const;
         bool IsInLineOfSight(float x1, float y1, float z1, float x2, float y2, float z2, uint32 phasemask) const;
         bool GetHitPosition(float srcX, float srcY, float srcZ, float& destX, float& destY, float& destZ, uint32 phasemask, float modifyDist) const;
 
--- src/game/MotionMaster.cpp
+++ src/game/MotionMaster.cpp
@@ -486,7 +486,7 @@ void MotionMaster::MoveJump(float x, float y, float z, float horizontalSpeed, fl
 void MotionMaster::MoveFall()
 {
     // use larger distance for vmap height search than in most other cases
-    float tz = m_owner->GetMap()->GetHeight(m_owner->GetPhaseMask(), m_owner->GetPositionX(), m_owner->GetPositionY(), m_owner->GetPositionZ(), true, MAX_FALL_DISTANCE);
+    float tz = m_owner->GetMap()->GetHeight(m_owner->GetPhaseMask(), m_owner->GetPositionX(), m_owner->GetPositionY(), m_owner->GetPositionZ());
     if (tz <= INVALID_HEIGHT)
     {
         DEBUG_LOG("MotionMaster::MoveFall: unable retrive a proper height at map %u (x: %f, y: %f, z: %f).",
