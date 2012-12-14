f3a24b50a1062cc49c4326fea9e43f960577d01c
Schmoozerd <schmoozerd@scriptdev2.com>
2012-12-14 16:22:04 +0100

    [12309] Fix No-PCH compile. Also make some InstanceData Getter const

--- src/game/InstanceData.cpp
+++ src/game/InstanceData.cpp
@@ -21,7 +21,7 @@
 #include "Map.h"
 #include "Log.h"
 
-void InstanceData::SaveToDB()
+void InstanceData::SaveToDB() const
 {
     // no reason to save BGs/Arenas
     if (instance->IsBattleGroundOrArena())
@@ -39,14 +39,14 @@ void InstanceData::SaveToDB()
         CharacterDatabase.PExecute("UPDATE world SET data = '%s' WHERE map = '%u'", data.c_str(), instance->GetId());
 }
 
-bool InstanceData::CheckAchievementCriteriaMeet(uint32 criteria_id, Player const* /*source*/, Unit const* /*target*/ /*= NULL*/, uint32 /*miscvalue1*/ /*= 0*/)
+bool InstanceData::CheckAchievementCriteriaMeet(uint32 criteria_id, Player const* /*source*/, Unit const* /*target*/ /*= NULL*/, uint32 /*miscvalue1*/ /*= 0*/) const
 {
     sLog.outError("Achievement system call InstanceData::CheckAchievementCriteriaMeet but instance script for map %u not have implementation for achievement criteria %u",
                   instance->GetId(), criteria_id);
     return false;
 }
 
-bool InstanceData::CheckConditionCriteriaMeet(Player const* /*source*/, uint32 instance_condition_id, WorldObject const* /*conditionSource*/, ConditionSource conditionSourceType)
+bool InstanceData::CheckConditionCriteriaMeet(Player const* /*source*/, uint32 instance_condition_id, WorldObject const* /*conditionSource*/, uint32 conditionSourceType) const
 {
     sLog.outError("Condition system call InstanceData::CheckConditionCriteriaMeet but instance script for map %u not have implementation for player condition criteria with internal id %u (called from %u)",
                   instance->GetId(), instance_condition_id, uint32(conditionSourceType));
