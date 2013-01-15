b7dbcdaf75a56e6eb5ee2fa67361bcaa7d8e13c7
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-15 21:57:56 +0100

    [12325] Happy New Year 2013
    Update authorship information and prepare for easier ways next year
    
    Signed-off-by: Schmoozerd <schmoozerd@cmangos>

--- contrib/git_id/git_id.cpp
+++ contrib/git_id/git_id.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ * This file is part of the Continued-MaNGOS Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -640,7 +640,7 @@ bool generate_sql_makefile()
     if(!fout) { pclose(cmd_pipe); return false; }
 
     fprintf(fout,
-        "# Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>\n"
+        "# This file is part of the Continued-MaNGOS Project\n"
         "#\n"
         "# This program is free software; you can redistribute it and/or modify\n"
         "# it under the terms of the GNU General Public License as published by\n"
