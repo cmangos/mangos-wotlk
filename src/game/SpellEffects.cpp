b4b0f4b218b13f63c291bf0c9d854c8287925878
Xfurry <xfurry@scriptdev2.com>
2013-02-26 21:07:57 +0200

    [12381] Fix behavior of spell 42631
    Thanks zkillertheman for pointing
    
    Also fix https://github.com/scriptdev2/scriptdev2/issues/33

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1478,11 +1478,14 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                 }
                 case 42631:                                 // Fire Bomb (explode)
                 {
-                    if (!unitTarget)
+                    if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
                         return;
 
                     unitTarget->RemoveAurasDueToSpell(42629);
                     unitTarget->CastSpell(unitTarget, 42630, true);
+
+                    // despawn the bomb after exploding
+                    ((Creature*)unitTarget)->ForcedDespawn(3000);
                     return;
                 }
                 case 42793:                                 // Burn Body
