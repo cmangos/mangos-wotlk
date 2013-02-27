e9f2099f90dc5eb848eba04d8ebbd96b07883e3f
Maxxie <maxxie@cmangos.net>
2013-02-27 16:16:40 +0100

    [12383] Looks for passive auras that need to be recast. Like talent spell Feral Swiftness (id: 17002 and ranks), once you go inside a building and then outside again.
    Contributor : Ebrithil

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -500,6 +500,11 @@ inline bool IsNeedCastSpellAtFormApply(SpellEntry const* spellInfo, ShapeshiftFo
     // passive spells with SPELL_ATTR_EX2_NOT_NEED_SHAPESHIFT are already active without shapeshift, do no recast!
     return (spellInfo->Stances & (1 << (form - 1)) && !spellInfo->HasAttribute(SPELL_ATTR_EX2_NOT_NEED_SHAPESHIFT));
 }
+ 
+inline bool IsNeedCastSpellAtOutdoor(SpellEntry const* spellInfo)
+{
+    return (spellInfo->HasAttribute(SPELL_ATTR_OUTDOORS_ONLY) && spellInfo->HasAttribute(SPELL_ATTR_PASSIVE));
+}
 
 inline bool NeedsComboPoints(SpellEntry const* spellInfo)
 {
