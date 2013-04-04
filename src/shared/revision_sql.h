808846969809af6a7115a53bdb8ba03b804a2082
begemotius <poprotsky@mail.ru>
2013-04-04 10:36:08 +0300

    Resolve merge conflict

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12339_02_characters_calendar_invites"
- #define REVISION_DB_MANGOS "required_12416_01_mangos_anticheat"
+ #define REVISION_DB_MANGOS "required_12401_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_12416_06_realmd_anticheat"
 #endif // __REVISION_SQL_H__
