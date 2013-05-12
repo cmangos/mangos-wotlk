b15e39f165171088cb98731c8b474603399699b1
zwerg <seaofgreen@gmx.net>
2013-05-13 01:25:35 +0200

    [12488] Add visual vomiting to Spell::EffectInebriate
    Vomit chance is guessed
    
    Patch 3.2.0 (04-Aug-2009): A "Drunken Vomit" feature was implemented. This causes the character to "vomit" upon drinking too much alcohol.
    
    Signed-off-by: stfx <stfx@hotmail.de>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -9318,7 +9318,12 @@ void Spell::EffectInebriate(SpellEffectIndex /*eff_idx*/)
 
     uint8 drunkValue = player->GetDrunkValue() + (uint8)damage;
     if (drunkValue > 100)
+    {
         drunkValue = 100;
+
+        if (roll_chance_i(25))
+            player->CastSpell(player, 67468, false);    // Drunken Vomit
+    }
 
     player->SetDrunkValue(drunkValue, m_CastItem ? m_CastItem->GetEntry() : 0);
 }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12487"
+ #define REVISION_NR "12488"
 #endif // __REVISION_NR_H__
