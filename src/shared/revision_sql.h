659f65eb6996d561138aaf29c7c94bce90f03f1b
Schmoozerd <schmoozerd@scriptdev2.com>
2013-05-20 17:49:13 +0200

    [12505] Add and implement server-side spell 23770

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12487_01_characters_characters"
- #define REVISION_DB_MANGOS "required_12465_01_mangos_spell_template"
+ #define REVISION_DB_MANGOS "required_12505_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
