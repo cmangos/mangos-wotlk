25c1a8b42a6280a07ccbed401e73c2d45908624f
Xfurry <xfurry@scriptdev2.com>
2013-12-17 00:50:06 +0100

    [12578] Implement spell effect 45625

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7707,6 +7707,14 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     unitTarget->CastSpell(unitTarget, 45259, true);
                     return;
                 }
+                case 45625:                                 // Arcane Chains: Character Force Cast
+                {
+                    if (!unitTarget)
+                        return;
+
+                    unitTarget->CastSpell(unitTarget, m_spellInfo->CalculateSimpleValue(eff_idx), true);
+                    return;
+                }
                 case 45668:                                 // Ultra-Advanced Proto-Typical Shortening Blaster
                 {
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
