1af082e44188e76cb83e383957373763aa70a22a
Cyberium <cybermomo@techemail.com>
2013-12-28 23:33:12 +0100

    [12587] Chat system refactoring All chat function now call same method
    I also reworked the packet structure to be more near of original.
    Due to havy of code changed this commit need to be applyed carrefully.
    
    Any feedback is welcome here or on cmangos.net

--- src/game/ChatHandler.cpp
+++ src/game/ChatHandler.cpp
@@ -257,7 +257,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 return;
 
             WorldPacket data;
-            ChatHandler::FillMessageData(&data, this, type, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, ChatMsg(type), msg.c_str(), Language(lang), _player->GetObjectGuid(), _player->GetName());
             group->BroadcastPacket(&data, false, group->GetMemberGroup(GetPlayer()->GetObjectGuid()));
 
             break;
@@ -335,7 +335,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
             }
 
             WorldPacket data;
-            ChatHandler::FillMessageData(&data, this, CHAT_MSG_RAID, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_RAID, msg.c_str(), Language(lang),
+                _player->GetObjectGuid(), _player->GetName(), ObjectGuid(), NULL, NULL, 0, _player->isGMChat(), _player->GetChatTag());
             group->BroadcastPacket(&data, false);
         } break;
         case CHAT_MSG_RAID_LEADER:
@@ -365,7 +366,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
             }
 
             WorldPacket data;
-            ChatHandler::FillMessageData(&data, this, CHAT_MSG_RAID_LEADER, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_RAID_LEADER, msg.c_str(), Language(lang),
+                _player->GetObjectGuid(), _player->GetName(), ObjectGuid(), NULL, NULL, 0, _player->isGMChat(), _player->GetChatTag());
             group->BroadcastPacket(&data, false);
         } break;
 
@@ -387,7 +389,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
 
             WorldPacket data;
             // in battleground, raid warning is sent only to players in battleground - code is ok
-            ChatHandler::FillMessageData(&data, this, CHAT_MSG_RAID_WARNING, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_RAID_WARNING, msg.c_str(), Language(lang),
+                _player->GetObjectGuid(),_player->GetName(), ObjectGuid(), NULL, NULL, 0, _player->isGMChat(), _player->GetChatTag());
             group->BroadcastPacket(&data, false);
         } break;
 
@@ -408,7 +411,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 return;
 
             WorldPacket data;
-            ChatHandler::FillMessageData(&data, this, CHAT_MSG_BATTLEGROUND, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_BATTLEGROUND, msg.c_str(), Language(lang),
+                _player->GetObjectGuid(), _player->GetName(), ObjectGuid(), NULL, NULL, 0, _player->isGMChat(), _player->GetChatTag());
             group->BroadcastPacket(&data, false);
         } break;
 
@@ -429,7 +433,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 return;
 
             WorldPacket data;
-            ChatHandler::FillMessageData(&data, this, CHAT_MSG_BATTLEGROUND_LEADER, lang, msg.c_str());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_BATTLEGROUND_LEADER, msg.c_str(), Language(lang),
+                _player->GetObjectGuid(),_player->GetName(), ObjectGuid(), NULL, NULL, 0, _player->isGMChat(), _player->GetChatTag());
             group->BroadcastPacket(&data, false);
         } break;
 
@@ -619,7 +624,7 @@ void WorldSession::HandleChatIgnoredOpcode(WorldPacket& recv_data)
         return;
 
     WorldPacket data;
-    ChatHandler::FillMessageData(&data, this, CHAT_MSG_IGNORED, LANG_UNIVERSAL, NULL, GetPlayer()->GetObjectGuid(), GetPlayer()->GetName(), NULL);
+    ChatHandler::BuildChatPacket(data, CHAT_MSG_IGNORED, _player->GetName(), LANG_UNIVERSAL, _player->GetObjectGuid());
     player->GetSession()->SendPacket(&data);
 }
 
--- src/game/Player.h
+++ src/game/Player.h
@@ -780,6 +780,7 @@ enum PlayerChatTag
     CHAT_TAG_COM                = 0x08,                     // Commentator
     CHAT_TAG_DEV                = 0x10,                     // Developer
 };
+typedef uint32 ChatTagFlags;
 
 enum PlayedTimeIndex
 {
@@ -1055,7 +1056,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         void ToggleDND();
         bool isAFK() const { return HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK); }
         bool isDND() const { return HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_DND); }
-        uint8 GetChatTag() const;
+        ChatTagFlags GetChatTag() const;
         std::string autoReplyMsg;
 
         uint32 GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 newfacialhair, uint32 newskintone);
@@ -1129,7 +1130,6 @@ class MANGOS_DLL_SPEC Player : public Unit
         void Yell(const std::string& text, const uint32 language);
         void TextEmote(const std::string& text);
         void Whisper(const std::string& text, const uint32 language, ObjectGuid receiver);
-        void BuildPlayerChat(WorldPacket* data, uint8 msgtype, const std::string& text, uint32 language) const;
 
         /*********************************************************/
         /***                    STORAGE SYSTEM                 ***/
