cc137f65b8af7b451e6af03924a1ae28c6d5fd05
Patman64 <patman256@yahoo.com>
2014-03-24 20:29:33 +0100

    [12612] This is used to prevent creatures from pulling too many nearby monsters, for example during certain scripted events.
    A good example is Mr. Smite in the Deadmines. There are two guards at the
    bottom of his bridge which trigger aggro for him, but if he is missing
    this flag he will also pull nearby mobs on the ship, which is not expected
    behavior.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1768,6 +1768,10 @@ void Creature::CallAssistance()
     if (!m_AlreadyCallAssistance && getVictim() && !isCharmed())
     {
         SetNoCallAssistance(true);
+
+        if (GetCreatureInfo()->ExtraFlags & CREATURE_FLAG_EXTRA_NO_CALL_ASSIST)
+            return;
+
         AI()->SendAIEventAround(AI_EVENT_CALL_ASSISTANCE, getVictim(), sWorld.getConfig(CONFIG_UINT32_CREATURE_FAMILY_ASSISTANCE_DELAY), sWorld.getConfig(CONFIG_FLOAT_CREATURE_FAMILY_ASSISTANCE_RADIUS));
     }
 }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12611"
+ #define REVISION_NR "12612"
 #endif // __REVISION_NR_H__
