8e92a6a6eef0609dd6ea4bb4a080349e2e206cee
LilleCarl <lillecarl@gmail.com>
2014-04-20 14:26:49 +0200

    [12642] Implement SMSG_INVALIDATE_PLAYER
    Inspired by TC and R2
    
    close cmangos\issues#50

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -989,6 +989,8 @@ void WorldSession::HandleChangePlayerNameOpcodeCallBack(QueryResult* result, uin
     data << guid;
     data << newname;
     session->SendPacket(&data);
+
+    sWorld.InvalidatePlayerDataToAllClient(guid);
 }
 
 void WorldSession::HandleSetPlayerDeclinedNamesOpcode(WorldPacket& recv_data)
@@ -1244,6 +1246,8 @@ void WorldSession::HandleCharCustomizeOpcode(WorldPacket& recv_data)
     data << uint8(hairColor);
     data << uint8(facialHair);
     SendPacket(&data);
+
+    sWorld.InvalidatePlayerDataToAllClient(guid);
 }
 
 void WorldSession::HandleEquipmentSetSaveOpcode(WorldPacket& recv_data)
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12641"
+ #define REVISION_NR "12642"
 #endif // __REVISION_NR_H__
