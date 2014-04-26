8d3737537bf8757df34e04cafbaa339d367e01d6
Dragonfly <kid_10@gmx.at>
2014-04-26 12:25:59 +0200

    [12647] Let vehicle actionbar initialization independent of SEAT_FLAG_USABLE
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Vehicle.cpp
+++ src/game/Vehicle.cpp
@@ -533,7 +533,7 @@ void VehicleInfo::ApplySeatMods(Unit* passenger, uint32 seatFlags)
             ((Creature*)pVehicle)->AIM_Initialize();
         }
 
-        if (seatFlags & (SEAT_FLAG_USABLE | SEAT_FLAG_CAN_CAST))
+        if (seatFlags & SEAT_FLAG_CAN_CAST)
         {
             CharmInfo* charmInfo = pVehicle->InitCharmInfo(pVehicle);
             charmInfo->InitVehicleCreateSpells();
@@ -587,7 +587,7 @@ void VehicleInfo::RemoveSeatMods(Unit* passenger, uint32 seatFlags)
             ((Creature*)pVehicle)->AIM_Initialize();
         }
 
-        if (seatFlags & (SEAT_FLAG_USABLE | SEAT_FLAG_CAN_CAST))
+        if (seatFlags & SEAT_FLAG_CAN_CAST)
             pPlayer->RemovePetActionBar();
     }
     else if (passenger->GetTypeId() == TYPEID_UNIT)
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12646"
+ #define REVISION_NR "12647"
 #endif // __REVISION_NR_H__
