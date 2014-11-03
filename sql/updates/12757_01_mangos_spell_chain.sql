88b2aeb8abc1b82b87a785366ee84ae73cb5c887
y1x2 <e-fowl@gmx.de>
2014-11-03 22:17:25 +0100

    [12757] Add Shaman/Warrior flurry spell chains
    Fixes flurry procs related to spell_proc_event table. Closes #392
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ sql/updates/12757_01_mangos_spell_chain.sql
@@ -0,0 +1,16 @@
+ALTER TABLE db_version CHANGE COLUMN required_12748_01_mangos_spell_template required_12757_01_spell_chain bit;
+
+DELETE FROM spell_chain WHERE spell_id IN (12319, 12971, 12972, 12973, 12974, 16256, 16281, 16282, 16283, 16284);
+INSERT INTO spell_chain VALUES
+/* Flurry triggered, Warrior */
+(12319,0,12319,1,0),
+(12971,12319,12319,2,0),
+(12972,12971,12319,3,0),
+(12973,12972,12319,4,0),
+(12974,12973,12319,5,0),
+/* Flurry triggered, Shaman */
+(16256,0,16256,1,0),
+(16281,16256,16256,2,0),
+(16282,16281,16256,3,0),
+(16283,16282,16256,4,0),
+(16284,16283,16256,5,0);
