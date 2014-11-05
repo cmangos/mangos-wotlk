39c853b4ac8bf13331faeb91d9e5cf8432148071
ShinDarth <borzifrancesco@gmail.com>
2014-11-05 09:32:09 +0100

    Fit with PvPstats

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -613,8 +613,8 @@ enum Team
 
 enum PvpTeamIndex
 {
-    TEAM_INDEX_ALLIANCE = 0,
-    TEAM_INDEX_HORDE    = 1,
+    TEAM_INDEX_HORDE    = 0,
+    TEAM_INDEX_ALLIANCE = 1,
     TEAM_INDEX_NEUTRAL  = 2,
 };
 
