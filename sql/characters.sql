cmangos/mangos-wotlk

commit 9b7bb67c0eeabfe3e8146e6762231d7869e47f33

[12756] Implement Battleground scores storage system
Close #138

Signed-off-by: Xfurry <xfurry@scriptdev2.com>

sql/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_12562_01_characters_various_tables` bit(1) DEFAULT NULL
+  `required_12756_01_characters_pvpstats` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --
@@ -1724,6 +1724,60 @@ CREATE TABLE `petition_sign` (
 LOCK TABLES `petition_sign` WRITE;
 /*!40000 ALTER TABLE `petition_sign` DISABLE KEYS */;
 /*!40000 ALTER TABLE `petition_sign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pvpstats_battlegrounds`
+--
+
+DROP TABLE IF EXISTS `pvpstats_battlegrounds`;
+CREATE TABLE `pvpstats_battlegrounds` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `winner_team` TINYINT NOT NULL,
+  `bracket_id` TINYINT UNSIGNED NOT NULL,
+  `type` TINYINT UNSIGNED NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `pvpstats_battlegrounds`
+--
+
+LOCK TABLES `petition_sign` WRITE;
+/*!40000 ALTER TABLE `pvpstats_battlegrounds` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pvpstats_battlegrounds` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pvpstats_players`
+--
+
+DROP TABLE IF EXISTS `pvpstats_players`;
+CREATE TABLE `pvpstats_players` (
+  `battleground_id` BIGINT UNSIGNED NOT NULL,
+  `player_guid` INT UNSIGNED NOT NULL,
+  `score_killing_blows` MEDIUMINT UNSIGNED NOT NULL,
+  `score_deaths` MEDIUMINT UNSIGNED NOT NULL,
+  `score_honorable_kills` MEDIUMINT UNSIGNED NOT NULL,
+  `score_bonus_honor` MEDIUMINT UNSIGNED NOT NULL,
+  `score_damage_done` MEDIUMINT UNSIGNED NOT NULL,
+  `score_healing_done` MEDIUMINT UNSIGNED NOT NULL,
+  `attr_1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_3` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_4` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_5` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`battleground_id`, `playerguid`)
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `pvpstats_players`
+--
+
+LOCK TABLES `pvpstats_players` WRITE;
+/*!40000 ALTER TABLE `pvpstats_players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pvpstats_players` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --

src/game/BattleGround/BattleGround.cpp
@@ -629,17 +629,44 @@ void BattleGround::EndBattleGround(Team winner)
     WorldPacket data;
     int32 winmsg_id = 0;
 
+    uint32 winner_team = 0;
+    uint64 battleground_id = 1;
+
     if (winner == ALLIANCE)
     {
         winmsg_id = isBattleGround() ? LANG_BG_A_WINS : LANG_ARENA_GOLD_WINS;
+        winner_team = BG_TEAM_ALLIANCE;
 
         PlaySoundToAll(SOUND_ALLIANCE_WINS);                // alliance wins sound
     }
     else if (winner == HORDE)
     {
         winmsg_id = isBattleGround() ? LANG_BG_H_WINS : LANG_ARENA_GREEN_WINS;
+        winner_team = BG_TEAM_HORDE;
 
         PlaySoundToAll(SOUND_HORDE_WINS);                   // horde wins sound
+    }
+
+    // store battleground scores
+    if (isBattleGround() && sWorld.getConfig(CONFIG_BOOL_BATTLEGROUND_SCORE_STATISTICS))
+    {
+        static SqlStatementID insPvPstatsBattleground;
+        QueryResult * result;
+
+        SqlStatement stmt = CharacterDatabase.CreateStatement(insPvPstatsBattleground, "INSERT INTO pvpstats_battlegrounds (id, winner_team, bracket_id, type, date) VALUES (?, ?, ?, ?, NOW())");
+
+        uint8 battleground_bracket = GetMinLevel() / 10;
+        uint8 battleground_type = (uint8)GetTypeID();
+
+        // query next id
+        result = CharacterDatabase.Query("SELECT MAX(id) FROM pvpstats_battlegrounds");
+        if (result)
+        {
+            Field* fields = result->Fetch();
+            battleground_id = fields[0].GetUInt64() + 1;
+        }
+
+        stmt.PExecute(battleground_id, winner_team, battleground_bracket, battleground_type);
     }
 
     SetWinner(winner);
@@ -738,6 +765,30 @@ void BattleGround::EndBattleGround(Team winner)
                 // Arena lost => reset the win_rated_arena having the "no_loose" condition
                 plr->GetAchievementMgr().ResetAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, ACHIEVEMENT_CRITERIA_CONDITION_NO_LOOSE);
             }
+        }
+
+        // store battleground score statistics for each player
+        if (isBattleGround() && sWorld.getConfig(CONFIG_BOOL_BATTLEGROUND_SCORE_STATISTICS))
+        {
+            static SqlStatementID insPvPstatsPlayer;
+            BattleGroundScoreMap::iterator score = m_PlayerScores.find(itr->first);
+            SqlStatement stmt = CharacterDatabase.CreateStatement(insPvPstatsPlayer, "INSERT INTO pvpstats_players (battleground_id, player_guid, score_killing_blows, score_deaths, score_honorable_kills, score_bonus_honor, score_damage_done, score_healing_done, attr_1, attr_2, attr_3, attr_4, attr_5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            stmt.addUInt32(battleground_id);
+            stmt.addUInt32(plr->GetGUIDLow());
+            stmt.addUInt32(score->second->GetKillingBlows());
+            stmt.addUInt32(score->second->GetDeaths());
+            stmt.addUInt32(score->second->GetHonorableKills());
+            stmt.addUInt32(score->second->GetBonusHonor());
+            stmt.addUInt32(score->second->GetDamageDone());
+            stmt.addUInt32(score->second->GetHealingDone());
+            stmt.addUInt32(score->second->GetAttr1());
+            stmt.addUInt32(score->second->GetAttr2());
+            stmt.addUInt32(score->second->GetAttr3());
+            stmt.addUInt32(score->second->GetAttr4());
+            stmt.addUInt32(score->second->GetAttr5());
+
+            stmt.Execute();
         }
 
         if (team == winner)

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12755"
+ #define REVISION_NR "12756"
 #endif // __REVISION_NR_H__

src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_12562_01_characters_various_tables"
+ #define REVISION_DB_CHARACTERS "required_12756_01_characters_pvpstats"
  #define REVISION_DB_MANGOS "required_12748_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

sql/updates/12756_01_characters_pvpstats.sql
@@ -1,0 +1,29 @@
+ALTER TABLE character_db_version CHANGE COLUMN required_12562_01_characters_various_tables required_12756_01_characters_pvpstats bit;
+
+DROP TABLE IF EXISTS `pvpstats_battlegrounds`;
+CREATE TABLE `pvpstats_battlegrounds` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `winner_team` TINYINT NOT NULL,
+  `bracket_id` TINYINT UNSIGNED NOT NULL,
+  `type` TINYINT UNSIGNED NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `pvpstats_players`;
+CREATE TABLE `pvpstats_players` (
+  `battleground_id` BIGINT UNSIGNED NOT NULL,
+  `player_guid` INT UNSIGNED NOT NULL,
+  `score_killing_blows` MEDIUMINT UNSIGNED NOT NULL,
+  `score_deaths` MEDIUMINT UNSIGNED NOT NULL,
+  `score_honorable_kills` MEDIUMINT UNSIGNED NOT NULL,
+  `score_bonus_honor` MEDIUMINT UNSIGNED NOT NULL,
+  `score_damage_done` MEDIUMINT UNSIGNED NOT NULL,
+  `score_healing_done` MEDIUMINT UNSIGNED NOT NULL,
+  `attr_1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_3` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_4` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_5` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`battleground_id`, `playerguid`)
+) ENGINE=InnoDB;

src/game/BattleGround/BattleGround.h
@@ -255,6 +255,19 @@ class BattleGroundScore
         {}
         virtual ~BattleGroundScore() {}                     // virtual destructor is used when deleting score from scores map
 
+        uint32 GetKillingBlows() const      { return KillingBlows; }
+        uint32 GetDeaths() const            { return Deaths; }
+        uint32 GetHonorableKills() const    { return HonorableKills; }
+        uint32 GetBonusHonor() const        { return BonusHonor; }
+        uint32 GetDamageDone() const        { return DamageDone; }
+        uint32 GetHealingDone() const       { return HealingDone; }
+
+        virtual uint32 GetAttr1() const     { return 0; }
+        virtual uint32 GetAttr2() const     { return 0; }
+        virtual uint32 GetAttr3() const     { return 0; }
+        virtual uint32 GetAttr4() const     { return 0; }
+        virtual uint32 GetAttr5() const     { return 0; }
+
         uint32 KillingBlows;
         uint32 Deaths;
         uint32 HonorableKills;

src/game/BattleGround/BattleGroundAB.h
@@ -147,6 +147,10 @@ class BattleGroundABScore : public BattleGroundScore
     public:
         BattleGroundABScore(): BasesAssaulted(0), BasesDefended(0) {};
         virtual ~BattleGroundABScore() {};
+
+        uint32 GetAttr1() const { return BasesAssaulted; }
+        uint32 GetAttr2() const { return BasesDefended; }
+
         uint32 BasesAssaulted;
         uint32 BasesDefended;
 };

src/game/BattleGround/BattleGroundAV.h
@@ -306,6 +306,13 @@ class BattleGroundAVScore : public BattleGroundScore
     public:
         BattleGroundAVScore() : GraveyardsAssaulted(0), GraveyardsDefended(0), TowersAssaulted(0), TowersDefended(0), SecondaryObjectives(0) {};
         virtual ~BattleGroundAVScore() {};
+
+        uint32 GetAttr1() const { return GraveyardsAssaulted; }
+        uint32 GetAttr2() const { return GraveyardsDefended; }
+        uint32 GetAttr3() const { return TowersAssaulted; }
+        uint32 GetAttr4() const { return TowersDefended; }
+        uint32 GetAttr5() const { return SecondaryObjectives; }
+
         uint32 GraveyardsAssaulted;
         uint32 GraveyardsDefended;
         uint32 TowersAssaulted;

commit dbb69dc759f6383488ba321aa9e6c4ff91232053

[12763] Fix a little memleak. Thank to @kid10. Also fix sql error introduced in 9b7bb67c0eeabfe3e8146e6762231d7869e47f33

sql/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_12756_01_characters_pvpstats` bit(1) DEFAULT NULL
+  `required_12763_00_characters_pvpstats` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --

sql/updates/12763_00_characters_pvpstats.sql
@@ -1,0 +1,29 @@
+ALTER TABLE character_db_version CHANGE COLUMN required_12756_01_characters_pvpstats required_12763_00_characters_pvpstats bit;
+
+DROP TABLE IF EXISTS `pvpstats_battlegrounds`;
+CREATE TABLE `pvpstats_battlegrounds` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `winner_team` TINYINT NOT NULL,
+  `bracket_id` TINYINT UNSIGNED NOT NULL,
+  `type` TINYINT UNSIGNED NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `pvpstats_players`;
+CREATE TABLE `pvpstats_players` (
+  `battleground_id` BIGINT UNSIGNED NOT NULL,
+  `player_guid` INT UNSIGNED NOT NULL,
+  `score_killing_blows` MEDIUMINT UNSIGNED NOT NULL,
+  `score_deaths` MEDIUMINT UNSIGNED NOT NULL,
+  `score_honorable_kills` MEDIUMINT UNSIGNED NOT NULL,
+  `score_bonus_honor` MEDIUMINT UNSIGNED NOT NULL,
+  `score_damage_done` MEDIUMINT UNSIGNED NOT NULL,
+  `score_healing_done` MEDIUMINT UNSIGNED NOT NULL,
+  `attr_1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_3` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_4` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_5` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`battleground_id`, `player_guid`)
+) ENGINE=InnoDB;

src/game/BattleGround/BattleGround.cpp
@@ -664,6 +664,7 @@ void BattleGround::EndBattleGround(Team winner)
         {
             Field* fields = result->Fetch();
             battleground_id = fields[0].GetUInt64() + 1;
+            delete result;
         }
 
         stmt.PExecute(battleground_id, winner_team, battleground_bracket, battleground_type);

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12762"
+ #define REVISION_NR "12763"
 #endif // __REVISION_NR_H__

src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_12756_01_characters_pvpstats"
+ #define REVISION_DB_CHARACTERS "required_12763_00_characters_pvpstats"
  #define REVISION_DB_MANGOS "required_12759_01_mangos_spell_chain"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__