c2c4e4f225c32c1aa0bbe87db76cc18553435ae4
Cyberium <cybermomo@techemail.com>
2014-12-17 11:13:23 +0100

    remove duplicate send error message

--- src/game/LootHandler.cpp
+++ src/game/LootHandler.cpp
@@ -440,12 +440,11 @@ void WorldSession::HandleLootMasterGiveOpcode(WorldPacket& recv_data)
 
     InventoryResult msg = pLoot->SendItem(target_playerguid, slotid);
 
+    // Don't have to use LootItemInSlot because i doubt quest item may be distributable by the master of the loot
     LootItem& item = pLoot->items[slotid];
 
     if (msg != EQUIP_ERR_OK)
     {
-        target->SendEquipError(msg, nullptr, nullptr, item.itemid);
-
         // send duplicate of error massage to master looter
         _player->SendEquipError(msg, nullptr, nullptr, item.itemid);
         return;
