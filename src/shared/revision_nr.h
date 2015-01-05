fdc35fb9b82272c221d3d01d32a6afbdbe0193cc
Schmoozerd <schmoozerd@cmangos.net>
2015-01-05 17:57:30 +0100

    [12816] Rewrite Waypoint related commands
    Toggle the commands .wp add .wp modify and .wp show to be able to work with waypoints from creature_movement_template
    
    Technical changes:
    * Move Path Selection to WaypointManager
    * Set visual waypoints to be summoned npcs

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12815"
+ #define REVISION_NR "12816"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12763_00_characters_pvpstats"
- #define REVISION_DB_MANGOS "required_12759_01_mangos_spell_chain"
+ #define REVISION_DB_MANGOS "required_12816_02_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
