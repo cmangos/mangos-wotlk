e8638d5cefc95c7b96c735a6a3678ca15103319a
mityada <mityada@gmail.com>
2015-03-01 18:55:00 +0500

    Store cast item GUID in Spell to prevent crash when item is deleted
    Signed-off-by: sidsukana <pgsilent@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2995,6 +2995,9 @@ void Spell::SpellStart(SpellCastTargets const* targets, Aura* triggeredByAura)
     m_spellState = SPELL_STATE_STARTING;
     m_targets = *targets;
 
+    if (m_CastItem)
+        m_CastItemGuid = m_CastItem->GetObjectGuid();
+
     m_castPositionX = m_caster->GetPositionX();
     m_castPositionY = m_caster->GetPositionY();
     m_castPositionZ = m_caster->GetPositionZ();
@@ -3588,6 +3591,12 @@ void Spell::update(uint32 difftime)
     UpdatePointers();
 
     if (m_targets.getUnitTargetGuid() && !m_targets.getUnitTarget())
+    {
+        cancel();
+        return;
+    }
+
+    if (m_CastItemGuid && !m_CastItem)
     {
         cancel();
         return;
@@ -4642,6 +4651,7 @@ void Spell::TakeReagents()
                 }
 
                 m_CastItem = nullptr;
+                m_CastItemGuid.Clear();
             }
         }
 
@@ -7115,6 +7125,11 @@ void Spell::UpdatePointers()
     UpdateOriginalCasterPointer();
 
     m_targets.Update(m_caster);
+
+    if (m_caster->GetTypeId() == TYPEID_PLAYER)
+        m_CastItem = ((Player *)m_caster)->GetItemByGuid(m_CastItemGuid);
+    else
+        m_CastItem = nullptr;
 }
 
 bool Spell::CheckTargetCreatureType(Unit* target) const
@@ -7666,6 +7681,7 @@ void Spell::ClearCastItem()
         m_targets.setItemTarget(nullptr);
 
     m_CastItem = nullptr;
+    m_CastItemGuid.Clear();
 }
 
 bool Spell::HasGlobalCooldown()
--- src/game/Spell.h
+++ src/game/Spell.h
@@ -434,6 +434,8 @@ class Spell
         SpellEntry const* m_spellInfo;
         SpellEntry const* m_triggeredBySpellInfo;
         int32 m_currentBasePoints[MAX_EFFECT_INDEX];        // cache SpellEntry::CalculateSimpleValue and use for set custom base points
+
+        ObjectGuid m_CastItemGuid;
         Item* m_CastItem;
         uint8 m_cast_count;
         uint32 m_glyphIndex;
