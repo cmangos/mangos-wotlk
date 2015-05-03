24243bf17e2402e185494cc6ffdd4b9ef22803e1
Xfurry <xfurry@scriptdev2.com>
2015-05-03 02:44:56 +0200

    [12872] Make use of attribute SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY
    This will allow all spell that have this attribute to be casted on targets that are immune to certain types of damage or spells.
    This involves mostly spells that are part of a script or event

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3254,7 +3254,7 @@ SpellMissInfo Unit::SpellHitResult(Unit* pVictim, SpellEntry const* spell, bool 
         return SPELL_MISS_EVADE;
 
     // Check for immune
-    if (pVictim->IsImmuneToSpell(spell, this == pVictim))
+    if (pVictim->IsImmuneToSpell(spell, this == pVictim) && !spell->HasAttribute(SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY))
         return SPELL_MISS_IMMUNE;
 
     // All positive spells can`t miss
@@ -3263,7 +3263,7 @@ SpellMissInfo Unit::SpellHitResult(Unit* pVictim, SpellEntry const* spell, bool 
         return SPELL_MISS_NONE;
 
     // Check for immune
-    if (pVictim->IsImmunedToDamage(GetSpellSchoolMask(spell)))
+    if (pVictim->IsImmunedToDamage(GetSpellSchoolMask(spell)) && !spell->HasAttribute(SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY))
         return SPELL_MISS_IMMUNE;
 
     // Try victim reflect spell
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12871"
+ #define REVISION_NR "12872"
 #endif // __REVISION_NR_H__
