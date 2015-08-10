3aabee37d07d71f5d78c72f9dbeb29da73c55b83
Xfurry <xfurry@scriptdev2.com>
2015-08-10 23:44:44 +0200

    [12900] Fix white space

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1227,7 +1227,7 @@ void GameObject::Use(Unit* user)
             if (!scriptReturnValue)
                 GetMap()->ScriptsStart(sGameObjectScripts, GetGUIDLow(), spellCaster, this);
             else
-               return;
+                return;
 
             // cast this spell later if provided
             spellId = info->goober.spellId;
