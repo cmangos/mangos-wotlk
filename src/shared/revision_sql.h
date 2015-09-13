6e60dfd61d8e5688e711e8fc144338c5a971ce17
ShinDarth <borzifrancesco@gmail.com>
2015-09-13 15:16:01 +0200

    [12937] Fix PvPstats table to fit with its web app
    Closes cmangos/mangos-wotlk#162
    
    [ci skip]
    
    Signed-off-by: stfx <stfx@hotmail.de>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_12931_03_characters_guild_member"
- #define REVISION_DB_MANGOS "required_12936_01_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
+ #define REVISION_DB_CHARACTERS "required_12937_01_characters_pvpstats"
+ #define REVISION_DB_MANGOS "required_12936_01_mangos_mangos_string"
 #endif // __REVISION_SQL_H__
