3a5dc98de6acc57141f2580df4b8325c91a5858b
stfx <stfx@hotmail.de>
2015-09-18 10:57:12 +0200

    Fix git_id sql update format issue

--- sql/base/characters.sql
+++ sql/base/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_characters 12937_01_characters_pvpstats` bit(1) DEFAULT NULL
+  `required_12937_01_characters_pvpstats` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
- #define REVISION_DB_CHARACTERS "required_characters 12937_01_characters_pvpstats"
+ #define REVISION_DB_CHARACTERS "required_12937_01_characters_pvpstats"
  #define REVISION_DB_MANGOS "required_12938_01_mangos_scriptdev2_tables"
 #endif // __REVISION_SQL_H__
