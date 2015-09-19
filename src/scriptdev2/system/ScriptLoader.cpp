b9ee5f843747cffe7db3011f80f51161ec20a685
Xfurry <xfurry@scriptdev2.com>
2015-09-19 23:37:39 +0200

    Add placeholder for Mana Tombs
    Will contain script for quest 10218

--- src/scriptdev2/system/ScriptLoader.cpp
+++ src/scriptdev2/system/ScriptLoader.cpp
@@ -390,6 +390,7 @@ extern void AddSC_boss_exarch_maladaar();                   // auchindoun, auche
 extern void AddSC_boss_shirrak();
 extern void AddSC_boss_nexusprince_shaffar();               // auchindoun, mana_tombs
 extern void AddSC_boss_pandemonius();
+extern void AddSC_mana_tombs();
 extern void AddSC_boss_anzu();                              // auchindoun, sethekk_halls
 extern void AddSC_boss_darkweaver_syth();
 extern void AddSC_boss_talon_king_ikiss();
@@ -856,6 +857,7 @@ void AddScripts()
     AddSC_boss_shirrak();
     AddSC_boss_nexusprince_shaffar();                       // auchindoun, mana_tombs
     AddSC_boss_pandemonius();
+    AddSC_mana_tombs();
     AddSC_boss_anzu();                                      // auchindoun, sethekk_halls
     AddSC_boss_darkweaver_syth();
     AddSC_boss_talon_king_ikiss();
