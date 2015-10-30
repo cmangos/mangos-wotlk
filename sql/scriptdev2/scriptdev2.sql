d79d20c28f98561ec40fcd6962f1a2ee6f2572b4
Xfurry <xfurry@scriptdev2.com>
2015-10-30 20:46:28 +0100

    Implement script for creature 10498
    This requires SD2 as it can't be handled by ACID.
    
    Thanks to @TheTrueAnimal
    
    Close scriptdev2/scriptdev2#118

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -1092,6 +1092,7 @@ UPDATE creature_template SET ScriptName='boss_head_of_horseman' WHERE entry=2377
 UPDATE instance_template SET ScriptName='instance_scholomance' WHERE map=289;
 UPDATE creature_template SET ScriptName='boss_darkmaster_gandling' WHERE entry=1853;
 UPDATE creature_template SET ScriptName='boss_jandice_barov' WHERE entry=10503;
+UPDATE creature_template SET ScriptName='npc_spectral_tutor' WHERE entry=10498;
 DELETE FROM scripted_event_id WHERE id IN (5618, 5619, 5620, 5621, 5622, 5623);
 INSERT INTO scripted_event_id VALUES
 (5618,'event_spell_gandling_shadow_portal'),
