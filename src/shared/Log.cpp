b3d8edc8f57f168a5b27e209930866870223ff2f
Cyberium <cybermomo@techemail.com>
2015-12-19 11:56:00 +0100

    Fix for not consistant text in console due to multithread message.
    Adding more lock guard to Log class where its needed.

--- src/shared/Log.cpp
+++ src/shared/Log.cpp
@@ -326,6 +326,7 @@ void Log::outTimestamp(FILE* file)
 
 void Log::outTime()
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     time_t t = time(nullptr);
     tm* aTm = localtime(&t);
     //       YYYY   year
@@ -354,6 +355,7 @@ std::string Log::GetTimestampStr()
 
 void Log::outString()
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_includeTime)
         outTime();
     printf("\n");
@@ -371,6 +373,8 @@ void Log::outString(const char* str, ...)
 {
     if (!str)
         return;
+
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
 
     if (m_colored)
         SetColor(true, m_colors[LogNormal]);
@@ -409,6 +413,8 @@ void Log::outError(const char* err, ...)
     if (!err)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
+
     if (m_colored)
         SetColor(false, m_colors[LogError]);
 
@@ -443,6 +449,8 @@ void Log::outError(const char* err, ...)
 
 void Log::outErrorDb()
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
+
     if (m_includeTime)
         outTime();
 
@@ -469,6 +477,8 @@ void Log::outErrorDb(const char* err, ...)
 {
     if (!err)
         return;
+
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
 
     if (m_colored)
         SetColor(false, m_colors[LogError]);
@@ -518,6 +528,8 @@ void Log::outErrorDb(const char* err, ...)
 
 void Log::outErrorEventAI()
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
+
     if (m_includeTime)
         outTime();
 
@@ -545,6 +557,7 @@ void Log::outErrorEventAI(const char* err, ...)
     if (!err)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_colored)
         SetColor(false, m_colors[LogError]);
 
@@ -596,6 +609,7 @@ void Log::outBasic(const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_logLevel >= LOG_LVL_BASIC)
     {
         if (m_colored)
@@ -634,6 +648,7 @@ void Log::outDetail(const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_logLevel >= LOG_LVL_DETAIL)
     {
         if (m_colored)
@@ -674,6 +689,7 @@ void Log::outDebug(const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_logLevel >= LOG_LVL_DEBUG)
     {
         if (m_colored)
@@ -714,6 +730,7 @@ void Log::outCommand(uint32 account, const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_logLevel >= LOG_LVL_DETAIL)
     {
         if (m_colored)
@@ -776,6 +793,7 @@ void Log::outChar(const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (charLogfile)
     {
         va_list ap;
@@ -790,6 +808,7 @@ void Log::outChar(const char* str, ...)
 
 void Log::outErrorScriptLib()
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_includeTime)
         outTime();
 
@@ -820,6 +839,7 @@ void Log::outErrorScriptLib(const char* err, ...)
     if (!err)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (m_colored)
         SetColor(false, m_colors[LogError]);
 
@@ -897,6 +917,8 @@ void Log::outWorldPacketDump(uint32 socket, uint32 opcode, char const* opcodeNam
 
 void Log::outCharDump(const char* str, uint32 account_id, uint32 guid, const char* name)
 {
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
+
     if (charLogfile)
     {
         fprintf(charLogfile, "== START DUMP == (account: %u guid: %u name: %s )\n%s\n== END DUMP ==\n", account_id, guid, name, str);
@@ -909,6 +931,7 @@ void Log::outRALog(const char* str, ...)
     if (!str)
         return;
 
+    std::lock_guard<std::mutex> guard(m_worldLogMtx);
     if (raLogfile)
     {
         va_list ap;
