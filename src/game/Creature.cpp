b9006d3237870e9cd13662e0ce98217fede9bcfe
Cyberium <cybermomo@techemail.com>
2015-12-24 11:16:07 +0100

    Another way to select model id.
    This is more logical to take first available display id instead to use last one as default?
    This happen when there is more than one model id in creature_template and model_info don't have any alternative model id set for that entry.
    
    Before we used last available model id wich is wrong in "Eye of Acherus" case at least.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -450,39 +450,30 @@ uint32 Creature::ChooseDisplayId(const CreatureInfo* cinfo, const CreatureData* 
     if (data && data->modelid_override)
         return data->modelid_override;
 
-    // use defaults from the template
-    uint32 display_id = 0;
+    // use defaults from the template model id 1
+    uint32 display_id = cinfo->ModelId[0];
 
     // models may be categorized as (in this order):
     // if mod4 && mod3 && mod2 && mod1  use any, by 25%-chance (other gender is selected and replaced after this function)
-    // if mod3 && mod2 && mod1          use mod3 unless mod2 has modelid_alt_model (then all by 33%-chance)
-    // if mod2                          use mod2 unless mod2 has modelid_alt_model (then both by 50%-chance)
+    // if mod3 && mod2 && mod1          use mod1 unless mod3 has modelid_alt_model (then all by 33%-chance)
+    // if mod2                          use mod1 unless mod2 has modelid_alt_model (then both by 50%-chance)
     // if mod1                          use mod1
 
-    // The follow decision tree needs to be updated if MAX_CREATURE_MODEL is changed.
-    static_assert(MAX_CREATURE_MODEL == 4, "Need to update model selection code for new or removed model fields");
-
     // model selected here may be replaced with other_gender using own function
-    if (cinfo->ModelId[3] && cinfo->ModelId[2] && cinfo->ModelId[1] && cinfo->ModelId[0])
+    // We use this to eliminate invisible models vs. "dummy" models (infernals, etc).
+    // Where it's expected to select one of two, model must have a alternative model defined (alternative model is normally the same as defined in ModelId1).
+    // Same pattern is used in the above model selection, but the result may be ModelId3 and not ModelId2 as here.
+    if (sObjectMgr.GetCreatureModelAlternativeModel(display_id))
     {
-        display_id = cinfo->ModelId[urand(0, 3)];
-    }
-    else if (cinfo->ModelId[2] && cinfo->ModelId[1] && cinfo->ModelId[0])
-    {
-        uint32 modelid_tmp = sObjectMgr.GetCreatureModelAlternativeModel(cinfo->ModelId[1]);
-        display_id = modelid_tmp ? cinfo->ModelId[urand(0, 2)] : cinfo->ModelId[2];
-    }
-    else if (cinfo->ModelId[1])
-    {
-        // We use this to eliminate invisible models vs. "dummy" models (infernals, etc).
-        // Where it's expected to select one of two, model must have a alternative model defined (alternative model is normally the same as defined in ModelId1).
-        // Same pattern is used in the above model selection, but the result may be ModelId3 and not ModelId2 as here.
-        uint32 modelid_tmp = sObjectMgr.GetCreatureModelAlternativeModel(cinfo->ModelId[1]);
-        display_id = modelid_tmp ? cinfo->ModelId[urand(0, 1)] : cinfo->ModelId[1];
-    }
-    else if (cinfo->ModelId[0])
-    {
-        display_id = cinfo->ModelId[0];
+        uint32 alternativeId = 0;
+        for (uint32 i = 1; i < MAX_CREATURE_MODEL - 1; ++i)
+        {
+            if (cinfo->ModelId[i])
+                ++alternativeId;
+            else
+                break;
+        }
+        display_id = cinfo->ModelId[urand(0, alternativeId)];
     }
 
     // fail safe, we use creature entry 1 and make error
