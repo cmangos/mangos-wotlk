88524e5c8c41f1ba97f197fcf0a022b4bb95a241
Cyberium <cybermomo@techemail.com>
2016-01-03 13:48:30 +0100

    Fix problem that occur when the charm unit field is updated after the possess bar.
    In case of the raise ally it simply fix the window "Release Spirit" and stop client timer.
    
    Thank to @Shauren for his precious help on this.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11352,6 +11352,7 @@ Unit* Unit::TakePossessOf(SpellEntry const* spellEntry, SummonPropertiesEntry co
         player->GetCamera().SetView(pCreature);                         // modify camera view to the creature view
         player->SetClientControl(pCreature, 1);                         // transfer client control to the creature
         player->SetMover(pCreature);                                    // set mover so now we know that creature is "moved" by this unit
+        player->SendForcedObjectUpdate();                               // we have to update client data here to avoid problem with the "release spirit" windows reappear.
     }
 
     // initialize AI
@@ -11401,6 +11402,7 @@ bool Unit::TakePossessOf(Unit* possessed)
         player->GetCamera().SetView(possessed);
         player->SetClientControl(possessed, 1);
         player->SetMover(possessed);
+        player->SendForcedObjectUpdate();
 
         if (possessedCreature && possessedCreature->IsPet() && possessedCreature->GetObjectGuid() == GetPetGuid())
         {
