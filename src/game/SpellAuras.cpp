f8387192d332e20542c9eef002f945500536ac64
Xfurry <xfurry@scriptdev2.com>
2016-01-20 21:59:28 +0100

    Fix spells 48979 and 49483
    Close cmangos/issues#834

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -7876,6 +7876,9 @@ void Aura::PeriodicTick()
             // so 17 is rounded amount for 5 sec tick grow ~ 1 range grow in 3 sec
             if (powerType == POWER_RAGE)
                 target->ModifyPower(powerType, m_modifier.m_amount * 3 / 5);
+            // Butchery
+            else if (powerType == POWER_RUNIC_POWER && target->isInCombat())
+                target->ModifyPower(powerType, m_modifier.m_amount);
             break;
         }
         // Here tick dummy auras
