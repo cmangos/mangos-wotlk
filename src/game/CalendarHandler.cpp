31a2a09dc04e4aba6e392281a72d25e2d5c68503
stfx <stfx@hotmail.de>
2016-03-18 22:29:12 +0100

    Local variable is never used

--- src/game/CalendarHandler.cpp
+++ src/game/CalendarHandler.cpp
@@ -122,7 +122,6 @@ void WorldSession::HandleCalendarGetCalendar(WorldPacket& /*recv_data*/)
     {
         uint32 map_diff_pair = itr->first;
         uint32 mapId = PAIR32_LOPART(map_diff_pair);
-        Difficulty difficulty = Difficulty(PAIR32_HIPART(map_diff_pair));
         MapDifficultyEntry const* mapDiff = itr->second;
 
         // skip mapDiff without global reset time
@@ -584,7 +583,7 @@ void WorldSession::HandleCalendarEventRemoveInvite(WorldPacket& recv_data)
     DEBUG_FILTER_LOG(LOG_FILTER_CALENDAR, "EventId [" UI64FMTD "], ownerInviteId [" UI64FMTD "], Invitee ([%s] id: [" UI64FMTD "])",
                      eventId, ownerInviteId, invitee.GetString().c_str(), inviteId);
 
-    if (CalendarEvent* event = sCalendarMgr.GetEventById(eventId))
+    if (sCalendarMgr.GetEventById(eventId))
         sCalendarMgr.RemoveInvite(eventId, inviteId, guid);
     else
         sCalendarMgr.SendCalendarCommandResult(_player, CALENDAR_ERROR_EVENT_INVALID);
