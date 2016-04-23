81dd5b0513dba2e1e83edd6b27805f8144858d08
Harley Larsen <harley@harleylarsen.com>
2016-04-22 23:50:59 -0700

    fix lightwell gameobject not appearing after spell is cast

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -11312,6 +11312,11 @@ void Spell::EffectTransmitted(SpellEffectIndex eff_idx)
             }
             break;
         }
+        case GAMEOBJECT_TYPE_SPELLCASTER:
+        {
+            m_caster->AddGameObject(pGameObj);
+            break;
+        }
         case GAMEOBJECT_TYPE_FISHINGHOLE:
         case GAMEOBJECT_TYPE_CHEST:
         default:
