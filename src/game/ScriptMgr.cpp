83ff2277c3061c445ade30c122849b4d787211dc
Xfurry <xfurry@scriptdev2.com>
2016-04-24 23:16:59 +0200

    Update script commands doc for the recent commit

--- src/game/ScriptMgr.cpp
+++ src/game/ScriptMgr.cpp
@@ -2067,7 +2067,7 @@ bool ScriptAction::HandleScriptStep()
             }
             break;
         }
-        case SCRIPT_COMMAND_UPDATE_TEMPLATE:
+        case SCRIPT_COMMAND_UPDATE_TEMPLATE:                // 44
         {
             if (LogIfNotCreature(pSource))
                 return false;
