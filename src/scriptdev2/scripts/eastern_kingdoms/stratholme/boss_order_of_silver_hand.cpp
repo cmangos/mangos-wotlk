114dedcfd7444c48f94b7ef6a98b9986b81422e5
killerwife <killerwife@gmail.com>
2016-05-21 12:44:49 +0200

    Fix group kill credit for quest 9737
    Signed-off-by: Xfurry <xfurry@cmangos.net>

--- src/scriptdev2/scripts/eastern_kingdoms/stratholme/boss_order_of_silver_hand.cpp
+++ src/scriptdev2/scripts/eastern_kingdoms/stratholme/boss_order_of_silver_hand.cpp
@@ -106,7 +106,7 @@ struct boss_silver_hand_bossesAI : public ScriptedAI
                 if (pKiller->GetTypeId() == TYPEID_PLAYER)
                 {
                     if (Creature* pCredit = m_pInstance->GetSingleCreatureFromStorage(NPC_PALADIN_QUEST_CREDIT))
-                        ((Player*)pKiller)->KilledMonsterCredit(NPC_PALADIN_QUEST_CREDIT, pCredit->GetObjectGuid());
+                        ((Player*)pKiller)->RewardPlayerAndGroupAtEvent(pCredit->GetEntry(), pCredit);
                 }
             }
         }
