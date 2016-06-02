b2404e85846e92e8eb5cf3dc6cfd0b094203e560
Sidsukana <pgsilent@gmail.com>
2016-06-02 10:16:13 +0200

    AURA_STATE_HEALTHLESS_XX_PERCENT apply only on alive targets
    before -> http://i.imgur.com/LlBPDBD.jpg
    after -> http://i.imgur.com/NbybJ7X.jpg
    
    close https://github.com/cmangos/mangos-classic/pull/135

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -362,9 +362,13 @@ void Unit::Update(uint32 update_diff, uint32 p_time)
     // update abilities available only for fraction of time
     UpdateReactives(update_diff);
 
-    ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, GetHealth() < GetMaxHealth() * 0.20f);
-    ModifyAuraState(AURA_STATE_HEALTHLESS_35_PERCENT, GetHealth() < GetMaxHealth() * 0.35f);
-    ModifyAuraState(AURA_STATE_HEALTH_ABOVE_75_PERCENT, GetHealth() > GetMaxHealth() * 0.75f);
+    if (isAlive())
+    {
+        ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, GetHealth() < GetMaxHealth() * 0.20f);
+        ModifyAuraState(AURA_STATE_HEALTHLESS_35_PERCENT, GetHealth() < GetMaxHealth() * 0.35f);
+        ModifyAuraState(AURA_STATE_HEALTH_ABOVE_75_PERCENT, GetHealth() > GetMaxHealth() * 0.75f);
+    }
+
     UpdateSplineMovement(p_time);
     i_motionMaster.UpdateMotion(p_time);
 }
