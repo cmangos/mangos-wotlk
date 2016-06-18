e1a12d411fb28f1c4bf803ca645495b41c030ba7
killerwife <killerwife@gmail.com>
2016-06-18 22:45:59 +0200

    q.10657 script

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -354,6 +354,11 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                         damage = damage * unitTarget->GetMaxHealth() / 100;
                         break;
                     }
+                    // Lightning Strike
+                    case 37841:
+                        if (unitTarget->GetTypeId() == TYPEID_PLAYER && unitTarget->HasAura(37830)) // Repolarized Magneto Sphere
+                            ((Player*)unitTarget)->KilledMonsterCredit(21910);
+                        break;
                     // Cataclysmic Bolt
                     case 38441:
                     {
