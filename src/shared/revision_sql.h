7e23f3cc02cc4f8c339f973e1169d41c7eb30792
Laizerox <dpetmanson@gmail.com>
2016-07-05 17:38:59 +0200

    Game_event linkedTo addition

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
  #define REVISION_DB_CHARACTERS "required_12937_01_characters_pvpstats"
- #define REVISION_DB_MANGOS "required_12940_01_mangos_creature_template"
+ #define REVISION_DB_MANGOS "required_12941_01_mangos_event_linkedto"
 #endif // __REVISION_SQL_H__
