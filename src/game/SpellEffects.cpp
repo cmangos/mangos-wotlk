c2ace3a6ee059d3ee801509668c7aa7088d72590
Cyberium <cybermomo@techemail.com>
2016-07-31 12:49:21 +0200

    Fix a couple of invalid name for spell attributes
    fix incorrect use of SPELL_ATTR_EX_UNK7. Using it as negative indicator wich is not right.
    SPELL_ATTR_UNK26 seem to be a better candidate
    
    This is backported from trinitycore my apologies i cant find the correct author.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6115,7 +6115,7 @@ void Spell::EffectDispel(SpellEffectIndex eff_idx)
                 if (!holder->IsPositive())
                     positive = false;
                 else
-                    positive = !holder->GetSpellProto()->HasAttribute(SPELL_ATTR_EX_NEGATIVE);
+                    positive = !holder->GetSpellProto()->HasAttribute(SPELL_ATTR_NEGATIVE);
 
                 // do not remove positive auras if friendly target
                 //               negative auras if non-friendly target
