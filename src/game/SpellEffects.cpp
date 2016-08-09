629fd1ef7e7e1b14c132f06920f6f0465941b306
killerwife <killerwife@gmail.com>
2016-08-09 17:43:11 +0200

    Fix looting of chests which require a key
    close https://github.com/cmangos/mangos-tbc/pull/67
    close https://github.com/cmangos/issues/issues/777

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5266,7 +5266,10 @@ void Spell::SendLoot(ObjectGuid guid, LootType loottype, LockType lockType)
 
                 Loot*& loot = gameObjTarget->loot;
                 if (!loot)
+                {
                     loot = new Loot((Player*)m_caster, gameObjTarget, loottype);
+                    TakeCastItem();
+                }
                 loot->ShowContentTo((Player*)m_caster);
                 return;
             }
@@ -5279,7 +5282,10 @@ void Spell::SendLoot(ObjectGuid guid, LootType loottype, LockType lockType)
             {
                 Loot*& loot = itemTarget->loot;
                 if (!loot)
+                {
                     loot = new Loot((Player*)m_caster, itemTarget, loottype);
+                    TakeCastItem();
+                }
                 loot->ShowContentTo((Player*)m_caster);
                 return;
             }
