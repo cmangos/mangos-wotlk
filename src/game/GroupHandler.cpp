9dab2c760eb6826e1bff37db995d218a11dbb2b1
Ono <warlockbugs@users.noreply.github.com>
2016-11-03 21:39:45 +0300

    Switch usage of guid to pointer

--- src/game/GroupHandler.cpp
+++ src/game/GroupHandler.cpp
@@ -641,7 +641,7 @@ void WorldSession::BuildPartyMemberStatsChangedPacket(Player* player, WorldPacke
     *data << uint32(mask);
 
     if (mask & GROUP_UPDATE_FLAG_STATUS)
-        *data << uint16(GetGroupMemberStatus(player->GetObjectGuid()));
+        *data << uint16(GetGroupMemberStatus(player));
 
     if (mask & GROUP_UPDATE_FLAG_CUR_HP)
         *data << uint32(player->GetHealth());
@@ -801,7 +801,7 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode(WorldPacket& recv_data)
 
     Powers powerType = player->GetPowerType();
     data << uint32(mask1);                                  // group update mask
-    data << uint16(GetGroupMemberStatus(guid));             // member's online status
+    data << uint16(GetGroupMemberStatus(player));           // member's online status
     data << uint32(player->GetHealth());                    // GROUP_UPDATE_FLAG_CUR_HP
     data << uint32(player->GetMaxHealth());                 // GROUP_UPDATE_FLAG_MAX_HP
     data << uint8(powerType);                               // GROUP_UPDATE_FLAG_POWER_TYPE
