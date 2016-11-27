506c55555e4fbe2ea3d2386e0866ed38f316405c
Ono <warlockbugs@users.noreply.github.com>
2016-11-27 22:04:24 +0300

    Incapacitated (Feared/Confused/Stunned) units cannot Block/Dodge/Parry

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3507,7 +3507,7 @@ uint32 Unit::GetDefenseSkillValue(Unit const* target) const
 
 float Unit::GetUnitDodgeChance() const
 {
-    if (hasUnitState(UNIT_STAT_STUNNED))
+    if (IsIncapacitated())
         return 0.0f;
     if (GetTypeId() == TYPEID_PLAYER)
         return GetFloatValue(PLAYER_DODGE_PERCENTAGE);
@@ -3526,7 +3526,7 @@ float Unit::GetUnitDodgeChance() const
 
 float Unit::GetUnitParryChance() const
 {
-    if (IsNonMeleeSpellCasted(false) || hasUnitState(UNIT_STAT_STUNNED))
+    if (IsNonMeleeSpellCasted(false) || IsIncapacitated())
         return 0.0f;
 
     float chance = 0.0f;
@@ -3558,7 +3558,7 @@ float Unit::GetUnitParryChance() const
 
 float Unit::GetUnitBlockChance() const
 {
-    if (IsNonMeleeSpellCasted(false) || hasUnitState(UNIT_STAT_STUNNED))
+    if (IsNonMeleeSpellCasted(false) || IsIncapacitated())
         return 0.0f;
 
     if (GetTypeId() == TYPEID_PLAYER)
