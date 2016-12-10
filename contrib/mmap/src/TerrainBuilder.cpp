b651490c1397803ceacb8fea1c7ccd96c36ad4c0
boxa <boxa@mail.ru>
2016-12-10 21:58:57 +0300

    MoveMapGenerator: improvements and fix * Correct Nav type for WMO objects (picked from TC, author @zorix) more info here: https://github.com/TrinityCore/TrinityCore/pull/18364 * Allow to npc and pets jump over small fences * Change mmap files version to 5
    All strongly tested on live servers, works very well
    
    !WARNING: required mmap files reextraction!

--- contrib/mmap/src/TerrainBuilder.cpp
+++ contrib/mmap/src/TerrainBuilder.cpp
@@ -631,7 +631,7 @@ namespace MMAP
                         uint8 type = NAV_EMPTY;
 
                         // convert liquid type to NavTerrain
-                        switch (liquid->GetType())
+                        switch (liquid->GetType() & 3)
                         {
                             case 0:
                             case 1:
