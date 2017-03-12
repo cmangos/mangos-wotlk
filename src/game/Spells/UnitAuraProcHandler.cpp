b48d65e7c25009a659668e8bb09a353322b14d7d
killerwife <killerwife@gmail.com>
2017-03-12 20:45:06 +0100

    Implement Flexibility Script

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -3859,6 +3859,9 @@ SpellAuraProcResult Unit::HandleOverrideClassScriptAuraProc(Unit* pVictim, uint3
         case 5497:                                          // Improved Mana Gems (Serpent-Coil Braid)
             triggered_spell_id = 37445;                     // Mana Surge
             break;
+        case 5510:                                          // Flexibility - T4 Holy Priest bonus
+            RemoveAurasDueToSpell(37565);
+            return SPELL_AURA_PROC_OK;
         case 6953:                                          // Warbringer
             RemoveAurasAtMechanicImmunity(IMMUNE_TO_ROOT_AND_SNARE_MASK, 0, true);
             return SPELL_AURA_PROC_OK;
