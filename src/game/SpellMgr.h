d36bbee82bff0fa8e7be1dc2bb49d75cf7848336
killerwife <killerwife@gmail.com>
2017-06-09 11:23:36 +0200

    Passive spells need to be castable when dead
    Talents, proficiencies, etc

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -198,7 +198,7 @@ inline bool IsSealSpell(SpellEntry const* spellInfo)
 
 inline bool IsAllowingDeadTarget(SpellEntry const* spellInfo)
 {
-    return spellInfo->HasAttribute(SPELL_ATTR_EX2_CAN_TARGET_DEAD) || spellInfo->Targets & (TARGET_FLAG_PVP_CORPSE | TARGET_FLAG_UNIT_CORPSE | TARGET_FLAG_CORPSE_ALLY);
+    return spellInfo->HasAttribute(SPELL_ATTR_EX2_CAN_TARGET_DEAD) || spellInfo->HasAttribute(SPELL_ATTR_PASSIVE) || spellInfo->Targets & (TARGET_FLAG_PVP_CORPSE | TARGET_FLAG_UNIT_CORPSE | TARGET_FLAG_CORPSE_ALLY);
 }
 
 inline bool IsElementalShield(SpellEntry const* spellInfo)
