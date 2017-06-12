7f910d5fc1ae3227c4a862ddb52a8adb656de51f
killerwife <killerwife@gmail.com>
2017-06-12 19:10:46 +0200

    Refactor AttackStart and MoveInLineOfSight for AI globally

--- src/game/AI/ScriptDevAI/include/sc_creature.h
+++ src/game/AI/ScriptDevAI/include/sc_creature.h
@@ -58,7 +58,7 @@ struct ScriptedAI : public CreatureAI
         // == Reactions At =================================
 
         // Called if IsVisible(Unit* pWho) is true at each relative pWho move
-        void MoveInLineOfSight(Unit* pWho) override;
+        // void MoveInLineOfSight(Unit* pWho) override;
 
         // Called for reaction at enter to combat if not in combat yet (enemy can be NULL)
         void EnterCombat(Unit* pEnemy) override;
@@ -125,7 +125,7 @@ struct ScriptedAI : public CreatureAI
         void ReceiveEmote(Player* /*pPlayer*/, uint32 /*uiEmote*/) override {}
 
         // Called at each attack of m_creature by any victim
-        void AttackStart(Unit* pWho) override;
+        // void AttackStart(Unit* pWho) override;
 
         // Called at World update tick
         void UpdateAI(const uint32) override;
