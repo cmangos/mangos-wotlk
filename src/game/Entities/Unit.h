2032cfb9384d9007c57e9d9a1941a5b89ac631a5
Ono <warlockbugs@users.noreply.github.com>
2017-06-20 11:03:40 +0300

    Rename deprecated flag UNIT_FLAG_DISABLE_MOVE
    https://github.com/cmangos/issues/issues/1397

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -558,7 +558,7 @@ enum UnitFlags
 {
     UNIT_FLAG_UNK_0                 = 0x00000001,
     UNIT_FLAG_NON_ATTACKABLE        = 0x00000002,           // not attackable
-    UNIT_FLAG_DISABLE_MOVE          = 0x00000004,
+    UNIT_FLAG_NON_MOVING_DEPRECATED = 0x00000004,           // TODO: Needs research
     UNIT_FLAG_PVP_ATTACKABLE        = 0x00000008,           // allow apply pvp rules to attackable state in addition to faction dependent state
     UNIT_FLAG_RENAME                = 0x00000010,
     UNIT_FLAG_PREPARATION           = 0x00000020,           // don't take reagents for spells with SPELL_ATTR_EX5_NO_REAGENT_WHILE_PREP
