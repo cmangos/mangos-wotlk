a04102b77b6472294bf003b69394c1d9eec40378
killerwife <killerwife@gmail.com>
2017-06-26 22:56:56 +0200

    Implement TRIGGERED_AUTOREPEAT
    Fix checkcast check that have to be done for autorepeat spell.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -392,6 +392,7 @@ Spell::Spell(Unit* caster, SpellEntry const* info, uint32 triggeredFlags, Object
     m_ignoreHitResult = !!(triggeredFlags & TRIGGERED_IGNORE_HIT_CALCULATION);
     m_ignoreUnselectableTarget = m_IsTriggeredSpell || (triggeredFlags & TRIGGERED_IGNORE_UNSELECTABLE_FLAG) != 0;
     m_ignoreUnattackableTarget = !!(triggeredFlags & TRIGGERED_IGNORE_UNATTACKABLE_FLAG);
+    m_triggerAutorepeat = !!(triggeredFlags & TRIGGERED_AUTOREPEAT);
 
     m_reflectable = IsReflectableSpell(m_spellInfo);
     m_spellFlags = SPELL_FLAG_NORMAL;
@@ -3171,7 +3172,8 @@ void Spell::Prepare()
     if (!m_IsTriggeredSpell)
     {
         // add to cast type slot
-        m_caster->SetCurrentCastedSpell(this);
+        if (!m_spellInfo->HasAttribute(SPELL_ATTR_EX4_CAN_CAST_WHILE_CASTING) && !m_triggerAutorepeat)
+            m_caster->SetCurrentCastedSpell(this);
 
         // will show cast bar
         SendSpellStart();
@@ -3179,7 +3181,7 @@ void Spell::Prepare()
         TriggerGlobalCooldown();
 
         // Execute instant spells immediate
-        if (m_timer == 0 && !IsNextMeleeSwingSpell() && !IsAutoRepeat() && !IsChanneledSpell(m_spellInfo))
+        if (m_timer == 0 && !IsNextMeleeSwingSpell() && (!IsAutoRepeat() || m_triggerAutorepeat) && !IsChanneledSpell(m_spellInfo))
             cast();
     }
     // execute triggered without cast time explicitly in call point
