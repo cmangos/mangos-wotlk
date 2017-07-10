b0126ca6af6b0d1e06eb642061a6202d4409be08
Phatcat <holsthein@live.dk>
2017-07-10 21:16:29 +0200

    [EventAI] update events; aura and missing aura
    There's no reason the npc needs to be in combat for this, do that logic
    in the ACID script itself, don't hardcore enforce it here.

--- src/game/AI/EventAI/CreatureEventAI.cpp
+++ src/game/AI/EventAI/CreatureEventAI.cpp
@@ -403,9 +403,6 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
             break;
         case EVENT_T_AURA:
         {
-            if (!m_creature->isInCombat())
-                return false;
-
             SpellAuraHolder* holder = m_creature->GetSpellAuraHolder(event.buffed.spellId);
             if (!holder || holder->GetStackAmount() < event.buffed.amount)
                 return false;
@@ -431,9 +428,6 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
         }
         case EVENT_T_MISSING_AURA:
         {
-            if (!m_creature->isInCombat())
-                return false;
-
             SpellAuraHolder* holder = m_creature->GetSpellAuraHolder(event.buffed.spellId);
             if (holder && holder->GetStackAmount() >= event.buffed.amount)
                 return false;
