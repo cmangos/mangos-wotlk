3239541a2610132e595840628c7d03342b531bfe
killerwife <killerwife@gmail.com>
2017-07-26 16:48:02 +0200

    Add OnPlayerResurrect SD2 hook

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -4712,6 +4712,10 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
     // update visibility of player for nearby cameras
     UpdateObjectVisibility();
 
+    if (IsInWorld())
+        if (InstanceData* data = GetMap()->GetInstanceData())
+            data->OnPlayerResurrect(this);
+
     if (!applySickness)
         return;
 
