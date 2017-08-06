145169d201a7fee248d4a392eab70f8b36c10190
killerwife <killerwife@gmail.com>
2017-08-06 15:01:37 +0200

    [13959] Implement dbscript_random_templates and drop dbscript_string_template

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
  #define REVISION_DB_CHARACTERS "required_13957_03_characters_playerbot_quest_data"
- #define REVISION_DB_MANGOS "required_13958_01_mangos_dbscripts_on_relay"
+ #define REVISION_DB_MANGOS "required_13959_01_mangos_dbscript_random_template"
 #endif // __REVISION_SQL_H__
