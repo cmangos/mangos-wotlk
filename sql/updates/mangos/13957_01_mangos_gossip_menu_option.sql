d407b62cf6a222933558f341e1ba063569678bfd
Cyberium <cyberium@users.noreply.github.com>
2017-08-11 12:21:34 +0200

    Fix column number in sql update.

--- sql/updates/mangos/13957_01_mangos_gossip_menu_option.sql
+++ sql/updates/mangos/13957_01_mangos_gossip_menu_option.sql
@@ -1,3 +1,4 @@
 ALTER TABLE db_version CHANGE COLUMN required_13956_01_mangos_spell_chain_totems_typos required_13957_01_mangos_gossip_menu_option bit;
 
-INSERT INTO `gossip_menu_option` VALUES('0','16','0','GOSSIP_OPTION_BOT','99','1','0','0','0','0','0',NULL,'0','0','0','0','0','0','0','0','0');
+DELETE FROM gossip_menu_option WHERE menu_id=0 AND id=16;
+INSERT INTO `gossip_menu_option` VALUES('0','16','0','GOSSIP_OPTION_BOT','99','1','0','0','0','0','0',NULL,'0');
