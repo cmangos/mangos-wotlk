624b6d1944cfe695c8d59613703dc1c93132f372
killerwife <killerwife@gmail.com>
2017-09-27 22:25:30 +0200

    Add more information to auth fail log

--- src/game/Server/WorldSocket.cpp
+++ src/game/Server/WorldSocket.cpp
@@ -435,7 +435,7 @@ bool WorldSocket::HandleAuthSession(WorldPacket& recvPacket)
 
         SendPacket(packet);
 
-        sLog.outError("WorldSocket::HandleAuthSession: Sent Auth Response (authentification failed).");
+        sLog.outError("WorldSocket::HandleAuthSession: Sent Auth Response (authentification failed), account ID: %u.", id);
         return false;
     }
 
