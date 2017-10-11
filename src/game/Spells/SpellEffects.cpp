silent92/mangos-tbc

Remove more duplicate code and include EnterEvadeMode in refactor

src/game/Spells/SpellEffects.cpp
@@ -3990,15 +3990,13 @@ bool Spell::DoSummonPet(SpellEffectIndex eff_idx)
     spawnCreature->InitStatsForLevel(level);
 
     if (CharmInfo* charmInfo = spawnCreature->GetCharmInfo())
-    {
-        charmInfo->GetAI()->SetReactState(REACT_DEFENSIVE);
         charmInfo->SetPetNumber(pet_number, spawnCreature->isControlled());
-    }
 
     // spawnCreature->SetName("");                          // generated by client
 
     map->Add((Creature*)spawnCreature);
     spawnCreature->AIM_Initialize();
+    spawnCreature->AI()->SetReactState(REACT_DEFENSIVE);
     spawnCreature->InitPetCreateSpells();
 
     m_caster->SetPet(spawnCreature);
@@ -4399,16 +4397,7 @@ bool Spell::DoSummonGuardian(CreatureSummonPositions& list, SummonPropertiesEntr
             spawnCreature->SetPvP(true);
 
         if (CharmInfo* charmInfo = spawnCreature->GetCharmInfo())
-        {
             charmInfo->SetPetNumber(pet_number, false);
-
-            if (cInfo->UnitFlags & UNIT_FLAG_IMMUNE_TO_NPC)
-                charmInfo->GetAI()->SetReactState(REACT_PASSIVE);
-            else if (cInfo->ExtraFlags & CREATURE_EXTRA_FLAG_NO_MELEE)
-                charmInfo->GetAI()->SetReactState(REACT_DEFENSIVE);
-            else
-                charmInfo->GetAI()->SetReactState(REACT_AGGRESSIVE);
-        }
 
         m_caster->AddGuardian(spawnCreature);
     }
@@ -4693,8 +4682,6 @@ void Spell::EffectTameCreature(SpellEffectIndex /*eff_idx*/)
 
     pet->GetCharmInfo()->SetPetNumber(sObjectMgr.GeneratePetNumber(), true);
 
-    pet->GetCharmInfo()->GetAI()->SetReactState(REACT_DEFENSIVE);
-
     uint32 level = creatureTarget->getLevel();
     pet->InitStatsForLevel(level);
 
@@ -4709,6 +4696,8 @@ void Spell::EffectTameCreature(SpellEffectIndex /*eff_idx*/)
     // add pet object to the world
     pet->GetMap()->Add((Creature*)pet);
     pet->AIM_Initialize();
+
+    pet->AI()->SetReactState(REACT_DEFENSIVE);
 
     // visual effect for levelup
     pet->SetUInt32Value(UNIT_FIELD_LEVEL, level);
@@ -4809,7 +4798,6 @@ void Spell::EffectSummonPet(SpellEffectIndex eff_idx)
     // Level of pet summoned
     uint32 level = std::max(m_caster->getLevel() + m_spellInfo->EffectMultipleValue[eff_idx], 1.0f);
 
-    NewSummon->GetCharmInfo()->GetAI()->SetReactState(REACT_DEFENSIVE);
     NewSummon->SetOwnerGuid(m_caster->GetObjectGuid());
     NewSummon->setFaction(m_caster->getFaction());
     NewSummon->SetUInt32Value(UNIT_FIELD_PET_NAME_TIMESTAMP, uint32(time(nullptr)));
@@ -4820,6 +4808,7 @@ void Spell::EffectSummonPet(SpellEffectIndex eff_idx)
 
     map->Add((Creature*)NewSummon);
     NewSummon->AIM_Initialize();
+    NewSummon->AI()->SetReactState(REACT_DEFENSIVE);
 
     m_caster->SetPet(NewSummon);
     DEBUG_LOG("New Pet has guid %u", NewSummon->GetGUIDLow());

cmangos/mangos-wotlk

src/game/Spells/SpellEffects.cpp
@@ -6181,7 +6181,7 @@ bool Spell::DoSummonPet(SpellEffectIndex eff_idx)
 
     if (m_caster->GetTypeId() == TYPEID_PLAYER)
     {
-        spawnCreature->GetCharmInfo()->GetAI()->SetReactState(REACT_DEFENSIVE);
+        spawnCreature->GetAI()->SetReactState(REACT_DEFENSIVE);
         ((Player*)m_caster)->PetSpellInitialize();
         if (m_caster->getClass() != CLASS_PRIEST)
             spawnCreature->SavePetToDB(PET_SAVE_AS_CURRENT);
@@ -6854,7 +6854,7 @@ void Spell::EffectTameCreature(SpellEffectIndex /*eff_idx*/)
     pet->GetMap()->Add((Creature*)pet);
     pet->AIM_Initialize();
 
-    pet->GetCharmInfo()->GetAI()->SetReactState(REACT_DEFENSIVE);
+    pet->GetAI()->SetReactState(REACT_DEFENSIVE);
 
     // visual effect for levelup
     pet->SetUInt32Value(UNIT_FIELD_LEVEL, level);
@@ -6953,7 +6953,7 @@ void Spell::EffectSummonPet(SpellEffectIndex eff_idx)
     map->Add((Creature*)NewSummon);
     NewSummon->AIM_Initialize();
 
-    NewSummon->GetCharmInfo()->GetAI()->SetReactState(REACT_DEFENSIVE);
+    NewSummon->GetAI()->SetReactState(REACT_DEFENSIVE);
 
     m_caster->SetPet(NewSummon);
     DEBUG_LOG("New Pet has guid %u", NewSummon->GetGUIDLow());