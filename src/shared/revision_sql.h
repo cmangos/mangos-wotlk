ea756dc380c977ec8b793ea0eb1468b8e929f3c8
killerwife <killerwife@gmail.com>
2017-10-13 12:26:59 +0200

    [13966] Drop redundant creature_movement columns

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
  #define REVISION_DB_CHARACTERS "required_13963_01_characters_account_instances_entered"
- #define REVISION_DB_MANGOS "required_13965_01_mangos_questgiver_greeting"
+ #define REVISION_DB_MANGOS "required_13966_01_mangos_creature_movement_drops"
 #endif // __REVISION_SQL_H__
