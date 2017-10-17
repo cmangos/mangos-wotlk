c6e225a49257682054b80c532a63b99e3c4d3720
killerwife <killerwife@gmail.com>
2017-10-17 18:31:46 +0200

    Fix Vanish Purge serverside for wotlk

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -259,8 +259,8 @@ INSERT INTO spell_template (Id, SchoolMask, Category, Attributes, AttributesEx, 
 (23644, 4, 0, 536871168, 268435456, 0, 0, 1, 0, 0, 101, 0, 13, 1, 0, 0, 1, 0, 0, 1710, 'Burning Adrenaline', 1, 1);
 
 -- missing rogue vanish serverside
-INSERT INTO spell_template(Id,Attributes,AttributesEx,DurationIndex,Effect1,Effect2,EffectDieSides1,EffectDieSides2,EffectBaseDice1,EffectBaseDice2,EffectBasePoints1,EffectBasePoints2,EffectImplicitTargetA1,EffectImplicitTargetA2,EffectApplyAuraName1,EffectApplyAuraName2,EffectMiscValue1,EffectMiscValue2,SpellIconID,SpellName,SpellFamilyName,DmgMultiplier1,DmgMultiplier2,DmgMultiplier3) VALUES
-(18461,262544,268599296,36,6,6,1,1,1,1,-1,-1,1,1,77,77,7,11,252,'Vanish Purge',7,1,1,1);
+INSERT INTO spell_template(Id,Attributes,AttributesEx,DurationIndex,Effect1,Effect2,EffectImplicitTargetA1,EffectImplicitTargetA2,EffectApplyAuraName1,EffectApplyAuraName2,EffectMiscValue1,EffectMiscValue2,SpellIconID,SpellName,SpellFamilyName,DmgMultiplier1,DmgMultiplier2,DmgMultiplier3) VALUES
+(18461,262544,268599296,36,6,6,1,1,77,77,7,11,252,'Vanish Purge',7,1,1,1);
 
 INSERT INTO spell_template(Id,Attributes,CastingTimeIndex,AuraInterruptFlags,DurationIndex,Effect1,EffectDieSides1,EffectBaseDice1,EffectImplicitTargetA1,EffectMiscValue1,SpellIconID,SpellName,DmgMultiplier1,DmgMultiplier2,DmgMultiplier3) VALUES
 (37957,256,1,128,4,76,1,1,18,185033,289,'Gnome Mercy: Summon Collection of Souls Chest',1,1,1);
