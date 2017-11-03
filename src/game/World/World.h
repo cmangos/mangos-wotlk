2dcc068da70d87f72bf06b95fb4c40bc2d1e3eac
killerwife <killerwife@gmail.com>
2017-11-03 10:10:04 +0100

    Add compensation for latency to GCD Exposes diff from world so that it can be used in other places

--- src/game/World/World.h
+++ src/game/World/World.h
@@ -617,6 +617,7 @@ class World
         void InvalidatePlayerDataToAllClient(ObjectGuid guid) const;
 
         static TimePoint GetCurrentClockTime() { return m_currentTime; }
+        static uint32 GetCurrentDiff() { return m_currentDiff; }
 
     protected:
         void _UpdateGameTime();
@@ -723,6 +724,7 @@ class World
         std::vector<std::string> m_spamRecords;
 
         static TimePoint m_currentTime;
+        static uint32 m_currentDiff;
 };
 
 extern uint32 realmID;
