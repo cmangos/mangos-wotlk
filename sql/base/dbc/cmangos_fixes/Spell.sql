c8720f4d9cffa3019295f183321144e701b99171
Saltgurka <saltgurkasalt@gmail.com>
2017-11-04 20:23:40 +0100

    Add Serverside Summoning spells used by c.18885 and c.18886

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -346,6 +346,16 @@ INSERT INTO spell_template (Id,Attributes,AttributesEx,AttributesEx3,CastingTime
 -- summoned creatures behave like wild summons and not like guardians per sniff data
 UPDATE spell_template SET EffectMiscValueB1=64 WHERE Id IN(33803,38611);
 
+INSERT INTO spell_template (Id,Attributes,AttributesEx,AttributesEx3,CastingTimeIndex,procChance,DurationIndex,rangeIndex,StackAmount,EquippedItemClass,Effect1,Effect2,Effect3,EffectDieSides1,EffectDieSides2,EffectDieSides3,EffectBaseDice1,EffectBaseDice2,EffectBaseDice3,EffectImplicitTargetA1,EffectImplicitTargetA2,EffectImplicitTargetA3,EffectRadiusIndex1,EffectApplyAuraName2,EffectApplyAuraName3,EffectMiscValue1,EffectMiscValue3,EffectMiscValueB1,SpellIconID,SpellName,DmgMultiplier1,DmgMultiplier2,DmgMultiplier3) VALUES
+-- Used by Farahlon Giant c.18885
+(36042,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21077,127,64,2154,'Summon Farahlon Crumbler',1,1,1),
+(36043,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21077,127,64,2154,'Summon Farahlon Crumbler',1,1,1),
+(36044,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21077,127,64,2154,'Summon Farahlon Crumbler',1,1,1),
+-- Used by Farahlon Breaker c.18886
+(36045,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21078,127,64,2154,'Summon Farahlon Shardling',1,1,1),
+(36046,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21078,127,64,2154,'Summon Farahlon Shardling',1,1,1),
+(36047,384,268435456,1048576,1,101,21,1,3,-1,28,6,6,1,-31,-21,1,1,1,48,1,1,29,61,79,21078,127,64,2154,'Summon Farahlon Shardling',1,1,1);
+
 -- Missing selector spells for BWL
 INSERT INTO spell_template (id, attributes, attributesEx, attributesEx2, CastingTimeIndex, ProcChance, RangeIndex, Effect1, EffectImplicitTargetA1, EffectImplicitTargetB1, EffectRadiusIndex1, EffectMiscValue1, SpellIconId, SpellName, DmgMultiplier1) VALUES
 (23195, 8388864, 0, 1, 1, 101, 1, 3, 1, 0, 0, 0, 1, 'Dragondog Breath Selection', 1),
