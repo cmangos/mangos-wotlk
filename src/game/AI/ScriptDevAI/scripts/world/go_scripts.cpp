3f6b060e431ee811d790c2285fdedf58f9e68bad
Killerwife <killerwife@gmail.com>
2017-11-19 20:04:10 +0100

    Implement Transpolyporter trap searcher
    Only succeeds when player has Goblin Transponder Item

--- src/game/AI/ScriptDevAI/scripts/world/go_scripts.cpp
+++ src/game/AI/ScriptDevAI/scripts/world/go_scripts.cpp
@@ -765,6 +765,25 @@ GameObjectAI* GetAIgo_pirate_day_music(GameObject* go)
     return new go_pirate_day_music(go);
 }
 
+enum
+{
+    ITEM_GOBLIN_TRANSPONDER = 9173,
+};
+
+bool TrapTargetSearch(Unit* unit)
+{
+    if (unit->GetTypeId() == TYPEID_PLAYER)
+    {
+        Player* player = static_cast<Player*>(unit);
+        if (player->HasItemCount(ITEM_GOBLIN_TRANSPONDER, 1))
+            return true;
+    }
+
+    return false;
+}
+
+std::function<bool(Unit*)> function = &TrapTargetSearch;
+
 void AddSC_go_scripts()
 {
     Script* pNewScript;
@@ -837,5 +856,10 @@ void AddSC_go_scripts()
     pNewScript = new Script;
     pNewScript->Name = "go_pirate_day_music";
     pNewScript->GetGameObjectAI = &GetAIgo_pirate_day_music;
+    pNewScript->RegisterSelf();
+
+    pNewScript = new Script;
+    pNewScript->Name = "go_transpolyporter_bb";
+    pNewScript->pTrapSearching = &function;
     pNewScript->RegisterSelf();
 }
