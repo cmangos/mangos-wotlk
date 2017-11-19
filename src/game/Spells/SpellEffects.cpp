3b6b096c85275f39327b432b05e66e853cece991
Killerwife <killerwife@gmail.com>
2017-11-19 11:37:03 +0100

    Fix devastate sunder missing

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -7459,7 +7459,7 @@ void Spell::EffectWeaponDmg(SpellEffectIndex eff_idx)
                 // Devastate causing Sunder Armor Effect
                 // and no need to cast over max stack amount
                 if (!sunder || sunder->GetStackAmount() < sunder->GetSpellProto()->StackAmount)
-                    m_caster->CastSpell(unitTarget, 58567, TRIGGERED_OLD_TRIGGERED);
+                    m_caster->CastSpell(unitTarget, 58567, TRIGGERED_OLD_TRIGGERED | TRIGGERED_IGNORE_HIT_CALCULATION);
             }
             break;
         }
