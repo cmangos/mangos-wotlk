ef007fda884effbdab8b40cce3f5c95d45bd331b
Killerwife <killerwife@gmail.com>
2017-12-09 23:25:59 +0100

    Any confused target is second choice

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -9783,7 +9783,7 @@ bool Unit::IsSecondChoiceTarget(Unit* pTarget, bool taunt, bool checkThreatArea)
 
     return
         (!pTarget->IsVehicle() && pTarget->IsTargetUnderControl(*this)) ||
-        (!taunt && pTarget->IsImmuneToDamage(GetMeleeDamageSchoolMask())) ||
+        (!taunt && pTarget->IsImmuneToDamage(GetMeleeDamageSchoolMask())) || pTarget->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_CONFUSED) ||
         pTarget->hasNegativeAuraWithInterruptFlag(AURA_INTERRUPT_FLAG_DAMAGE) ||
         (thisCreature && checkThreatArea && thisCreature->IsOutOfThreatArea(pTarget));
 }
