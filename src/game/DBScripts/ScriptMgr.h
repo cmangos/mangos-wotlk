2520a00173bf840821195d165bbf34bcdc0740ff
Xfurry <xfurry.cmangos@outlook.com>
2017-12-10 22:20:04 +0100

    Allow DB scripts to remove aura by caster
    This is specifically useful for vehicles with multiple passengers as it
    allows the script to unboard each of them individually
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/DBScripts/ScriptMgr.h
+++ src/game/DBScripts/ScriptMgr.h
@@ -462,7 +462,7 @@ struct ScriptInfo
         {
             case SCRIPT_COMMAND_MOVE_TO:
             case SCRIPT_COMMAND_TEMP_SPAWN_CREATURE:
-            case SCRIPT_COMMAND_CAST_SPELL:
+            case SCRIPT_COMMAND_REMOVE_AURA:
             case SCRIPT_COMMAND_PLAY_SOUND:
             case SCRIPT_COMMAND_MOVEMENT:
             case SCRIPT_COMMAND_MORPH_TO_ENTRY_OR_MODEL:
