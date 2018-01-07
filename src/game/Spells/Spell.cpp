0e65850a40e459b653b610f81140a9b7d5e537b1
killerwife <killerwife@gmail.com>
2018-01-07 09:21:54 +0100

    A no duration channel should proc triggers on aura end

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4205,14 +4205,15 @@ void Spell::finish(bool ok)
     if (m_spellState == SPELL_STATE_FINISHED)
         return;
 
+    bool channeledChannel = m_spellState == SPELL_STATE_CHANNELING;
     m_spellState = SPELL_STATE_FINISHED;
 
     // other code related only to successfully finished spells
     if (!ok)
         return;
 
-    // Normal spells proc on finish, channeled spells proc on start
-    if (!IsChanneledSpell(m_spellInfo))
+    // Normal spells proc on finish, channeled spells proc on start when they have duration, thats when channeledChannel is true
+    if (!IsChanneledSpell(m_spellInfo) && !channeledChannel)
         ProcSpellAuraTriggers();
 
     // Heal caster for all health leech from all targets
