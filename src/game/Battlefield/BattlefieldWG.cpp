a3a25285f5eddebf2b5f13dd29e8e97adf9fee2c
Yaki Khadafi <ElSolDolLo@gmail.com>
2018-01-08 22:19:42 +0100

    Add basic functions to be used in battlefield Wintergrasp
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Battlefield/BattlefieldWG.cpp
+++ src/game/Battlefield/BattlefieldWG.cpp
@@ -15,3 +15,115 @@
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
+#include "BattlefieldWG.h"
+#include "Entities/Creature.h"
+#include "Entities/GameObject.h"
+#include "Entities/Player.h"
+#include "Entities/Unit.h"
+#include "Spells/SpellAuras.h"
+#include "Globals/ObjectMgr.h"
+
+BattlefieldWG::BattlefieldWG() : Battlefield(),
+    m_zoneOwner(TEAM_NONE)
+{
+    m_zoneId = ZONE_ID_WINTERGRASP;
+    m_battleFieldId = BATTLEFIELD_WG;
+
+    Reset();
+}
+
+void BattlefieldWG::Reset()
+{
+    m_status = BF_STATUS_COOLDOWN;
+}
+
+void BattlefieldWG::FillInitialWorldStates(WorldPacket& data, uint32& count)
+{
+
+}
+
+void BattlefieldWG::SendRemoveWorldStates(Player* player)
+{
+
+}
+
+void BattlefieldWG::HandlePlayerEnterZone(Player* player, bool isMainZone)
+{
+    Battlefield::HandlePlayerEnterZone(player, isMainZone);
+}
+
+void BattlefieldWG::HandlePlayerLeaveZone(Player* player, bool isMainZone)
+{
+    Battlefield::HandlePlayerLeaveZone(player, isMainZone);
+}
+
+void BattlefieldWG::HandleCreatureCreate(Creature* creature)
+{
+
+}
+
+void BattlefieldWG::HandleCreatureDeath(Creature* creature)
+{
+
+}
+
+void BattlefieldWG::HandleGameObjectCreate(GameObject* go)
+{
+    OutdoorPvP::HandleGameObjectCreate(go);
+}
+
+void BattlefieldWG::HandlePlayerKillInsideArea(Player* player, Unit* victim)
+{
+
+}
+
+bool BattlefieldWG::HandleEvent(uint32 uiEventId, GameObject* pGo)
+{
+    return false;
+}
+
+void BattlefieldWG::Update(uint32 diff)
+{
+    Battlefield::Update(diff);
+}
+
+void BattlefieldWG::StartBattle(Team defender)
+{
+
+}
+
+void BattlefieldWG::EndBattle(Team winner, bool byTimer)
+{
+
+}
+
+void BattlefieldWG::RewardPlayersOnBattleEnd(Team winner)
+{
+
+}
+
+void BattlefieldWG::InitPlayerBattlefieldData(Player* player)
+{
+
+}
+
+void BattlefieldWG::SetupPlayerPosition(Player* player)
+{
+
+}
+
+void BattlefieldWG::UpdateGraveyardOwner(uint8 id, PvpTeamIndex newOwner)
+{
+
+}
+
+bool BattlefieldWG::GetPlayerKickLocation(Player* player, float& x, float& y, float& z)
+{
+    return false;
+}
+
+void BattlefieldWG::UpdatePlayerOnWarResponse(Player* player)
+{
+
+}
--- src/game/Battlefield/BattlefieldWG.h
+++ src/game/Battlefield/BattlefieldWG.h
@@ -15,3 +15,59 @@
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
+#ifndef WORLD_PVP_WG
+#define WORLD_PVP_WG
+
+#include "Common.h"
+#include "Battlefield.h"
+#include "Tools/Language.h"
+#include "World/World.h"
+
+class Group;
+class BattlefieldWG;
+
+enum
+{
+    // ToDo: add the script defines
+};
+
+class BattlefieldWG : public Battlefield
+{
+    public:
+        BattlefieldWG();
+
+        void Reset();
+
+        void HandlePlayerEnterZone(Player* player, bool isMainZone) override;
+        void HandlePlayerLeaveZone(Player* player, bool isMainZone) override;
+        void FillInitialWorldStates(WorldPacket& data, uint32& count) override;
+        void SendRemoveWorldStates(Player* player) override;
+
+        bool HandleEvent(uint32 eventId, GameObject* go) override;
+
+        void HandleCreatureCreate(Creature* creature) override;
+        void HandleGameObjectCreate(GameObject* go) override;
+        void HandleCreatureDeath(Creature* creature) override;
+
+        void HandlePlayerKillInsideArea(Player* player, Unit* victim) override;
+        void Update(uint32 diff) override;
+
+        void StartBattle(Team defender) override;
+        void EndBattle(Team winner, bool byTimer) override;
+        void RewardPlayersOnBattleEnd(Team winner) override;
+
+        void UpdatePlayerOnWarResponse(Player* player) override;
+
+        void UpdateGraveyardOwner(uint8 id, PvpTeamIndex newOwner) override;
+
+    private:
+        void InitPlayerBattlefieldData(Player* player) override;
+        bool GetPlayerKickLocation(Player* player, float& x, float& y, float& z) override;
+
+        void SetupPlayerPosition(Player* player) override;
+
+        Team m_zoneOwner;
+};
+
+#endif
