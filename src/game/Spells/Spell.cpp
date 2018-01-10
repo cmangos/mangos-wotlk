0fb8959d84d0d968bbed80ffec10cadfe83a9cac
killerwife <killerwife@gmail.com>
2018-01-10 21:02:22 +0100

    Dont report errors from triggeredByAuraSpells

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4300,6 +4300,9 @@ void Spell::finish(bool ok)
 
 void Spell::SendCastResult(SpellCastResult result) const
 {
+    if (m_triggeredByAuraSpell) // should not report these
+        return;
+
     Player const* recipient;
     if (m_caster->GetTypeId() != TYPEID_PLAYER)
     {
