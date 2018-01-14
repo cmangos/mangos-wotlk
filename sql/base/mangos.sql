0821f68dc3d71bce17270c3d7e321148f5c32219
Yaki Khadafi <ElSolDolLo@gmail.com>
2018-01-14 18:46:29 +0100

    [13972] Add battlefield emote strings for Wintergrasp
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -24,7 +24,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_13971_01_mangos_spam_records_length` bit(1) DEFAULT NULL
+  `required_13972_01_mangos_string` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
@@ -4801,7 +4801,45 @@ INSERT INTO `mangos_string` VALUES
 (1633,'|cffffff00The Alliance has taken control of Halaa!|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1634,'|cffffff00Halaa is defenseless!|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1635,'|cffffff00The Horde has collected 200 silithyst!|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(1636,'|cffffff00The Alliance has collected 200 silithyst!|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(1636,'|cffffff00The Alliance has collected 200 silithyst!|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1637,'The battle for Wintergrasp is about to begin!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1638,'Let the battle begin!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1639,'The Alliance has defended Wintergrasp Fortress!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1640,'The Alliance has captured Wintergrasp Fortress!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1641,'The Horde has defended Wintergrasp Fortress!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1642,'The Horde has captured Wintergrasp Fortress!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1643,'The Broken Temple siege workshop has been attacked by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1644,'The Broken Temple siege workshop has been captured by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1645,'The Broken Temple siege workshop has been attacked by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1646,'The Broken Temple siege workshop has been captured by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1647,'The Westspark siege workshop has been attacked by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1648,'The Westspark siege workshop has been captured by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1649,'The Westspark siege workshop has been attacked by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1650,'The Westspark siege workshop has been captured by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1651,'The Eastspark siege workshop has been attacked by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1652,'The Eastspark siege workshop has been captured by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1653,'The Eastspark siege workshop has been attacked by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1654,'The Eastspark siege workshop has been captured by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1655,'The Sunken Ring siege workshop has been attacked by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1656,'The Sunken Ring siege workshop has been captured by the Alliance!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1657,'The Sunken Ring siege workshop has been attacked by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1658,'The Sunken Ring siege workshop has been captured by the Horde!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1659,'The western tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1660,'The western tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1661,'The southern tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1662,'The southern tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1663,'The eastern tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1664,'The eastern tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1665,'The north-western keep tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1666,'The north-western keep tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1667,'The north-eastern keep tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1668,'The north-eastern keep tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1669,'The south-western keep tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1670,'The south-western keep tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1671,'The south-eastern keep tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1672,'The south-eastern keep tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1673,'You have reached Rank 1: Corporal',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(1674,'You have reached Rank 2: First Lieutenant',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `mangos_string` ENABLE KEYS */;
 UNLOCK TABLES;
 
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_13970_01_realmd_totp"
  #define REVISION_DB_CHARACTERS "required_13963_01_characters_account_instances_entered"
- #define REVISION_DB_MANGOS "required_13971_01_mangos_spam_records_length"
+ #define REVISION_DB_MANGOS "required_13972_01_mangos_string"
 #endif // __REVISION_SQL_H__
