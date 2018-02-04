c3150cf1827e01fb67d1f62cfa114d0f3133c7ff
Killerwife <killerwife@gmail.com>
2018-02-04 21:31:18 +0100

    Add Moveflag debug command

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -238,6 +238,7 @@ class ChatHandler
         bool HandleDebugUpdateWorldStateCommand(char* args);
         bool HandleDebugWaypoint(char* args);
         bool HandleDebugSpellVisual(char* args);
+        bool HandleDebugMoveflags(char* args);
 
         bool HandleDebugPlayCinematicCommand(char* args);
         bool HandleDebugPlayMovieCommand(char* args);
