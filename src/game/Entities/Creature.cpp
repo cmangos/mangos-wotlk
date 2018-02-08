97b63b3e84038d5ddc145e3b6b50f6e91372c1a1
Killerwife <killerwife@gmail.com>
2018-02-08 20:06:11 +0100

    Implement CREATURE_EXTRA_FLAG_HASTE_SPELL_IMMUNITY Also adds some numbers to the comments for ease of use

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -1783,6 +1783,11 @@ bool Creature::IsImmuneToSpellEffect(SpellEntry const* spellInfo, SpellEffectInd
         else if (spellInfo->Effect[index] == SPELL_EFFECT_ATTACK_ME)
             return true;
     }
+    // Haste spell aura immunity
+    if (GetCreatureInfo()->ExtraFlags & CREATURE_EXTRA_FLAG_HASTE_SPELL_IMMUNITY)
+        if (spellInfo->Effect[index] == SPELL_EFFECT_APPLY_AURA)
+            if (spellInfo->EffectApplyAuraName[index] == SPELL_AURA_HASTE_SPELLS)
+                return true;
 
     return Unit::IsImmuneToSpellEffect(spellInfo, index, castOnSelf);
 }
