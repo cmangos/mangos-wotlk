938821aaf1361017ac67f1ed9011f6c39002a0df
Killerwife <killerwife@gmail.com>
2018-02-11 20:05:23 +0100

    Implement Jub Jub 1 week hatching

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -8007,6 +8007,11 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     m_caster->CastSpell(unitTarget, 22682, TRIGGERED_OLD_TRIGGERED);
                     return;
                 }
+                case 23853:                                 // Jub Jub Cooldown
+                {
+                    unitTarget->CastSpell(nullptr, 23852, TRIGGERED_NONE);
+                    return;
+                }
                 case 24194:                                 // Uther's Tribute
                 case 24195:                                 // Grom's Tribute
                 {
