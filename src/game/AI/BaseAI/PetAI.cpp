1ffef01152e56e99fcdd82469520bf9cd552dad8
Killerwife <killerwife@gmail.com>
2018-03-01 14:50:21 +0100

    Fix pet not attacking in combat when aggroed during defensive stance
    The follow condition expects follow movegen not to be on pet anymore.
    Follow movegen is not supposed to be removed, as well as being present
    at times even in current buggy system.

--- src/game/AI/BaseAI/PetAI.cpp
+++ src/game/AI/BaseAI/PetAI.cpp
@@ -243,7 +243,7 @@ void PetAI::UpdateAI(const uint32 diff)
 
                 Spell* spell = new Spell(m_unit, spellInfo, TRIGGERED_NONE);
 
-                if (inCombat && !m_unit->hasUnitState(UNIT_STAT_FOLLOW) && spell->CanAutoCast(victim))
+                if (inCombat && spell->CanAutoCast(victim))
                 {
                     targetSpellStore.push_back(TargetSpellList::value_type(victim, spell));
                     continue;
