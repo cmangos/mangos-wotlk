50802fd0566a6291de90d0675494baa17a995803
Deyan Dobromirov <dvd_video@abv.bg>
2018-05-09 11:06:47 +0300

    Update grant privileges

--- sql/create/db_drop_mysql.sql
+++ sql/create/db_drop_mysql.sql
@@ -2,15 +2,9 @@ REVOKE ALL PRIVILEGES ON * . * FROM 'mangos'@'localhost';
 
 REVOKE ALL PRIVILEGES ON `mangos` . * FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `mangos` . * FROM 'mangos'@'localhost';
-
 REVOKE ALL PRIVILEGES ON `characters` . * FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'mangos'@'localhost';
-
 REVOKE ALL PRIVILEGES ON `realmd` . * FROM 'mangos'@'localhost';
-
-REVOKE GRANT OPTION ON `realmd` . * FROM 'mangos'@'localhost';
 
 DELETE FROM `mysql`.`user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'mangos' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
 
@@ -19,3 +13,5 @@ DROP DATABASE IF EXISTS `mangos` ;
 DROP DATABASE IF EXISTS `characters` ;
 
 DROP DATABASE IF EXISTS `realmd` ;
+
+DROP USER IF EXISTS `mangos`;
