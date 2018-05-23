fa48023fa2a07087ebf95c0873d25523e858e1f7
killerwife <killerwife@gmail.com>
2018-05-23 19:17:49 +0200

    Remove GetFixateTargetGuid

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -3289,7 +3289,7 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
 
                     if (apply)
                         caster->FixateTarget(target);
-                    else if (target->GetObjectGuid() == caster->GetFixateTargetGuid())
+                    else
                         caster->FixateTarget(nullptr);
 
                     return;
