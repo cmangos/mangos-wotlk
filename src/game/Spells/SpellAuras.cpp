14b8277571ef2340b083dc09e9e3953b9cba07f3
anonxs <anonxs@web.de>
2018-05-27 23:15:19 +0200

    Kill original Caster for s.40105 triggered by 40106 used by Aqueous Spawn 22883 on Aqueous Lord 22878

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -2131,6 +2131,7 @@ void Aura::TriggerSpell()
                 return;
             }
             case 33419:                                     // Arcane Missiles - TODO: Review other spells with TARGET_UNIT_ENEMY
+            case 40106:                                     // Merge
             case 42483:                                     // Ooze Channel
             {
                 triggerCaster = GetCaster();
