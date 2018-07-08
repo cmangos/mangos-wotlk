6453a769d05a6bf61fa525b39cc54b3531d53e80
killerwife <killerwife@gmail.com>
2018-07-08 10:52:25 +0200

    Fix compilation warning with float constant in deadsoul orb

--- src/game/AI/ScriptDevAI/scripts/outland/blades_edge_mountains.cpp
+++ src/game/AI/ScriptDevAI/scripts/outland/blades_edge_mountains.cpp
@@ -1435,7 +1435,7 @@ struct npc_deadsoul_orb : public ScriptedAI
             m_creature->GetMotionMaster()->MovePoint(pointCount, 2809.716f, 5250.526f, 274.4666f);
         else
         {
-            if (Creature* waypointTrigger = GetClosestCreatureWithEntry(m_creature, nextTrigger, 95.f))
+            if (Creature* waypointTrigger = GetClosestCreatureWithEntry(m_creature, nextTrigger, 95))
             {
                 m_creature->GetMotionMaster()->MovePoint(pointCount, waypointTrigger->GetPositionX(), waypointTrigger->GetPositionY(), waypointTrigger->GetPositionZ());
             }
