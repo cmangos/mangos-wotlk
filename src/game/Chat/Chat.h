33e9dcdb0b7d419292c558f9692a2838e7ce4950
Killerwife <killerwife@gmail.com>
2018-07-18 18:52:08 +0200

    Implement LOS debug command

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -613,6 +613,7 @@ class ChatHandler
         bool HandleCooldownClearArenaCommand(char* args);
         bool HandleUnLearnCommand(char* args);
         bool HandleGetDistanceCommand(char* args);
+        bool HandleGetLosCommand(char* args);
         bool HandleModifyStandStateCommand(char* args);
         bool HandleDieCommand(char* args);
         bool HandleDamageCommand(char* args);
