a68879648a328eb010761bdfb0f066a9cc9123bb
Killerwife <killerwife@gmail.com>
2018-07-19 19:37:23 +0200

    Fix GM mode change field updates
    Now everything is forced to change every time

--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -2444,7 +2444,6 @@ class Player : public Unit
         // Public Save system functions
         void SaveItemToInventory(Item* item); // optimization for gift wrapping
         void SaveTitles(); // optimization for arena rewards
-
     protected:
         /*********************************************************/
         /***               BATTLEGROUND SYSTEM                 ***/
