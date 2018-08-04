e7d0a3b1820bab4cc0f489fc18a4c13b3d0c587a
Killerwife <killerwife@gmail.com>
2018-08-04 13:56:04 +0200

    Remove mistake in query in mangos file due to threat changes

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -4864,7 +4864,7 @@ INSERT INTO `mangos_string` VALUES
 (1671,'The south-eastern keep tower has been damaged!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1672,'The south-eastern keep tower has been destroyed!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1673,'You have reached Rank 1: Corporal',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(1674,'You have reached Rank 2: First Lieutenant',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(1674,'You have reached Rank 2: First Lieutenant',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1702,'Player |cffff0000%s|r [GUID: %u] has |cffff0000%f|r threat and taunt state %u',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1703,'Showing threat for %s [Entry %u]',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `mangos_string` ENABLE KEYS */;
