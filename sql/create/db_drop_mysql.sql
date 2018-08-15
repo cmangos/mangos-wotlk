bf206f71a2720f3795a2a804dc16dcecf3d83a4a
killerwife <killerwife@gmail.com>
2018-08-15 21:41:57 +0200

    Change the DB default schema names to reflect repository

--- sql/create/db_drop_mysql.sql
+++ sql/create/db_drop_mysql.sql
@@ -1,21 +1,21 @@
 REVOKE ALL PRIVILEGES ON * . * FROM 'mangos'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `mangos` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `wotlkmangos` . * FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `mangos` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `wotlkmangos` . * FROM 'mangos'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `characters` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `wotlkcharacters` . * FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `wotlkcharacters` . * FROM 'mangos'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `realmd` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `wotlkrealmd` . * FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `realmd` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `wotlkrealmd` . * FROM 'mangos'@'localhost';
 
 DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'mangos' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
 
-DROP DATABASE IF EXISTS `mangos` ;
+DROP DATABASE IF EXISTS `wotlkmangos` ;
 
-DROP DATABASE IF EXISTS `characters` ;
+DROP DATABASE IF EXISTS `wotlkcharacters` ;
 
-DROP DATABASE IF EXISTS `realmd` ;
+DROP DATABASE IF EXISTS `wotlkrealmd` ;
