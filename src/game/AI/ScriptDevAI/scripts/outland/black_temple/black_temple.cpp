58e7542b25328de76f2f17a205790e41e03249c8
Xfurry <xfurry.cmangos@outlook.com>
2018-09-02 15:44:24 +0200

    Move gossip scripts to DB
    Thanks to @Grz3s and @NeatElves
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/AI/ScriptDevAI/scripts/outland/black_temple/black_temple.cpp
+++ src/game/AI/ScriptDevAI/scripts/outland/black_temple/black_temple.cpp
@@ -16,51 +16,17 @@
 
 /* ScriptData
 SDName: Black_Temple
-SD%Complete: 95
-SDComment: Spirit of Olum: Player Teleporter to Seer Kanai Teleport after defeating Naj'entus and Supremus. TODO: Find proper gossip.
+SD%Complete: 0
+SDComment: Placeholder
 SDCategory: Black Temple
 EndScriptData */
 
 /* ContentData
-npc_spirit_of_olum
 EndContentData */
 
 #include "AI/ScriptDevAI/include/precompiled.h"
 #include "black_temple.h"
 
-/*###
-# npc_spirit_of_olum
-####*/
-
-#define SPELL_TELEPORT      41566                           // s41566 - Teleport to Ashtongue NPC's
-#define GOSSIP_OLUM1        "Teleport me to the other Ashtongue Deathsworn"
-
-bool GossipHello_npc_spirit_of_olum(Player* pPlayer, Creature* pCreature)
-{
-    ScriptedInstance* pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-
-    if (pInstance && (pInstance->GetData(TYPE_SUPREMUS) >= DONE) && (pInstance->GetData(TYPE_NAJENTUS) >= DONE))
-        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_OLUM1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-
-    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
-    return true;
-}
-
-bool GossipSelect_npc_spirit_of_olum(Player* pPlayer, Creature* /*pCreature*/, uint32 /*uiSender*/, uint32 uiAction)
-{
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
-        pPlayer->CLOSE_GOSSIP_MENU();
-
-    pPlayer->InterruptNonMeleeSpells(false);
-    pPlayer->CastSpell(pPlayer, SPELL_TELEPORT, TRIGGERED_NONE);
-    return true;
-}
-
 void AddSC_black_temple()
 {
-    Script* pNewScript = new Script;
-    pNewScript->Name = "npc_spirit_of_olum";
-    pNewScript->pGossipHello = &GossipHello_npc_spirit_of_olum;
-    pNewScript->pGossipSelect = &GossipSelect_npc_spirit_of_olum;
-    pNewScript->RegisterSelf();
 }
