f0d1aa6ef9c860422cfccda1f15cf73a0f13d687
Warlockbugs <warlockbugs@outlook.com>
2018-09-11 18:41:41 +0300

    Revert Seal of Command proc attribute change
    This change never made it to live out of 1.3.0 PTR, confirmed by retail combat logs.
    
    + Line endings

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -59,8 +59,6 @@ ALTER TABLE spell_template ADD COLUMN `AttributesServerside` int(11) unsigned NO
 
 UPDATE spell_template SET AttributesServerside= AttributesServerside|1 WHERE id IN(770,778,9749,9806,9907,9991,13424,13752,16857,17390,17391,17392,26993,27011); -- all spells with Cannot stealth or turn invisible in tooltip
 
-UPDATE spell_template SET Attributes= Attributes|0x00200000 WHERE Id IN(20424); -- Set Seal of Command proc as Impossible do dodge/parry/block, source - patchnotes 1.3
-
 INSERT INTO spell_template(Id,Attributes,procFlags,procChance,procCharges,DurationIndex,Effect1,EffectDieSides1,EffectImplicitTargetA1,EffectApplyAuraName1,EffectMiscValue1,DmgMultiplier1,SpellName) VALUES
 (31934,384,1048576,100,1,21,6,-51,1,87,127,1,'Blessed Life Proc');
 -- Talent will work as expected but very counterintuitively. It will proc on one damage source and reduce damage on the next.
@@ -139,7 +137,7 @@ INSERT INTO spell_template(Id, Category, Dispel, Mechanic, Attributes, Attribute
 ('19394', '0', '0', '0', '384', '268435456', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '101', '0', '0', '0', '0', '4', '0', '0', '0', '0', '0', '12', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '0', '76', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '47', '0', '0', '0', '0', '0', '15', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '177681', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', 'Summon Gordunni chest (JUNK)', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '0');
 
 -- Spells used in Ragnaros encounter
-INSERT INTO `spell_template` (`Id`,`Attributes`,`CastingTimeIndex`,`ProcChance`,`DurationIndex`,`RangeIndex`,`EquippedItemClass`,`Effect1`,`EffectImplicitTargetA1`,`EffectRadiusIndex1`,`EffectMiscValue1`,`SpellIconID`,`SpellName`,`DmgMultiplier1`,`EffectMiscValueB1`) VALUES 
+INSERT INTO `spell_template` (`Id`,`Attributes`,`CastingTimeIndex`,`ProcChance`,`DurationIndex`,`RangeIndex`,`EquippedItemClass`,`Effect1`,`EffectImplicitTargetA1`,`EffectRadiusIndex1`,`EffectMiscValue1`,`SpellIconID`,`SpellName`,`DmgMultiplier1`,`EffectMiscValueB1`) VALUES
 ('21108','256','1','101','0','13','-1','3','0','16','0','876','Summon Sons of Flame','1','0'),
 ('21110','256','1','101','30','13','-1','28','17','16','12143','876','Summon Son of Flame B','1','64'),
 ('21111','256','1','101','30','13','-1','28','17','16','12143','876','Summon Son of Flame C','1','64'),
@@ -355,7 +353,7 @@ INSERT INTO spell_template(Id,Attributes,AttributesEx,AttributesEx2,AttributesEx
 (39960,0x09800180,0x10000020,0x00000001,0x00100000,1,21,13,6,6,1,1,2,2,1,1,139,139,929,1001,1,'Finish the Gronn: Ogre Force Reaction',1,1); -- data gathered ID
 
 -- Missing Elemental Shield spell from Classic (BWL)
-INSERT INTO `spell_template` (`Id`, `Attributes`, `AttributesEx`, `AttributesEx3`, `AttributesEx4`, `CastingTimeIndex`, `ProcChance`, `DurationIndex`, `rangeIndex`, `Effect1`, `EffectDieSides1`, `EffectBaseDice1`, `EffectImplicitTargetA1`, `EffectRadiusIndex1`, `EffectMiscValue1`, `SpellIconID`, `SpellName`, `DmgMultiplier1`, `DmgMultiplier2`) VALUES 
+INSERT INTO `spell_template` (`Id`, `Attributes`, `AttributesEx`, `AttributesEx3`, `AttributesEx4`, `CastingTimeIndex`, `ProcChance`, `DurationIndex`, `rangeIndex`, `Effect1`, `EffectDieSides1`, `EffectBaseDice1`, `EffectImplicitTargetA1`, `EffectRadiusIndex1`, `EffectMiscValue1`, `SpellIconID`, `SpellName`, `DmgMultiplier1`, `DmgMultiplier2`) VALUES
 (22276, 272, 0, 0, 0, 1, 101, 0, 1, 3, 1, 1, 1, 0, 0, 459, 'Elemental Shield', 1, 0);
 
 -- Add missing vanilla spells used in scripts
