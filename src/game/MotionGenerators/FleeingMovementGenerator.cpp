d5c36158af021e9a73801b2b3f66da303dab5fa2
Warlockbugs <warlockbugs@outlook.com>
2018-11-09 12:41:21 +0300

    Fix players teleporting on fleeing movement generator finalization
    For legacy motion master: players no longer teleport to current spline's destination when fleeing movement generator finalizes

--- src/game/MotionGenerators/FleeingMovementGenerator.cpp
+++ src/game/MotionGenerators/FleeingMovementGenerator.cpp
@@ -138,7 +138,7 @@ template<>
 void FleeingMovementGenerator<Player>::Finalize(Player& owner) const
 {
     owner.clearUnitState(UNIT_STAT_FLEEING | UNIT_STAT_FLEEING_MOVE);
-    owner.StopMoving();
+    owner.StopMoving(true);
 }
 
 template<>
