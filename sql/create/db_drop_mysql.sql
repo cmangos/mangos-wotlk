563aafd3f7f3d8cc2d7402b924139f892aae305f
Cyberium <cyberium@users.noreply.github.com>
2018-11-16 20:16:55 +0100

    Fix db_drop_mysql.sql
    Another fix for this file to be more correct.

--- sql/create/db_drop_mysql.sql
+++ sql/create/db_drop_mysql.sql
@@ -1,5 +1,5 @@
 /* Delete default user and its privileges (require MYSQL 5.7, MARIADB 10.1.3) */
-DROP USER IF EXISTS `mangos`;
+DROP USER IF EXISTS `mangos`@`localhost`;
 
 /* Delete default tables */
 DROP DATABASE IF EXISTS `wotlkmangos`;
