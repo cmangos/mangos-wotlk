7b7d3d5e87e9967bd2eede210c513bd2d069c6b1
killerwife <killerwife@gmail.com>
2018-12-04 20:24:52 +0100

    Suppress warning for 48562 spell cone

--- sql/base/dbc/cmangos_fixes/SpellCone.sql
+++ sql/base/dbc/cmangos_fixes/SpellCone.sql
@@ -1,3 +1,4 @@
 -- romulo hits targets in the back and does not turn at all
 UPDATE spell_cone SET ConeDegrees=-180 WHERE id IN(30815);
+DELETE FROM spell_cone WHERE id IN(48562); -- reused in Cataclysm for a different purpose
 
