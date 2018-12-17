65fbd068b4048f425429180a11dd19addecbfc11
Xfurry <xfurry.cmangos@outlook.com>
2018-12-17 21:39:35 +0100

    Revert "Fix typo from previous commit"
    This reverts commit dc3e29dc44564d1e4b8ab7537fcc101aa1b7cb08.

--- src/game/Entities/Vehicle.cpp
+++ src/game/Entities/Vehicle.cpp
@@ -365,7 +365,7 @@ void VehicleInfo::UnBoard(Unit* passenger, bool changeVehicle)
         // TODO: Guesswork, but seems to be fairly near correct
         // Only if the passenger was on control seat? Also depending on some flags
         if ((seatEntry->m_flags & SEAT_FLAG_CAN_CONTROL) &&
-                !(m_vehicleEntry->m_flags & (VEHICLE_FLAG_FIXED_POSITION | VEHICLE_FLAG_NOT_DISMISSED)))
+                !(m_vehicleEntry->m_flags & (VEHICLE_FLAG_UNK4 | VEHICLE_FLAG_NOT_DISMISSED)))
         {
             if (((Creature*)m_owner)->IsTemporarySummon())
                 ((Creature*)m_owner)->ForcedDespawn(1000);
