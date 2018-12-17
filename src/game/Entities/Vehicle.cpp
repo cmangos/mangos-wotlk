02b6f3cc731471512cd48f91f395304c69818313
Xfurry <xfurry.cmangos@outlook.com>
2018-12-17 22:20:01 +0100

    Attempt to fix rooted vehicles one more time
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Entities/Vehicle.cpp
+++ src/game/Entities/Vehicle.cpp
@@ -152,7 +152,8 @@ void VehicleInfo::Initialize()
     if (vehicleFlags & VEHICLE_FLAG_FULLSPEEDPITCHING)
         pVehicle->m_movementInfo.AddMovementFlags2(MOVEFLAG2_FULLSPEEDPITCHING);
 
-    if (vehicleFlags & VEHICLE_FLAG_FIXED_POSITION)
+    // NOTE: this is the best possible combination to root a vehicle; However, there are still exceptions, such as creature 27292
+    if (vehicleFlags & VEHICLE_FLAG_FIXED_POSITION || (!vehicleFlags & VEHICLE_FLAG_UNK7 && vehicleFlags & VEHICLE_FLAG_UNK15))
         pVehicle->SetRoot(true);
 
     // TODO: Guesswork, but it looks correct
