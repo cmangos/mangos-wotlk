df40ec1623ac183123ca7f13fa74080dd3936aa8
MantisLord <sabinprosper@gmail.com>
2019-01-15 02:54:53 -0500

    Add scripts for Assist (40887) and Fixate (40892)

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -9400,6 +9400,23 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
 
                     return;
                 }
+                case 40887:                                 // Assist
+                {
+                    if (!unitTarget)
+                        return;
+
+                    unitTarget->CastSpell(m_caster, 40892, TRIGGERED_OLD_TRIGGERED);
+                    return;
+                }
+                case 40892:                                 // Fixate
+                {
+                    if (!unitTarget || unitTarget->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    m_caster->AI()->SendAIEvent(AI_EVENT_CUSTOM_EVENTAI_A, unitTarget, m_caster);
+                    unitTarget->CastSpell(m_caster, m_spellInfo->CalculateSimpleValue(eff_idx), TRIGGERED_OLD_TRIGGERED);
+                    return;
+                }
                 case 41055:                                 // Copy Weapon
                 case 63416:                                 // Copy Weapon
                 case 69891:                                 // Copy Weapon (No Threat)
