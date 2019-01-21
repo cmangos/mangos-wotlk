0b2f25f669a1c165935fda56b967621f01dc23da
Warlockbugs <warlockbugs@outlook.com>
2019-01-21 08:11:45 +0300

    Use specialized API in custom mount command handler

--- src/game/Chat/Level1.cpp
+++ src/game/Chat/Level1.cpp
@@ -1476,18 +1476,8 @@ bool ChatHandler::HandleModifyMountCommand(char* args)
     // chr->SetUInt32Value(UNIT_FIELD_FLAGS, UNIT_FLAG_PVP);
     chr->Mount(mId);
 
-    WorldPacket data(SMSG_FORCE_RUN_SPEED_CHANGE, (8 + 4 + 1 + 4));
-    data << chr->GetPackGUID();
-    data << (uint32)0;
-    data << (uint8)0;                                       // new 2.1.0
-    data << float(speed);
-    chr->SendMessageToSet(data, true);
-
-    data.Initialize(SMSG_FORCE_SWIM_SPEED_CHANGE, (8 + 4 + 4));
-    data << chr->GetPackGUID();
-    data << (uint32)0;
-    data << float(speed);
-    chr->SendMessageToSet(data, true);
+    chr->SetSpeedRate(MOVE_RUN, speed, true);
+    chr->SetSpeedRate(MOVE_SWIM, speed, true);
 
     return true;
 }
