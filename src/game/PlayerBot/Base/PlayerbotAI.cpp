f9e45cdae95b512f5d9f8c408a26008d2682a882
cala <cala@users.noreply.github.com>
2019-03-08 15:18:21 +0100

    [PlayerBot] Fix self use of item by bots
    * Fix bot use of items for themselves being broken after changes in the implicit targets rules. This will fix bots burning their whole stock of bandages in a matter of seconds.

--- src/game/PlayerBot/Base/PlayerbotAI.cpp
+++ src/game/PlayerBot/Base/PlayerbotAI.cpp
@@ -7655,7 +7655,7 @@ bool PlayerbotAI::CanStore()
 // use item on self
 void PlayerbotAI::UseItem(Item* item)
 {
-    UseItem(item, TARGET_FLAG_SELF, ObjectGuid());
+    UseItem(item, m_bot);
 }
 
 // use item on equipped item
