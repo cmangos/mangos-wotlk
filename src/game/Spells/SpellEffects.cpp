7d2943b78cc60e0ae909f7b0803d13bf840fef97
killerwife <killerwife@gmail.com>
2019-06-07 08:39:47 +0200

    Add mistakenly forgotten ranks of wotlk healthstones

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -13475,30 +13475,36 @@ uint32 Spell::GetUsableHealthStoneItemType(Unit* target)
         }
     }
 
-    static uint32 const itypes[6][3] =
+    static uint32 const itypes[][3] =
     {
         { 5512, 19004, 19005},              // Minor Healthstone
         { 5511, 19006, 19007},              // Lesser Healthstone
         { 5509, 19008, 19009},              // Healthstone
         { 5510, 19010, 19011},              // Greater Healthstone
         { 9421, 19012, 19013},              // Major Healthstone
-        {22103, 22104, 22105}               // Master Healthstone
+        {22103, 22104, 22105},              // Master Healthstone
+        {36889, 36890, 36891},              // Demonic Healthstone
+        {36892, 36893, 36894}               // Fel Healthstone
     };
 
     switch (m_spellInfo->Id)
     {
-    case  6201:
-        itemtype = itypes[0][rank]; break; // Minor Healthstone
-    case  6202:
-        itemtype = itypes[1][rank]; break; // Lesser Healthstone
-    case  5699:
-        itemtype = itypes[2][rank]; break; // Healthstone
-    case 11729:
-        itemtype = itypes[3][rank]; break; // Greater Healthstone
-    case 11730:
-        itemtype = itypes[4][rank]; break; // Major Healthstone
-    case 27230:
-        itemtype = itypes[5][rank]; break; // Master Healthstone
+        case  6201:
+            itemtype = itypes[0][rank]; break; // Minor Healthstone
+        case  6202:
+            itemtype = itypes[1][rank]; break; // Lesser Healthstone
+        case  5699:
+            itemtype = itypes[2][rank]; break; // Healthstone
+        case 11729:
+            itemtype = itypes[3][rank]; break; // Greater Healthstone
+        case 11730:
+            itemtype = itypes[4][rank]; break; // Major Healthstone
+        case 27230:
+            itemtype = itypes[5][rank]; break; // Master Healthstone
+        case 47871:
+            itemtype = itypes[6][rank]; break; // Demonic Healthstone
+        case 47878:
+            itemtype = itypes[7][rank]; break; // Fel Healthstone
     }
     return itemtype;
 }
