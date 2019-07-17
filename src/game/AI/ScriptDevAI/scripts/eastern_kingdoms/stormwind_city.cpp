02ddce0e705678185c38fadd6031efb64b8d1625
Qiankun Sheng <worldeditsqk@gmail.com>
2019-07-17 23:44:08 +0800

    Fix rare fail of npc_dashel_stonefistAI end phase.
    m_uiStartEventTimer & m_uiEndEventTimer related conditions in UpdateAI have a boundary logic problem. 
    if m_uiStartEventTimer == uiDiff exactly at one server tick, then the m_uiStartEventTimer will result in a certain 0 value at the very next server tick. 
    This will cause a rare false on the if (m_uiStartEventTimer) check at the beginning of UpdateAI() and lead to the related quest cannot start. 
    So as the m_uiEndEventTimer will lead to a cannot end.

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/stormwind_city.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/stormwind_city.cpp
@@ -135,7 +135,7 @@ struct npc_dashel_stonefistAI : public ScriptedAI
     {
         if (m_uiStartEventTimer)
         {
-            if (m_uiStartEventTimer < uiDiff)
+            if (m_uiStartEventTimer <= uiDiff)
             {
                 if (Player* pPlayer = m_creature->GetMap()->GetPlayer(m_playerGuid))
                 {
@@ -156,7 +156,7 @@ struct npc_dashel_stonefistAI : public ScriptedAI
 
         if (m_uiEndEventTimer)
         {
-            if (m_uiEndEventTimer < uiDiff)
+            if (m_uiEndEventTimer <= uiDiff)
             {
                 DoScriptText(SAY_STONEFIST_3, m_creature);
 
