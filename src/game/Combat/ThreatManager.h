a7cdf869eb2377a3de6acf8ce886ad13e8a9d53f
Killerwife <killerwife@gmail.com>
2019-07-26 16:43:31 +0200

    Implement ignoring of units at range during root for EAI

--- src/game/Combat/ThreatManager.h
+++ src/game/Combat/ThreatManager.h
@@ -180,7 +180,7 @@ class ThreatContainer
         void addReference(HostileReference* hostileReference) { iThreatList.push_back(hostileReference); }
         void clearReferences();
         // Sort the list if necessary
-        void update();
+        void update(bool force);
 
         ThreatList iThreatList;
     private:
