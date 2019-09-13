0fa99d5fd3dfc9f456b9c81df34e145a0085d4e2
killerwife <killerwife@gmail.com>
2019-09-13 12:21:05 +0200

    Name and implement UNIT_FLAG_IMMUNE
    Credit to TC. Verified with TBC sniffs.

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -566,7 +566,7 @@ enum UnitFlags
     UNIT_FLAG_UNK_28                = 0x10000000,
     UNIT_FLAG_UNK_29                = 0x20000000,           // used in Feing Death spell
     UNIT_FLAG_SHEATHE               = 0x40000000,
-    UNIT_FLAG_UNK_31                = 0x80000000            // set skinnable icon and also changes color of portrait
+    UNIT_FLAG_IMMUNE                = 0x80000000
 };
 
 // Value masks for UNIT_FIELD_FLAGS_2
