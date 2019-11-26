10952503c82430390905617d22deba3562f9ca99
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-11-26 17:53:19 +0000

    Fix Crystal Spire of Karabor Effect
    -Factors in prior hp rather than the players hp after the heal.

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -3815,7 +3815,7 @@ SpellAuraProcResult Unit::HandleProcTriggerSpellAuraProc(ProcExecutionData& data
             else if (auraSpellInfo->Id == 40971)
             {
                 // If your target is below $s1% health
-                if (pVictim->GetHealth() > pVictim->GetMaxHealth() * triggerAmount / 100)
+                if (pVictim->GetHealth() - damage > pVictim->GetMaxHealth() * triggerAmount / 100)
                     return SPELL_AURA_PROC_FAILED;
             }
             // Thunder Capacitor
