3b4bee8c7e3603d36765298defbae29e66344111
Killerwife <killerwife@gmail.com>
2019-12-29 13:44:26 +0100

    [14005] Dbscript: Implement priority and milisecond precision

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -24,7 +24,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_14003_01_mangos_world_safe_locs_facing` bit(1) DEFAULT NULL
+  `required_14005_01_mangos_dbscript_priority_miliseconds` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
@@ -1567,6 +1567,7 @@ DROP TABLE IF EXISTS `dbscripts_on_creature_movement`;
 CREATE TABLE `dbscripts_on_creature_movement` (
   `id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `delay` int(10) unsigned NOT NULL DEFAULT '0',
+  `priority` INT(11) UNSIGNED NOT NULL DEFAULT '0',
   `command` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `datalong` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `datalong2` int(10) unsigned NOT NULL DEFAULT '0',
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_14004_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_14000_01_characters_WorldState"
- #define REVISION_DB_MANGOS "required_14003_01_mangos_world_safe_locs_facing"
+ #define REVISION_DB_MANGOS "required_14005_01_mangos_dbscript_priority_miliseconds"
 #endif // __REVISION_SQL_H__
