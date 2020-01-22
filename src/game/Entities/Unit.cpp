01678dd9b781f1b30ea8d132261d7ad015a9f434
killerwife <killerwife@gmail.com>
2020-01-22 13:52:05 +0100

    Fix minor order bug in sending combo points to player Closes https://github.com/cmangos/issues/issues/2249

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -13493,10 +13493,10 @@ void Unit::ClearComboPoints()
     if (Unit* target = ObjectAccessor::GetUnit(*this, m_comboTargetGuid))
         target->RemoveComboPointHolder(GetGUIDLow());
 
-    m_comboTargetGuid.Clear();
-
     if (IsPlayer())
         static_cast<Player*>(this)->SendComboPoints();
+
+    m_comboTargetGuid.Clear();
 }
 
 void Unit::RegisterScalingAura(Aura* aura, bool apply)
