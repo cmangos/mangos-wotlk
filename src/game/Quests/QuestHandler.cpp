cmangos/mangos-tbc

Safeguard CMSG_PUSHQUESTTOPARTY by checking that the player has the quest

src/game/Quests/QuestHandler.cpp
@@ -422,6 +422,12 @@ void WorldSession::HandlePushQuestToParty(WorldPacket& recvPacket)
 
     DEBUG_LOG("WORLD: Received opcode CMSG_PUSHQUESTTOPARTY quest = %u", questId);
 
+    if (!_player->HasQuest(questId))
+    {
+        sLog.outError("Error in CMSG_PUSHQUESTTOPARTY - %s tried to share invalid quest (%u) (probably packet hacking)", _player->GetGuidStr().c_str(), questId);
+        return;
+    }
+
     if (Quest const* pQuest = sObjectMgr.GetQuestTemplate(questId))
     {
         if (Group* pGroup = _player->GetGroup())

cmangos/mangos-wotlk

src/game/Quests/QuestHandler.cpp
@@ -434,6 +434,12 @@ void WorldSession::HandlePushQuestToParty(WorldPacket& recvPacket)
 
     DEBUG_LOG("WORLD: Received opcode CMSG_PUSHQUESTTOPARTY quest = %u", questId);
 
+    if (!_player->CanShareQuest(questId))
+    {
+        sLog.outError("Error in CMSG_PUSHQUESTTOPARTY - %s tried to share invalid quest (%u) (probably packet hacking)", _player->GetGuidStr().c_str(), questId);
+        return;
+    }
+
     if (Quest const* pQuest = sObjectMgr.GetQuestTemplate(questId))
     {
         if (Group* pGroup = _player->GetGroup())