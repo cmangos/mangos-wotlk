226a315d126254bb64f0de7a5b24c410a3a1f4ff
anonxs <anonxs@web.de>
2020-02-01 13:18:36 +0100

    [SD] Add some Emotes for npc_millhouse_manastorm 20977 Texts

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -4833,8 +4833,8 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,broadc
 ('-1552008','We span the universe, as countless as the stars!','11131','1','0','0','0','skyriss SAY_IMAGE'),
 ('-1552009','I am merely one of... infinite multitudes.','11126','1','0','0','0','skyriss SAY_DEATH'),
 
-('-1552010','Where in Bonzo''s brass buttons am I? And who are-- yaaghh, that''s one mother of a headache!','11171','1','0','0','0','millhouse SAY_INTRO_1'),
-('-1552011','"Lowly"? I don''t care who you are friend, no one refers to the mighty Millhouse Manastorm as "Lowly"! I have no idea what goes on here, but I will gladly join your fight against this impudent imbecile! Prepare to defend yourself, cretin!','11172','1','0','0','0','millhouse SAY_INTRO_2'),
+('-1552010','Where in Bonzo''s Brass Buttons am I? And who are-- yaaghh, that''s one mother of a headache!','11171','1','0','6','0','millhouse SAY_INTRO_1'),
+('-1552011','"Lowly"? I don''t care who you are friend, no one refers to the mighty Millhouse Manastorm as "Lowly"! I have no idea what goes on here, but I will gladly join your fight against this impudent imbecile! Prepare to defend yourself, cretin!','11172','1','0','5','0','millhouse SAY_INTRO_2'),
 ('-1552012','I just need to get some things ready first. You guys go ahead and get started. I need to summon up some water...','11173','1','0','0','0','millhouse SAY_WATER'),
 ('-1552013','Fantastic! Next, some protective spells. Yes! Now we''re cookin''','11174','1','0','0','0','millhouse SAY_BUFFS'),
 ('-1552014','And of course i''ll need some mana. You guys are gonna love this, just wait.','11175','1','0','0','0','millhouse SAY_DRINK'),
@@ -4845,7 +4845,7 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,broadc
 ('-1552019','Ice, ice, baby!','11180','1','0','0','0','millhouse SAY_ICEBLOCK'),
 ('-1552020','Heal me! Oh, for the love of all that is holy, HEAL me! I''m dying!','11181','1','0','0','0','millhouse SAY_LOWHP'),
 ('-1552021','You''ll be hearing from my lawyer...','11182','1','0','0','0','millhouse SAY_DEATH'),
-('-1552022','Who''s bad? Who''s bad? That''s right: we bad!','11183','1','0','0','0','millhouse SAY_COMPLETE'),
+('-1552022','Who''s bad? Who''s bad? That''s right: we bad!','11183','1','0','4','0','millhouse SAY_COMPLETE'),
 
 ('-1552023','I knew the prince would be angry but, I... I have not been myself. I had to let them out! The great one speaks to me, you see. Wait--outsiders. Kael''thas did not send you! Good... I''ll just tell the prince you released the prisoners!','11222','1','0','0','0','mellichar YELL_INTRO1'),
 ('-1552024','The naaru kept some of the most dangerous beings in existence here in these cells. Let me introduce you to another...','11223','1','0','0','0','mellichar YELL_INTRO2'),
