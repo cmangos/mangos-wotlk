03f2a21b4987b59d2de96f87aaff12ddd3b9894f
Warlockbugs <warlockbugs@outlook.com>
2020-03-11 20:03:27 +0300

    [14008] New Ticket System: SQL timestamp columns compliance
    Modify columns to be compatible with strict dates settings

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_14004_01_realmd_banning"
- #define REVISION_DB_CHARACTERS "required_14007_01_characters_new_ticket_system"
+ #define REVISION_DB_CHARACTERS "required_14008_01_characters_new_ticket_system_sql_dates"
  #define REVISION_DB_MANGOS "required_14007_01_mangos_new_ticket_system"
 #endif // __REVISION_SQL_H__
