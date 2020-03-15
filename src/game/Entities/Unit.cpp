9ceb5597a90b1a1af60e8adfda75f56b96a39c33
killerwife <killerwife@gmail.com>
2020-03-15 19:13:11 +0100

    Fix outsider helping formula for diminishing xp

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -13586,8 +13586,8 @@ uint32 Unit::GetModifierXpBasedOnDamageReceived(uint32 xp)
         float percentageHp = float(GetDamageDoneByOthers()) / health;
         if (percentageHp >= 1.f)
             xp = 0;
-        else
-            xp *= percentageHp;
+        else if (percentageHp > 0.f)
+            xp *= (1.f - percentageHp);
     }
     return xp;
 }
