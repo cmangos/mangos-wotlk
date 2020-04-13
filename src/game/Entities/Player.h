806b1c71f0b86a09b237093f84ffceef993096d3
Warlockbugs <warlockbugs@outlook.com>
2020-04-13 19:37:51 +0300

    Revert "Chat: Implement LFG channel joining method to accompany leaving one"
    This reverts commit ce311b56ec8a0c9e20e020796ba752bc0db7f6db.
    
    My latest reverse engineering efforts did show that joining channels requires client to be initiator, otherwise its either ignored or leads to bugs.

--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -1911,7 +1911,6 @@ class Player : public Unit
         void LeftChannel(Channel* c);
         void CleanupChannels();
         void UpdateLocalChannels(uint32 newZone);
-        void JoinLFGChannel();
         void LeaveLFGChannel();
 
         void UpdateDefense();
