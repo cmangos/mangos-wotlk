1b0ceb2030419ad3f783109c30ba71dd68d787e0
killerwife <killerwife@gmail.com>
2020-04-16 11:36:24 +0200

    Fix chain heal not working
    Closes https://github.com/cmangos/mangos-tbc/pull/385

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -2694,9 +2694,9 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, bool targ
             if (!m_caster->CanAssistSpell(unitTarget, m_spellInfo))
                 break;
 
-            if (targetingData.chainTargetCount[effIndex] <= 1)
-                tempUnitList.push_back(unitTarget);
-            else
+            tempUnitList.push_back(unitTarget);
+
+            if (targetingData.chainTargetCount[effIndex] > 1)
             {
                 float max_range = targetingData.chainTargetCount[effIndex] * CHAIN_SPELL_JUMP_RADIUS;
                 Group* group = nullptr;
@@ -2735,7 +2735,7 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, bool targ
 
                     }
                 }
-             
+
                 UnitList tempAoeList;
                 MaNGOS::AnyFriendlyOrGroupMemberUnitInUnitRangeCheck u_check(m_caster, group, m_spellInfo, max_range);
                 MaNGOS::UnitListSearcher<MaNGOS::AnyFriendlyOrGroupMemberUnitInUnitRangeCheck> searcher(tempAoeList, u_check);
@@ -8155,7 +8155,7 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellEffectIndex effIndex,
                 if (!prev->IsWithinDist(*next, m_jumpRadius))
                     break;
 
-                if (!prev->IsWithinLOSInMap(*next))
+                if (!prev->IsWithinLOSInMap(*next, true))
                 {
                     ++next;
                     continue;
