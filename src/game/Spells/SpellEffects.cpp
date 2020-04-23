7689b5c694b29244f38938405377104d68eae993
killerwife <killerwife@gmail.com>
2020-04-23 11:44:24 +0200

    Revert "Fix meaningless diff between cores in Spell::EffectFeedPet"
    This reverts commit c71d3e61674a0268b09049a91f102456480444aa.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -12895,7 +12895,7 @@ void Spell::EffectFeedPet(SpellEffectIndex eff_idx)
 
     Player* _player = (Player*)m_caster;
 
-    Item* foodItem = itemTarget;
+    Item* foodItem = m_targets.getItemTarget();
     if (!foodItem)
         return;
 
