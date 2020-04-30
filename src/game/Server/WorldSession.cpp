a4ae89e6855ae5d1b9ad0fabc518d669cf389d6b
Cyberium <cyberium@users.noreply.github.com>
2020-04-30 14:51:39 +0200

    Fix kicking a bot

--- src/game/Server/WorldSession.cpp
+++ src/game/Server/WorldSession.cpp
@@ -382,8 +382,8 @@ bool WorldSession::Update(PacketFilter& updater)
                 OpcodeHandler const& opHandle = opcodeTable[botpacket->GetOpcode()];
                 pBotWorldSession->ExecuteOpcode(opHandle, *botpacket);
             }
-            pBotWorldSession->m_recvQueue.clear();
         }
+        GetPlayer()->GetPlayerbotMgr()->RemoveBots();
     }
 #endif
 
@@ -465,7 +465,7 @@ void WorldSession::LogoutPlayer()
 #ifdef BUILD_PLAYERBOT
         // Log out all player bots owned by this toon
         if (_player->GetPlayerbotMgr())
-            _player->GetPlayerbotMgr()->LogoutAllBots();
+            _player->GetPlayerbotMgr()->LogoutAllBots(true);
 #endif
 
         sLog.outChar("Account: %d (IP: %s) Logout Character:[%s] (guid: %u)", GetAccountId(), GetRemoteAddress().c_str(), _player->GetName(), _player->GetGUIDLow());
@@ -630,7 +630,19 @@ void WorldSession::LogoutPlayer()
 /// Kick a player out of the World
 void WorldSession::KickPlayer()
 {
+#ifdef BUILD_PLAYERBOT
+    if (_player->GetPlayerbotAI())
+    {
+        auto master = _player->GetPlayerbotAI()->GetMaster();
+        auto botMgr = master->GetPlayerbotMgr();
+        if (botMgr)
+            botMgr->LogoutPlayerBot(_player->GetObjectGuid());
+    }
+    else
+        LogoutRequest(time(nullptr) - 20, false);
+#else
     LogoutRequest(time(nullptr) - 20, false);
+#endif
 }
 
 void WorldSession::SendExpectedSpamRecords()
