b0ca9596b2eb9b24f4e3e01a2dae30d2ae12c167
Warlockbugs <warlockbugs@outlook.com>
2020-06-06 21:31:54 +0300

    [14018] Extend GM mount command functionality by a popular request
    Command '.gm mountup' now has additional args:
    * If a number is provided, it attempts to visually mount a displayid
    * If target is a creature and 'target' arg is provided, attempts to visually mount this creature's likeness

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_14004_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_14009_01_characters_new_ticket_system_sql_created"
- #define REVISION_DB_MANGOS "required_14017_01_mangos_mount_commands"
+ #define REVISION_DB_MANGOS "required_14018_01_mangos_gm_mountup_command"
 #endif // __REVISION_SQL_H__
