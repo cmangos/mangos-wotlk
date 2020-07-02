f04c3ed498a35afa3f8998a60551246b7c7716f2
killerwife <killerwife@gmail.com>
2020-07-02 11:10:22 +0200

    Arena: Implement arena point to honor rollover at 4x amount

--- src/game/BattleGround/BattleGroundMgr.cpp
+++ src/game/BattleGround/BattleGroundMgr.cpp
@@ -2256,6 +2256,8 @@ void BattleGroundMgr::RewardArenaSeason(uint32 seasonId)
         player->SetTitle(titleEntries[3], true);
         player->SetTitle(titleEntries[4], true);
         player->SaveTitles();
+        player->ModifyHonorPoints(player->GetArenaPoints() * 4);
+        player->SetArenaPoints(0);
     });
 
     /* TODO: Fix removal of old titles in wotlk due to 6x uint32 values
@@ -2269,6 +2271,8 @@ void BattleGroundMgr::RewardArenaSeason(uint32 seasonId)
         "CONCAT(SUBSTRING_INDEX(knownTitles, ' ', 1), ' ', CAST(TRIM(SUBSTR(knownTitles, LOCATE(' ', knownTitles)))  AS UNSIGNED) &~0x00000001)"
         "WHERE(CAST(TRIM(SUBSTR(knownTitles, LOCATE(' ', knownTitles)))  AS UNSIGNED) & 0x00000001) != 0");
     */
+
+    CharacterDatabase.PExecute("UPDATE characters SET totalHonorPoints=4*arenaPoints,arenaPoints=0");
 
     for (auto& data : playerRanks)
     {
