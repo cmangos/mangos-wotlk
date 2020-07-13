addcc0f7516519ee9e4ee63b452e48abdb7f772e
killerwife <killerwife@gmail.com>
2020-07-13 11:36:12 +0200

    Remove KillAllEvents call from uncharm which causes memory leaks

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -13214,7 +13214,8 @@ void Unit::Uncharm(Unit* charmed, uint32 spellId)
             charmedCreature->ClearTemporaryFaction();
 
             charmed->AttackStop(true, true);
-            charmed->m_events.KillAllEvents(true);
+            charmed->InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
+            charmed->InterruptSpell(CURRENT_MELEE_SPELL);
 
             charmInfo->ResetCharmState();
             charmed->DeleteCharmInfo();
@@ -13235,7 +13236,6 @@ void Unit::Uncharm(Unit* charmed, uint32 spellId)
             for (auto attacker : friendlyTargets)
             {
                 attacker->AttackStop(true, true);
-                attacker->m_events.KillAllEvents(true);
                 attacker->getThreatManager().modifyThreatPercent(charmed, -101);     // only remove the possessed creature from threat list because it can be filled by other players
                 attacker->AddThreat(this);
             }
