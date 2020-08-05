9f5bd5e2a0b5848326ebfe20e6af848303822623
killerwife <killerwife@gmail.com>
2020-08-05 13:04:32 +0200

    Vmap: Search upward for finding height if below is nothing

--- src/game/Vmap/MapTree.cpp
+++ src/game/Vmap/MapTree.cpp
@@ -208,12 +208,22 @@ namespace VMAP
 
     float StaticMapTree::getHeight(Vector3 const& pPos, float maxSearchDist) const
     {
-        Vector3 dir = Vector3(0, 0, -1);
-        G3D::Ray ray(pPos, dir);   // direction with length of 1
-        float maxDist = maxSearchDist;
+        float height = G3D::inf();
+        Vector3 dir;
+        if (maxSearchDist >= 0.f)
+            dir = Vector3(0, 0, -1);
+        else
+            dir = Vector3(0, 0, 1);
+        G3D::Ray ray(pPos, dir); // direction with length of 1
+        float maxDist = std::abs(maxSearchDist);
         if (getIntersectionTime(ray, maxDist))
-            return pPos.z - maxDist;
-        return G3D::inf();
+        {
+            if (maxSearchDist >= 0.f)
+                height = pPos.z - maxDist;
+            else
+                height = pPos.z + maxDist;
+        }
+        return height;
     }
 
     //=========================================================
