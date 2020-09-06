89c387518bbe20b6964b192cfca713d2b7c9d582
killerwife <killerwife@gmail.com>
2020-09-06 13:53:20 +0200

    Make Infusion use proper caster for triggered spell

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -2262,6 +2262,7 @@ void Aura::TriggerSpell()
                 break;
             }
             case 38652:                                     // Spore Cloud
+            case 40105:                                     // Infusion
             {
                 triggerCaster = GetCaster();
                 break;
