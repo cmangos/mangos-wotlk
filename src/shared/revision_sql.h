36e3c79e14c26cdc6ba401555f4ffd7e8ab179da
cala <cala@users.noreply.github.com>
2020-09-09 21:49:24 +0200

    [14021] Fix console errors when building with AH Bot disabled
    * Fix console errors when building with AHBot disabled by making AHBot commands documenation optional. They are now in a separate file that need to be added in addition to regular DB install.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_14004_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_14020_01_characters_ahbot_items"
- #define REVISION_DB_MANGOS "required_14019_01_mangos_ability_sets"
+ #define REVISION_DB_MANGOS "required_14021_01_mangos_command_ahbot"
 #endif // __REVISION_SQL_H__
