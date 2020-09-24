0e8e62e0fe9dc870a789704acf063587a4ec8b92
Xfurry <xfurry.cmangos@outlook.com>
2020-09-24 20:58:31 +0200

    [Achievements] Add definition for timed achievement types
    This will define when to start specific achievements depending on type
    
    Actual logic to follow up soon
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Server/DBCEnums.h
+++ src/game/Server/DBCEnums.h
@@ -110,6 +110,16 @@ enum AchievementCriteriaCompletionFlags
     ACHIEVEMENT_CRITERIA_FLAG_IS_ACHIEVEMENT_ID = 0x00000040,
 };
 
+enum AchievementCriteriaTimedTypes
+{
+    ACHIEVEMENT_TIMED_TYPE_EVENT            = 1,            // Timer is started by internal event with id in timerStartEvent
+    ACHIEVEMENT_TIMED_TYPE_QUEST            = 2,            // Timer is started by accepting quest with entry in timerStartEvent
+    ACHIEVEMENT_TIMED_TYPE_SPELL_CASTER     = 5,            // Timer is started by casting a spell with entry in timerStartEvent
+    ACHIEVEMENT_TIMED_TYPE_SPELL_TARGET     = 6,            // Timer is started by being target of spell with entry in timerStartEvent
+    ACHIEVEMENT_TIMED_TYPE_CREATURE         = 7,            // Timer is started by killing creature with entry in timerStartEvent
+    ACHIEVEMENT_TIMED_TYPE_ITEM             = 9,            // Timer is started by using item with entry in timerStartEvent
+};
+
 enum AchievementCriteriaTypes
 {
     ACHIEVEMENT_CRITERIA_TYPE_KILL_CREATURE = 0,
--- src/game/Server/DBCStructure.h
+++ src/game/Server/DBCStructure.h
@@ -537,7 +537,7 @@ struct AchievementCriteriaEntry
     char*  name[16];                                        // 9-24     m_description_lang
     // uint32 name_flags;                                   // 25
     uint32  completionFlag;                                 // 26       m_flags
-    // uint32  timedCriteriaStartType;                      // 27       m_timer_start_event Only appears with timed achievements, seems to be the type of starting a timed Achievement, only type 1 and some of type 6 need manual starting: 1: ByEventId(?) (serverside IDs), 2: ByQuestId, 5: ByCastSpellId(?), 6: BySpellIdTarget(some of these are unknown spells, some not, some maybe spells), 7: ByKillNpcId,  9: ByUseItemId
+    uint32  timedCriteriaStartType;                         // 27       m_timer_start_event Only appears with timed achievements, seems to be the type of starting a timed Achievement, only type 1 and some of type 6 need manual starting: 1: ByEventId(?) (serverside IDs), 2: ByQuestId, 5: ByCastSpellId(?), 6: BySpellIdTarget(some of these are unknown spells, some not, some maybe spells), 7: ByKillNpcId,  9: ByUseItemId
     uint32  timedCriteriaMiscId;                            // 28       m_timer_asset_id Alway appears with timed events, used internally to start the achievement, store
     uint32  timeLimit;                                      // 29       m_timer_time
     uint32  showOrder;                                      // 30       m_ui_order also used in achievement shift-links as index in state bitmask
