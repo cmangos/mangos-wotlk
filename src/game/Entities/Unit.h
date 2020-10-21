ea9fcc07ef8a16d8d55d529b59f8ba80a9055495
killerwife <killerwife@gmail.com>
2020-10-21 11:58:31 +0200

    Add UpdateSplinePosition in order to combat rubberbanding on sending stop spline
    First trial.

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2655,6 +2655,7 @@ class Unit : public WorldObject
         uint32 GetDamageDoneByOthers() { return m_damageByOthers; }
         uint32 GetModifierXpBasedOnDamageReceived(uint32 xp);
 
+        void UpdateSplinePosition();
     protected:
 
         struct WeaponDamageInfo
