0b67be34ab4fd1d3d7910608e905829b5b0a2a0d
killerwife <killerwife@gmail.com>
2020-12-19 12:25:22 +0100

    Transport: Fix UpdateForMap condition

--- src/game/Entities/Transports.cpp
+++ src/game/Entities/Transports.cpp
@@ -742,7 +742,7 @@ void Transport::UpdateForMap(Map const* targetMap, bool newMap)
     if (pl.isEmpty())
         return;
 
-    if (!newMap)
+    if (newMap)
     {
         for (const auto& itr : pl)
         {
