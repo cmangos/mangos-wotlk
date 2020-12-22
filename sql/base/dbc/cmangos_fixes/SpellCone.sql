3a5ef8f741e829d8c9b71bccab9109dbe410a7cb
Xfurry <xfurry.cmangos@outlook.com>
2020-12-22 22:47:02 +0100

    Spell/Cone: Fix cone orientation for spell 74117
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- sql/base/dbc/cmangos_fixes/SpellCone.sql
+++ sql/base/dbc/cmangos_fixes/SpellCone.sql
@@ -1,4 +1,5 @@
 -- romulo hits targets in the back and does not turn at all
-UPDATE spell_cone SET ConeDegrees=-180 WHERE id IN(30815);
+-- Pain and Suffering: Lich King hits targets behind him: 74117
+UPDATE spell_cone SET ConeDegrees=-180 WHERE id IN(30815,74117);
 DELETE FROM spell_cone WHERE id IN(48562); -- reused in Cataclysm for a different purpose
 
