7dc4112c1b9fc6e10f3a64bc1c7551a3548e5b60
MantisLord <sabinprosper@gmail.com>
2021-01-02 09:56:49 -0500

    Add missing default return value for ABEffect000 spell script

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/scarlet_monastery/instance_scarlet_monastery.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/scarlet_monastery/instance_scarlet_monastery.cpp
@@ -205,6 +205,7 @@ struct ABEffect000 : public SpellScript
         if (WorldObject* caster = spell->GetCastingObject())
             if (!target->IsWithinLOSInMap(caster, true))
                 return false;
+        return true;
     }
 };
 
