f7d423d78f3757c9a3a851f2cde384326e9e0a88
killerwife <killerwife@gmail.com>
2021-01-06 19:48:51 +0100

    Transports: Fix crash due to adding passenger and then erroring it out

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -2124,8 +2124,12 @@ GameObject* WorldObject::SpawnGameObject(uint32 dbGuid, Map* map, GenericTranspo
 {
     GameObjectData const* data = sObjectMgr.GetGOData(dbGuid);
     MANGOS_ASSERT(data);
+
+    if (data->spawnMask && !map->CanSpawn(TYPEID_GAMEOBJECT, dbGuid))
+        return nullptr;
+
     GameObject* gameobject = GameObject::CreateGameObject(data->id);
-    if (!gameobject->LoadFromDB(dbGuid, map, map->GenerateLocalLowGuid(HIGHGUID_GAMEOBJECT), transport) || (data->spawnMask && !map->CanSpawn(TYPEID_GAMEOBJECT, dbGuid)))
+    if (!gameobject->LoadFromDB(dbGuid, map, map->GenerateLocalLowGuid(HIGHGUID_GAMEOBJECT), transport))
     {
         delete gameobject;
         return nullptr;
@@ -2150,9 +2154,12 @@ Creature* WorldObject::SpawnCreature(uint32 dbGuid, Map* map, GenericTransport* 
         return nullptr;
     }
 
+    if ((data->spawnMask && !map->CanSpawn(TYPEID_UNIT, dbGuid)))
+        return nullptr;
+
     Creature* creature = new Creature;
     // DEBUG_LOG("Spawning creature %u",*itr);
-    if (!creature->LoadFromDB(dbGuid, map, map->GenerateLocalLowGuid(cinfo->GetHighGuid()), transport) || (data->spawnMask && !map->CanSpawn(TYPEID_UNIT, dbGuid)))
+    if (!creature->LoadFromDB(dbGuid, map, map->GenerateLocalLowGuid(cinfo->GetHighGuid()), transport))
     {
         delete creature;
         return nullptr;
