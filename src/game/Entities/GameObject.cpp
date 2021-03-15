f915ad48d8910ba8e0f646895b231d53c7d6fb80
killerwife <killerwife@gmail.com>
2021-03-15 20:17:13 +0100

    DynGuid: Add forgotten addition of dbguid GOs to map store

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -121,7 +121,11 @@ void GameObject::AddToWorld()
 {
     ///- Register the gameobject for guid lookup
     if (!IsInWorld())
+    {
         GetMap()->GetObjectsStore().insert<GameObject>(GetObjectGuid(), (GameObject*)this);
+        if (GetDbGuid())
+            GetMap()->AddDbGuidObject(this);
+    }
 
     if (m_model)
         GetMap()->InsertGameObjectModel(*m_model);
@@ -173,6 +177,8 @@ void GameObject::RemoveFromWorld()
             GetMap()->RemoveGameObjectModel(*m_model);
 
         GetMap()->GetObjectsStore().erase<GameObject>(GetObjectGuid(), (GameObject*)nullptr);
+        if (GetDbGuid())
+            GetMap()->RemoveDbGuidObject(this);
     }
 
     WorldObject::RemoveFromWorld();
