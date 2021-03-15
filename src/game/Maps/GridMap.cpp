2204e5e45df4020f0480de14b07cb247346d48b7
killerwife <killerwife@gmail.com>
2021-03-15 11:15:46 +0100

    Terrain/Vmap: Fix another who entry check for outdoors

--- src/game/Maps/GridMap.cpp
+++ src/game/Maps/GridMap.cpp
@@ -961,7 +961,7 @@ uint16 TerrainInfo::GetAreaFlag(float x, float y, float z, bool* isOutdoors) con
 {
     uint32 mogpFlags;
     int32 adtId, rootId, groupId;
-    WMOAreaTableEntry const* wmoEntry = nullptr;
+    WMOAreaTableEntry const* foundWmoEntry = nullptr;
     AreaTableEntry const* atEntry = nullptr;
     bool haveAreaInfo = false;
 
@@ -975,7 +975,7 @@ uint16 TerrainInfo::GetAreaFlag(float x, float y, float z, bool* isOutdoors) con
             if (areaEntry && areaEntry->mapid == GetMapId())
             {
                 atEntry = areaEntry;
-                wmoEntry = wmoEntry;
+                foundWmoEntry = wmoEntry;
             }
         }
     }
@@ -995,7 +995,7 @@ uint16 TerrainInfo::GetAreaFlag(float x, float y, float z, bool* isOutdoors) con
     if (isOutdoors)
     {
         if (haveAreaInfo)
-            *isOutdoors = IsOutdoorWMO(mogpFlags, wmoEntry, atEntry);
+            *isOutdoors = IsOutdoorWMO(mogpFlags, foundWmoEntry, atEntry);
         else
             *isOutdoors = true;
     }
