0236d2d7001e3fdabdf9ab607cbc35b99e76e5a9
Cyberium <cyberium@users.noreply.github.com>
2021-03-17 23:08:34 +0100

    [14033] Fix commands error and security override
    Did a quick change on some command access level.
    If some of you prefer to change some level please do it in core and in db.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_14028_01_realmd_account_locale_agnostic"
  #define REVISION_DB_CHARACTERS "required_14030_01_characters_item_instance_duration_default"
- #define REVISION_DB_MANGOS "required_14032_01_mangos_dbscript_npc_flag_update"
+ #define REVISION_DB_MANGOS "required_14033_01_mangos_command"
 #endif // __REVISION_SQL_H__
