3b5027f5d3b65969dc513b97b2584ceea348ff88
killerwife <killerwife@gmail.com>
2021-04-11 21:33:33 +0200

    UnitAI: Add cataclysm breath to turning prototype for instant spells

--- src/game/AI/BaseAI/UnitAI.cpp
+++ src/game/AI/BaseAI/UnitAI.cpp
@@ -361,7 +361,7 @@ void UnitAI::OnSpellCastStateChange(Spell const* spell, bool state, WorldObject*
     }
     else
     {
-        std::set<uint32> spellIdsForTurning = { 31306, 33813, 38739 };
+        std::set<uint32> spellIdsForTurning = { 31306, 33813, 38739, 44811, 46292 };
         if (!spell->GetCastTime() && spellIdsForTurning.find(spellInfo->Id) != spellIdsForTurning.end())
         {
             HandleDelayedInstantAnimation(spellInfo);
