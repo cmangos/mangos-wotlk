1c9682f6d2a67ded9a288525183c8ea7e26e8018
killerwife <killerwife@gmail.com>
2021-04-13 17:39:14 +0200

    Spell: Adjust chain and persistent area effects to not use combat reach

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -8835,7 +8835,7 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellTargetFilterScheme sc
 
             while (chainTargetCount && next != filterUnitList.end())
             {
-                if (!prev->IsWithinDist(*next, m_jumpRadius))
+                if (!prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
                     break;
 
                 if (!prev->IsWithinLOSInMap(*next, true))
@@ -8863,14 +8863,14 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellTargetFilterScheme sc
             newList.push_back(unitTarget);
             bool isInGroup = m_caster->IsInGroup(unitTarget);
             filterUnitList.pop_front();
-            filterUnitList.sort(LowestHPNearestOrder(unitTarget, DIST_CALC_COMBAT_REACH));
+            filterUnitList.sort(LowestHPNearestOrder(unitTarget, DIST_CALC_NONE));
             Unit* prev = unitTarget;
             UnitList::iterator next = filterUnitList.begin();
             chainTargetCount -= 1; // unit target is one
 
             while (chainTargetCount && next != filterUnitList.end())
             {
-                if (!prev->IsWithinCombatDist(*next, m_jumpRadius))
+                if (!prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
                 {
                     ++next;
                     continue;
