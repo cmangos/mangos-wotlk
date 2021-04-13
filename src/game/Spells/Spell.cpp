554d143397f6068473207f448bbb1633540d9e6b
killerwife <killerwife@gmail.com>
2021-04-14 01:55:03 +0200

    Spell: Fix mistakes in chain jump eligibility calculation

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -8835,7 +8835,7 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellTargetFilterScheme sc
 
             while (chainTargetCount && next != filterUnitList.end())
             {
-                if (!prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
+                if (prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
                     break;
 
                 if (!prev->IsWithinLOSInMap(*next, true))
@@ -8863,18 +8863,15 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellTargetFilterScheme sc
             newList.push_back(unitTarget);
             bool isInGroup = m_caster->IsInGroup(unitTarget);
             filterUnitList.pop_front();
-            filterUnitList.sort(LowestHPNearestOrder(unitTarget, DIST_CALC_NONE));
+            filterUnitList.sort(LowestHPNearestOrder(unitTarget));
             Unit* prev = unitTarget;
             UnitList::iterator next = filterUnitList.begin();
             chainTargetCount -= 1; // unit target is one
 
             while (chainTargetCount && next != filterUnitList.end())
             {
-                if (!prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
-                {
-                    ++next;
-                    continue;
-                }
+                if (prev->GetDistance(*next, true, DIST_CALC_NONE) > m_jumpRadius * m_jumpRadius)
+                    break;
 
                 if (!prev->IsWithinLOSInMap(*next, true))
                 {
@@ -8891,7 +8888,7 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellTargetFilterScheme sc
                 prev = *next;
                 newList.push_back(prev);
                 filterUnitList.erase(next);
-                filterUnitList.sort(LowestHPNearestOrder(prev, DIST_CALC_COMBAT_REACH));
+                filterUnitList.sort(LowestHPNearestOrder(prev));
                 next = filterUnitList.begin();
 
                 --chainTargetCount;
