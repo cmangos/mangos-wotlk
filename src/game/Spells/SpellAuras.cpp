794f26704c81a39e5651ddd9da19901552c2acde
anonxs <anonxs@web.de>
2021-07-02 23:48:07 +0200

    [Spell] Add some perm procc auras to IsSpellRemovedOnEvade / HandleAuraModIncreaseHealth

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -7080,6 +7080,7 @@ void Aura::HandleAuraModIncreaseHealth(bool apply, bool Real)
         // no break here
 
         // Cases where m_amount already has the correct value (spells cast with CastCustomSpell or absolute values)
+        case 4148:                                          // Growl of Fortitude
         case 12976:                                         // Warrior Last Stand triggered spell (Cast with percentage-value by CastCustomSpell)
         case 16609:                                         // Warchief's Blessing
         case 28506:                                         // Potion of Heroes (i.22837 - Heroic Potion)
