acaa73b7497b6e8301e1e9891ef75706fb042df5
killerwife <killerwife@gmail.com>
2021-08-25 23:56:02 +0200

    SWP: Add forgotten instance script loader call

--- src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
+++ src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
@@ -123,6 +123,7 @@ extern void AddSC_boss_kalecgos();
 extern void AddSC_boss_kiljaeden();
 extern void AddSC_boss_muru();
 extern void AddSC_instance_sunwell_plateau();
+extern void AddSC_sunwell_plateau();
 extern void AddSC_boss_archaedas();                         // uldaman
 extern void AddSC_instance_uldaman();
 extern void AddSC_uldaman();
@@ -618,6 +619,7 @@ void AddScripts()
     AddSC_boss_kiljaeden();
     AddSC_boss_muru();
     AddSC_instance_sunwell_plateau();
+    AddSC_sunwell_plateau();
     AddSC_boss_archaedas();                                 // uldaman
     AddSC_instance_uldaman();
     AddSC_uldaman();
