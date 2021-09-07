c65ddbb67924245a79e8557ab67deac0f20da6ec
Jochen Oonincx <jochenoonincx@shadow-corporation.com>
2021-09-07 18:47:34 +0200

    Fixed unexpected ; on 4738. Changed it into a ,
    Signed-off-by: Jochen Oonincx <jochenoonincx@shadow-corporation.com>

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -4735,7 +4735,7 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,broadc
 ('-1540048','Cowards! You''ll never pull me into the shadows!','0','1','0','0','0','kargath SAY_EVADE'),
 
 ('-1540049','The Alliance dares to intrude this far into my fortress? Bring out the Honor Hold prisoners and call for the executioner! They''ll pay with their lives for this trespass!','0','6','0','0','0','kargath SAY_EXECUTE_ALLY'),
-('-1540050','It looks like we have a ranking officer among our captives...how amusing. Execute the green-skinned dog at once!','0','6','0','0','0','kargath SAY_EXECUTE_HORDE');
+('-1540050','It looks like we have a ranking officer among our captives...how amusing. Execute the green-skinned dog at once!','0','6','0','0','0','kargath SAY_EXECUTE_HORDE'),
 
 ('-1540051','Invaders have breached the defenses!','10285','6','0','0','14309','Gauntlet of fire yell 1'),
 ('-1540052','Archers, form ranks! On my mark!','10156','6','0','0','14310','Gauntlet of fire yell 2'),
