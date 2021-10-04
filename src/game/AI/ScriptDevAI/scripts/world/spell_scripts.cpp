400df860f42dc035593d6c460d46cbbc01faf044
anonxs <anonxs@web.de>
2021-10-04 20:31:12 +0200

    [Spell] Add spell_effect_summon_no_follow_movement for spells summoning nonfollower npcs for classic & tbc

--- src/game/AI/ScriptDevAI/scripts/world/spell_scripts.cpp
+++ src/game/AI/ScriptDevAI/scripts/world/spell_scripts.cpp
@@ -1341,6 +1341,14 @@ struct Drink : public DrinkAnimation
     }
 };
 
+struct spell_effect_summon_no_follow_movement : public SpellScript
+{
+    void OnSummon(Spell* spell, Creature* summon) const override
+    {
+        summon->AI()->SetFollowMovement(false);
+    }
+};
+
 void AddSC_spell_scripts()
 {
     Script* pNewScript = new Script;
@@ -1373,6 +1381,7 @@ void AddSC_spell_scripts()
     RegisterAuraScript<FoodAnimation>("spell_food_animation");
     RegisterAuraScript<DrinkAnimation>("spell_drink_animation");
     RegisterAuraScript<Drink>("spell_drink");
+    RegisterSpellScript<spell_effect_summon_no_follow_movement>("spell_effect_summon_no_follow_movement");
 
     // wotlk section
     RegisterSpellScript<Replenishment>("spell_replenishment");
