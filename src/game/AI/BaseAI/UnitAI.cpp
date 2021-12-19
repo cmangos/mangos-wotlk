1e0fc6aef2a92b934aaf52a8d3b2160f42dc323d
killerwife <killerwife@gmail.com>
2021-12-19 15:32:40 +0100

    SpellList: Fix crash due to wrong variable use

--- src/game/AI/BaseAI/UnitAI.cpp
+++ src/game/AI/BaseAI/UnitAI.cpp
@@ -1257,7 +1257,7 @@ void UnitAI::AddInitialCooldowns()
         uint32 cooldown = urand(data.second.InitialMin, data.second.InitialMax);
         if (cooldown)
         {
-            SpellEntry const* spellInfo = sSpellTemplate.LookupEntry<SpellEntry>(m_mainSpellId);
+            SpellEntry const* spellInfo = sSpellTemplate.LookupEntry<SpellEntry>(data.second.SpellId);
             m_unit->AddCooldown(*spellInfo, nullptr, false, cooldown);
         }
     }
