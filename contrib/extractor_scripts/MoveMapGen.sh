fc5a41e3b28a7dc04080b5b45d19f81eb42af47d
Yury <rip95_95@mail.ru>
2022-01-02 14:52:36 +0100

    MMap generator improvement
    Now mmap generator should use use all thread assigned (by default all available) from start to end.
    
    Some code from @deiteris and his PR
    I kept his authorship as nothing would be done without his work even if i heavly modified his code.
    
    - Implmented task queue
    - Improved output text
    - Removed map id from sh file
    
    After analyse i found that output files was not binary compatible with old version of the generator. Hopefully only PolyRef was impacted.
    In fact only salt part of the polyref is different and the reason is we use here a copy of original navmesh for each tile insted of same navmesh for entire map.
    Old behaviour modify salt value each time removeTile was called (at end of building tile).
    I haven't found any impact for path finding. All tiles are connected like before and none of my test failed.
    
    close https://github.com/cmangos/mangos-classic/pull/448

--- contrib/extractor_scripts/MoveMapGen.sh
+++ contrib/extractor_scripts/MoveMapGen.sh
@@ -11,16 +11,14 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 ## Syntax of this helper
-## First param must be number of to be used CPUs (only 1, 2, 4, 8 supported) or "offmesh" to recreate the special tiles from the OFFMESH_FILE
-## Second param can be an additional filename for storing log
-## Third param can be an addition filename for storing detailed log
+
+## 1st param must be "maps" to generate the maps and offmesh or "offmesh" to recreate the special tiles from the OFFMESH_FILE
+## 2nd param may be an additional filename for storing log
+## 3rd param may be an addition filename for storing detailed log
+## 4th param may be a number of threads to use for maps processing
 
 ## Additional Parameters to be forwarded to MoveMapGen, see mmaps/readme for instructions
 PARAMS="--silent --configInputPath config.json"
-
-## Already a few map extracted, and don't care anymore
-EXCLUDE_MAPS=""
-#EXCLUDE_MAPS="0 1 530 571" # example to exclude the continents
 
 ## Offmesh file
 OFFMESH_FILE="offmesh.txt"
@@ -30,43 +28,33 @@ LOG_FILE="MoveMapGen.log"
 ## Detailed log file
 DETAIL_LOG_FILE="MoveMapGen_detailed.log"
 
-## ! Use below only for finetuning or if you know what you are doing !
-
-## All maps
-LIST_A="571"
-LIST_B="1"
-LIST_C="530"
-LIST_D="0"
-LIST_E="169 531 409 545 604 542 44 109 389 565 450 617 616 592 582 489"
-LIST_F="603 631 574 560 269 602 309 585 566 469 25 615 369 230 649 576 547 555 48 35 129 558 557 249 449 588 586 608"
-LIST_G="607 533 509 543 30 600 532 650 553 43 554 34"
-LIST_H="619 628 723 609 564 37 534 595 209 568 529 573 658 47 349 229 546 189 552 540 598 556 601 544 593 42 451 587"
-LIST_I="591 548 33"
-LIST_J="594 572 562 329"
-LIST_K="596 580 429 36 13"
-LIST_L="289 724 559 599 668 90 550 70 632 624 618 590 584 589 578 575"
-
 badParam()
 {
  echo "ERROR! Bad arguments!"
  echo "You can (re)extract mmaps with this helper script,"
  echo "or recreate only the tiles from the offmash file"
  echo
- echo "Call with number of processes (1,2,4,8) to create mmaps"
+ echo "Call with 'maps' to create mmaps"
  echo "Call with 'offmesh' to reextract the tiles from offmash file"
  echo "Example: ./MoveMapGen.sh 8"
  echo
- echo "For further fine-tuning edit this helper script"
- echo
 }
 
-if [ "$#" = "3" ]
+if [ "$#" = "4" ]
 then
- LOG_FILE=$2
- DETAIL_LOG_FILE=$3
+  LOG_FILE=$2
+  DETAIL_LOG_FILE=$3
+  if [ "$4" != "all" ]
+  then
+    PARAMS="${PARAMS} --threads $4"
+  fi
+elif [ "$#" = "3" ]
+then
+  LOG_FILE=$2
+  DETAIL_LOG_FILE=$3
 elif [ "$#" = "2" ]
 then
- LOG_FILE=$2
+  LOG_FILE=$2
 fi
 
 # Offmesh file provided?
@@ -83,23 +71,6 @@ then
  fi
 fi
 
-# Function to process a list
-createMMaps()
-{
- for i in $@
- do
-   for j in $EXCLUDE_MAPS
-   do
-     if [ "$i" = "$j" ]
-     then
-       continue 2
-     fi
-   done
-   ./MoveMapGen $PARAMS $OFFMESH $i | tee -a $DETAIL_LOG_FILE
-   echo "`date`: (Re)created map $i" | tee -a $LOG_FILE
- done
-}
-
 createHeader()
 {
  echo "`date`: Start creating MoveMaps" | tee -a $LOG_FILE
@@ -107,7 +78,7 @@ createHeader()
  echo "Detailed log can be found in $DETAIL_LOG_FILE" | tee -a $LOG_FILE
  echo "Start creating MoveMaps" | tee -a $DETAIL_LOG_FILE
  echo
- echo "Be PATIENT - This will take a long time and might also have gaps between visible changes on the console."
+ echo "Be PATIENT - This may take a long time with small number of threads and might also have gaps between visible changes on the console."
  echo "WAIT until you are informed that 'creating MoveMaps' is 'finished'!"
 }
 
@@ -119,40 +90,13 @@ fi
 
 # Param control
 case "$1" in
- "1" )
-   createHeader $1
-   createMMaps $LIST_A $LIST_B $LIST_C $LIST_D $LIST_E $LIST_F $LIST_G $LIST_H $LIST_I $LIST_J $LIST_K $LIST_L &
-   ./MoveMapGen $PARAMS $OFFMESH --onlyGO
-   ;;
- "2" )
-   createHeader $1
-   createMMaps $LIST_A $LIST_C $LIST_E $LIST_G $LIST_J $LIST_L &
-   createMMaps $LIST_B $LIST_D $LIST_F $LIST_H $LIST_I $LIST_K &
-   ./MoveMapGen $PARAMS $OFFMESH --onlyGO
-   ;;
- "4" )
-   createHeader $1
-   createMMaps $LIST_A $LIST_E &
-   createMMaps $LIST_B $LIST_F $LIST_I &
-   createMMaps $LIST_C $LIST_G $LIST_J $LIST_L &
-   createMMaps $LIST_D $LIST_H $LIST_K &
-   ./MoveMapGen $PARAMS $OFFMESH --onlyGO
-   ;;
- "8" )
-   createHeader $1
-   createMMaps $LIST_A &
-   createMMaps $LIST_B &
-   createMMaps $LIST_C &
-   createMMaps $LIST_D &
-   createMMaps $LIST_E $LIST_I $LIST_J $LIST_K &
-   createMMaps $LIST_F &
-   createMMaps $LIST_G $LIST_L &
-   createMMaps $LIST_H &
-   ./MoveMapGen $PARAMS $OFFMESH --onlyGO
+ "maps" )
+    createHeader
+    ./MoveMapGen $PARAMS $OFFMESH --buildGameObjects | tee -a $DETAIL_LOG_FILE
    ;;
  "offmesh" )
-   echo "`date`: Recreate offmeshs from file $OFFMESH_FILE" | tee -a $LOG_FILE
-   echo "Recreate offmeshs from file $OFFMESH_FILE" | tee -a $DETAIL_LOG_FILE
+   echo "`date`: Recreate offmeshes from file $OFFMESH_FILE" | tee -a $LOG_FILE
+   echo "Recreate offmeshes from file $OFFMESH_FILE" | tee -a $DETAIL_LOG_FILE
    while read map tile line
    do
      ./MoveMapGen $PARAMS $OFFMESH $map --tile $tile | tee -a $DETAIL_LOG_FILE
