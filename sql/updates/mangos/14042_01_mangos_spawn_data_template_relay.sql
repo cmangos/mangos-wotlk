13f42f1da4710cc6443e0774875d451abade3683
killerwife <killerwife@gmail.com>
2022-01-02 16:49:40 +0100

    Add default value retroactively to creature_spawn_data_template RelayId

--- sql/updates/mangos/14042_01_mangos_spawn_data_template_relay.sql
+++ sql/updates/mangos/14042_01_mangos_spawn_data_template_relay.sql
@@ -1,4 +1,4 @@
 ALTER TABLE db_version CHANGE COLUMN required_14041_01_mangos_creature_spell_lists required_14042_01_mangos_spawn_data_template_relay bit;
 
-ALTER TABLE creature_spawn_data_template ADD COLUMN RelayId INT UNSIGNED NOT NULL COMMENT 'dbscripts_on_relay' AFTER SpawnFlags;
+ALTER TABLE creature_spawn_data_template ADD COLUMN RelayId INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'dbscripts_on_relay' AFTER SpawnFlags;
 
