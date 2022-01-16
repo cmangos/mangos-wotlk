bf6454e2db9b4afdb230235ab102f395012a1b1c
Cyberium <cyberium@users.noreply.github.com>
2022-01-16 11:52:09 +0100

    [Formation] Make direction change a bit soften for formation followers

--- src/game/MotionGenerators/TargetedMovementGenerator.h
+++ src/game/MotionGenerators/TargetedMovementGenerator.h
@@ -264,6 +264,7 @@ class FormationMovementGenerator : public FollowMovementGenerator
         bool HandleMasterDistanceCheck(Unit& owner, const uint32& time_diff);
         FormationSlotDataSPtr m_slot;
         bool m_headingToMaster;
+        float m_lastAngle;
 };
 
 #endif
