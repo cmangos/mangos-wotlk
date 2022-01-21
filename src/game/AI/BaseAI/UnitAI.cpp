50d624789e355c3c1063cf3ffadc4510e516712d
killerwife <killerwife@gmail.com>
2022-01-22 00:55:03 +0100

    ZG: Reimplement Arlokk using spellscript, spell lists and combatai

--- src/game/AI/BaseAI/UnitAI.cpp
+++ src/game/AI/BaseAI/UnitAI.cpp
@@ -1214,7 +1214,7 @@ void UnitAI::UpdateSpellLists()
                 if (castResult == CAST_OK)
                 {
                     success = true;
-                    OnSpellCast(sSpellTemplate.LookupEntry<SpellEntry>(spellId));
+                    OnSpellCast(sSpellTemplate.LookupEntry<SpellEntry>(spellId), target);
                     if (scriptId)
                         m_unit->GetMap()->ScriptsStart(sRelayScripts, scriptId, m_unit, target);
                     break;
