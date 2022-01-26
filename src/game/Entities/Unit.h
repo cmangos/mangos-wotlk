8ce89da8c918484f04e4c47607f6205553e1e55a
killerwife <killerwife@gmail.com>
2022-01-26 11:56:57 +0100

    Unit/Spell: Add caching for heartbeat proc to optimize calling procs in heartbeat
    Calling it every heartbeat is expensive AF

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2693,6 +2693,7 @@ class Unit : public WorldObject
         // Need to safeguard aura proccing in Unit::ProcDamageAndSpell
         bool m_spellProcsHappening;
         std::vector<SpellAuraHolder*> m_delayedSpellAuraHolders;
+        uint32 m_hasHeartbeatProcCounter;
 
         bool m_alwaysHit;
         bool m_noThreat;
