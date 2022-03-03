2cd2793ed5202cad952ab8915506776288637a1c
Nytanath <nytanath@protonmail.com>
2022-03-03 14:26:31 +0100

    Implement command .list areatriggers to list all areatriggers within the same map (if instanceable) or area (if continent) as the user. Closes https://github.com/cmangos/mangos-classic/pull/479

--- src/game/Tools/Language.h
+++ src/game/Tools/Language.h
@@ -588,6 +588,7 @@ enum MangosStrings
     LANG_YOURS_EXPLORE_SET_NOTHING      = 554,
 
     LANG_NPCINFO_PHASEMASK              = 555,
+
     //                                    556,              // not used
     LANG_YOURS_LEVEL_UP                 = 557,
     LANG_YOURS_LEVEL_DOWN               = 558,
@@ -606,7 +607,8 @@ enum MangosStrings
     LANG_GET_UINT_FIELD                 = 570,
     LANG_GET_FLOAT                      = 571,              // log
     LANG_GET_FLOAT_FIELD                = 572,
-    //                                    573,              // not used
+
+    LANG_AREATRIGGER_LIST               = 573,
     //                                    574,              // not used
     LANG_CHANGE_INT32                   = 575,              // log
     LANG_CHANGE_INT32_FIELD             = 576,
