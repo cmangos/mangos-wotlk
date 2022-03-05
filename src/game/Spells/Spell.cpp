5f6f65eca497bb25e374277873a6c1fd8cef4a91
killerwife <killerwife@gmail.com>
2022-03-05 09:26:24 +0100

    Spell: Fix safeguards in SPELL_EFFECT_CREATE_ITEM

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -5989,6 +5989,9 @@ SpellCastResult Spell::CheckCast(bool strict)
                     if (!target->IsPlayer() && m_spellInfo->EffectImplicitTargetA[i] != TARGET_UNIT_CASTER && m_spellInfo->EffectImplicitTargetB[i] != TARGET_UNIT_CASTER)
                         return SPELL_FAILED_BAD_TARGETS;
 
+                    if (!target->IsPlayer())
+                        target = m_caster;
+
                     if (i != EFFECT_INDEX_0) // TODO: Partial application
                         break;
 
