93a7cd9f672ed76160a8c2b6160e8071af67dc14
Brz <brz@nostalgeek-serveur.com>
2022-06-10 22:55:41 +0200

    fix death pact targeting returning early error

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -5605,7 +5605,8 @@ SpellCastResult Spell::CheckCast(bool strict)
                 }
             }
 
-            if ((m_targets.m_targetMask == TARGET_FLAG_SELF || m_trueCaster == target) && m_spellInfo->HasAttribute(SPELL_ATTR_EX_CANT_TARGET_SELF))
+            if ((m_targets.m_targetMask == TARGET_FLAG_SELF || m_trueCaster == target) && m_spellInfo->HasAttribute(SPELL_ATTR_EX_CANT_TARGET_SELF) 
+                && m_spellInfo->Id != 48743)        // targeting is handled later
             {
                 if (IsOnlySelfTargeting(m_spellInfo))
                     sLog.outCustomLog("Spell ID %u cast at self explicitly even though it has SPELL_ATTR_EX_CANT_TARGET_SELF", m_spellInfo->Id);
