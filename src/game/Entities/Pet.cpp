ba73ea71b1a09498350c894b6619d885e89c0f8c
killerwife <killerwife@gmail.com>
2022-07-02 20:58:24 +0200

    Pet: Fix ForcedDespawn not passing owner properly to unsummon

--- src/game/Entities/Pet.cpp
+++ src/game/Entities/Pet.cpp
@@ -2453,12 +2453,14 @@ void Pet::ForcedDespawn(uint32 timeMSToDespawn, bool onlyAlive)
     if (IsDespawned())
         return;
 
+    Unit* owner = GetOwner();
+
     if (IsAlive())
         SetDeathState(JUST_DIED);
 
     RemoveCorpse(true);                                     // force corpse removal in the same grid
 
-    Unsummon(PET_SAVE_NOT_IN_SLOT);
+    Unsummon(PET_SAVE_NOT_IN_SLOT, owner);
 }
 
 void Pet::InitializeSpellsForControllableGuardian(bool load)
