943d9b4f2b8f3fbfe908af497b163949ebb91cf1
killerwife <killerwife@gmail.com>
2022-07-27 17:55:08 +0200

    Pet: Fix missing scaling error by forcing NPC SUMMON_PET to GUARDIAN_PET

--- src/game/Entities/Pet.cpp
+++ src/game/Entities/Pet.cpp
@@ -1031,7 +1031,10 @@ void Pet::InitStatsForLevel(uint32 petlevel)
     float mana = 0.f;
     float armor = 0.f;
 
-    switch (getPetType())
+    PetType petType = getPetType();
+    if (!GetOwnerGuid().IsPlayer())
+        petType = GUARDIAN_PET; // for purpose of pet scaling, NPC summoned SUMMON_PET scale as GUARDIAN_PET
+    switch (petType)
     {
         case HUNTER_PET:
         {
