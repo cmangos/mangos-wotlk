a80998e18316c3ca555cbd3faa319f4fc73a3990
killerwife <killerwife@gmail.com>
2022-08-01 22:47:04 +0200

    Spell/AI: Implement SPELL_ATTR_EX_NO_AUTOCAST_AI

--- src/game/AI/BaseAI/UnitAI.cpp
+++ src/game/AI/BaseAI/UnitAI.cpp
@@ -1186,6 +1186,9 @@ void UnitAI::UpdateSpellLists()
     {
         CreatureSpellListSpell const& spell = data.second;
 
+        if (spell.DisabledForAI)
+            continue;
+
         if (spell.Flags & SPELL_LIST_FLAG_SUPPORT_ACTION)
             if (supportActionRoll > spells.ChanceSupportAction)
                 continue;
