805086b4b3551585a0f4da8d1106bf75710e8698
killerwife <killerwife@gmail.com>
2022-08-05 19:31:10 +0200

    Scripts: Fix targeting of dynguid GOs by dbguid

--- src/game/DBScripts/ScriptMgr.cpp
+++ src/game/DBScripts/ScriptMgr.cpp
@@ -1245,7 +1245,6 @@ bool ScriptAction::GetScriptProcessTargets(WorldObject* originalSource, WorldObj
             WorldObject* buddy = nullptr;
             if (m_script->IsCreatureBuddy())
             {
-                CreatureData const* cData = sObjectMgr.GetCreatureData(m_script->searchRadiusOrGuid);
                 buddy = m_map->GetCreature(m_script->searchRadiusOrGuid);
 
                 if (buddy && ((Creature*)buddy)->IsAlive() == m_script->IsDeadOrDespawnedBuddy())
@@ -1258,10 +1257,8 @@ bool ScriptAction::GetScriptProcessTargets(WorldObject* originalSource, WorldObj
                 }
             }
             else
-            {
-                GameObjectData const* oData = sObjectMgr.GetGOData(m_script->searchRadiusOrGuid);
-                buddy = m_map->GetGameObject(ObjectGuid(HIGHGUID_GAMEOBJECT, oData->id, m_script->searchRadiusOrGuid));
-            }
+                buddy = m_map->GetGameObject(m_script->searchRadiusOrGuid);
+
             // TODO Maybe load related grid if not already done? How to handle multi-map case?
             if (!buddy && m_script->command != SCRIPT_COMMAND_TERMINATE_SCRIPT)
             {
