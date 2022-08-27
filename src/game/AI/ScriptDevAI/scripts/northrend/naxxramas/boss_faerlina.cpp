88e4f719b8746ea4d1970aec642d67c65388d4b2
insunaa <dev.insuna@gmail.com>
2022-08-27 20:21:35 +0200

    BossAI: Convert Faerlina to BossAI+Spell Lists

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_faerlina.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_faerlina.cpp
@@ -21,23 +21,23 @@ SDComment:
 SDCategory: Naxxramas
 EndScriptData */
 
+#include "AI/ScriptDevAI/base/CombatAI.h"
 #include "AI/ScriptDevAI/include/sc_common.h"
 #include "naxxramas.h"
 
 enum
 {
-    SAY_GREET                   = -1533009,
-    SAY_AGGRO_1                 = -1533010,
-    SAY_AGGRO_2                 = -1533011,
-    SAY_AGGRO_3                 = -1533012,
-    SAY_AGGRO_4                 = -1533013,
-    SAY_SLAY_1                  = -1533014,
-    SAY_SLAY_2                  = -1533015,
-    SAY_DEATH                   = -1533016,
+    SAY_GREET                   = 12852,
+    SAY_DEATH                   = 12853,
+    SAY_SLAY_1                  = 12854,
+    SAY_SLAY_2                  = 12855,
+    SAY_AGGRO_1                 = 12856,
+    SAY_AGGRO_2                 = 12857,
+    SAY_AGGRO_3                 = 12858,
+    SAY_AGGRO_4                 = 12859,
 
-    EMOTE_BOSS_GENERIC_FRENZY   = -1000005,
-
-    // SOUND_RANDOM_AGGRO       = 8955,                     // soundId containing the 4 aggro sounds, we not using this
+    EMOTE_BOSS_GENERIC_FRENZY   = 1191,
+    EMOTE_WIDOWS_EMBRACE        = 31019,
 
     SPELL_POISONBOLT_VOLLEY     = 28796,
     SPELL_POISONBOLT_VOLLEY_H   = 54098,
@@ -47,157 +47,93 @@ enum
     SPELL_RAIN_OF_FIRE_H        = 54099,
     SPELL_WIDOWS_EMBRACE        = 28732,
     SPELL_WIDOWS_EMBRACE_H      = 54097,
+
+    FAERLINA_GRACE_TIMER        = 0,
+
+    SPELLSET_10N                = 1595301,
+    SPELLSET_25N                = 2926801,
 };
 
-struct boss_faerlinaAI : public ScriptedAI
+struct boss_faerlinaAI : public BossAI
 {
-    boss_faerlinaAI(Creature* pCreature) : ScriptedAI(pCreature)
+    boss_faerlinaAI(Creature* creature) : BossAI(creature, 0),
+    m_instance(static_cast<instance_naxxramas *>(creature->GetInstanceData())),
+    m_hasTaunted(false),
+    m_inGracePeriod(false)
     {
-        m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
-        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
-        m_bHasTaunted = false;
-        Reset();
+        SetDataType(TYPE_FAERLINA);
+        AddOnAggroText(SAY_AGGRO_1, SAY_AGGRO_2, SAY_AGGRO_3, SAY_AGGRO_4);
+        AddOnKillText(SAY_SLAY_1, SAY_SLAY_2);
+        AddOnDeathText(SAY_DEATH);
     }
 
-    instance_naxxramas* m_pInstance;
-    bool m_bIsRegularMode;
+    instance_naxxramas* m_instance;
+    bool m_hasTaunted;
+    bool m_inGracePeriod;
 
-    uint32 m_uiPoisonBoltVolleyTimer;
-    uint32 m_uiRainOfFireTimer;
-    uint32 m_uiEnrageTimer;
-    bool   m_bHasTaunted;
+    void Aggro(Unit* who) override
+    {
+        BossAI::Aggro(who);
+        m_inGracePeriod = true;
+        AddCustomAction(FAERLINA_GRACE_TIMER, 30s, [&](){
+            m_inGracePeriod = false;
+        });
+    }
 
     void Reset() override
     {
-        m_uiPoisonBoltVolleyTimer = 8000;
-        m_uiRainOfFireTimer = 16000;
-        m_uiEnrageTimer = 60000;
-    }
-
-    void Aggro(Unit* /*pWho*/) override
-    {
-        switch (urand(0, 3))
-        {
-            case 0: DoScriptText(SAY_AGGRO_1, m_creature); break;
-            case 1: DoScriptText(SAY_AGGRO_2, m_creature); break;
-            case 2: DoScriptText(SAY_AGGRO_3, m_creature); break;
-            case 3: DoScriptText(SAY_AGGRO_4, m_creature); break;
-        }
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_FAERLINA, IN_PROGRESS);
+        BossAI::Reset();
+        m_creature->SetSpellList(m_creature->GetMap()->IsRegularDifficulty() ? SPELLSET_10N : SPELLSET_25N);
     }
 
     void MoveInLineOfSight(Unit* pWho) override
     {
-        if (!m_bHasTaunted && pWho->GetTypeId() == TYPEID_PLAYER && m_creature->IsWithinDistInMap(pWho, 80.0f) &&  m_creature->IsWithinLOSInMap(pWho))
+        if (!m_hasTaunted && pWho->GetTypeId() == TYPEID_PLAYER && m_creature->IsWithinDistInMap(pWho, 80.0f) &&  m_creature->IsWithinLOSInMap(pWho))
         {
-            DoScriptText(SAY_GREET, m_creature);
-            m_bHasTaunted = true;
+            DoBroadcastText(SAY_GREET, m_creature);
+            m_hasTaunted = true;
         }
 
         ScriptedAI::MoveInLineOfSight(pWho);
     }
 
-    void KilledUnit(Unit* /*pVictim*/) override
-    {
-        DoScriptText(urand(0, 1) ? SAY_SLAY_1 : SAY_SLAY_2, m_creature);
-    }
-
-    void JustDied(Unit* /*pKiller*/) override
-    {
-        DoScriptText(SAY_DEATH, m_creature);
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_FAERLINA, DONE);
-    }
-
-    void JustReachedHome() override
-    {
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_FAERLINA, FAIL);
-    }
-
-    // Widow's Embrace prevents frenzy and poison bolt, if it removes frenzy, next frenzy is sceduled in 60s
-    // It is likely that this _should_ be handled with some dummy aura(s) - but couldn't find any
     void SpellHit(Unit* /*pCaster*/, const SpellEntry* pSpellEntry) override
     {
         // Check if we hit with Widow's Embrave
         if (pSpellEntry->Id == SPELL_WIDOWS_EMBRACE || pSpellEntry->Id == SPELL_WIDOWS_EMBRACE_H)
         {
-            bool bIsFrenzyRemove = false;
-
-            // If we remove the Frenzy, the Enrage Timer is reseted to 60s
-            if (m_creature->HasAura(m_bIsRegularMode ? SPELL_ENRAGE : SPELL_ENRAGE_H))
-            {
-                m_uiEnrageTimer = 60000;
-                m_creature->RemoveAurasDueToSpell(m_bIsRegularMode ? SPELL_ENRAGE : SPELL_ENRAGE_H);
-
-                bIsFrenzyRemove = true;
-            }
-
             // Achievement 'Momma said Knock you out': If we removed OR delayed the frenzy, the criteria is failed
-            if ((bIsFrenzyRemove || m_uiEnrageTimer < 30000) && m_pInstance)
-                m_pInstance->SetSpecialAchievementCriteria(TYPE_ACHIEV_KNOCK_YOU_OUT, false);
-
-            // In any case we prevent Frenzy and Poison Bolt Volley for Widow's Embrace Duration (30s)
-            // We do this be setting the timers to at least bigger than 30s
-            m_uiEnrageTimer = std::max(m_uiEnrageTimer, (uint32)30000);
-            m_uiPoisonBoltVolleyTimer = std::max(m_uiPoisonBoltVolleyTimer, urand(33000, 38000));
+            if ((!m_inGracePeriod) && m_instance)
+                m_instance->SetSpecialAchievementCriteria(TYPE_ACHIEV_KNOCK_YOU_OUT, false);
         }
-    }
-
-    void UpdateAI(const uint32 uiDiff) override
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->GetVictim())
-            return;
-
-        // Poison Bolt Volley
-        if (m_uiPoisonBoltVolleyTimer < uiDiff)
-        {
-            if (DoCastSpellIfCan(m_creature->GetVictim(), m_bIsRegularMode ? SPELL_POISONBOLT_VOLLEY : SPELL_POISONBOLT_VOLLEY_H) == CAST_OK)
-                m_uiPoisonBoltVolleyTimer = 11000;
-        }
-        else
-            m_uiPoisonBoltVolleyTimer -= uiDiff;
-
-        // Rain Of Fire
-        if (m_uiRainOfFireTimer < uiDiff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-            {
-                if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_RAIN_OF_FIRE : SPELL_RAIN_OF_FIRE_H) == CAST_OK)
-                    m_uiRainOfFireTimer = 16000;
-            }
-        }
-        else
-            m_uiRainOfFireTimer -= uiDiff;
-
-        // Enrage Timer
-        if (m_uiEnrageTimer < uiDiff)
-        {
-            if (DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_ENRAGE : SPELL_ENRAGE_H) == CAST_OK)
-            {
-                DoScriptText(EMOTE_BOSS_GENERIC_FRENZY, m_creature);
-                m_uiEnrageTimer = 60000;
-            }
-        }
-        else
-            m_uiEnrageTimer -= uiDiff;
-
-        DoMeleeAttackIfReady();
     }
 };
 
-UnitAI* GetAI_boss_faerlina(Creature* pCreature)
+struct WidowsEmbrace : public AuraScript, public SpellScript
 {
-    return new boss_faerlinaAI(pCreature);
-}
+    void OnApply(Aura* aura, bool apply) const override
+    {
+        if (Creature* caster = dynamic_cast<Creature*>(aura->GetCaster()))
+            if (caster->IsAlive() && caster->GetEntry() == NPC_FAERLINA_FOLLOWER)
+                caster->Suicide();
+        if (!apply)
+            return;
+        if (Creature* target = dynamic_cast<Creature*>(aura->GetTarget()))
+        {
+            DoBroadcastText(EMOTE_WIDOWS_EMBRACE, target);
+            bool isRegularDifficulty = target->GetMap()->IsRegularDifficulty();
+            target->AddCooldown(*(aura->GetSpellProto()));
+            target->RemoveAurasDueToSpell(isRegularDifficulty ? SPELL_ENRAGE : SPELL_ENRAGE_H);
+        }
+    }
+};
 
 void AddSC_boss_faerlina()
 {
     Script* pNewScript = new Script;
     pNewScript->Name = "boss_faerlina";
-    pNewScript->GetAI = &GetAI_boss_faerlina;
+    pNewScript->GetAI = &GetNewAIInstance<boss_faerlinaAI>;
     pNewScript->RegisterSelf();
+
+    RegisterSpellScript<WidowsEmbrace>("spell_widows_embrace");
 }
