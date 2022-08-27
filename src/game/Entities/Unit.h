89312aa804596eaaedc14504bb7dfd39eb2fc7e2
killerwife <killerwife@gmail.com>
2022-08-27 11:57:40 +0200

    Unit/Warlock: Fix demonic circle removing its own aura on recast

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2225,7 +2225,7 @@ class Unit : public WorldObject
         GameObject* GetGameObject(uint32 spellId) const;
         void AddGameObject(GameObject* gameObj);
         void AddWildGameObject(GameObject* gameObj);
-        void RemoveGameObject(GameObject* gameObj, bool del);
+        void RemoveGameObject(GameObject* gameObj, bool del, bool removeAura = true);
         void RemoveGameObject(uint32 spellid, bool del);
         void RemoveAllGameObjects();
 
