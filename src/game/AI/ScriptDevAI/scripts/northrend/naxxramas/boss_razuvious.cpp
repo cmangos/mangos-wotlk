78d6a129af7af52224328d7a59c24399253f7b79
insunaa <dev.insuna@gmail.com>
2022-08-28 11:42:47 +0200

    BossAI: Use own header

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_razuvious.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_razuvious.cpp
@@ -21,7 +21,7 @@ SDComment: TODO: Timers need confirmation
 SDCategory: Naxxramas
 EndScriptData */
 
-#include "AI/ScriptDevAI/base/CombatAI.h"
+#include "AI/ScriptDevAI/base/BossAI.h"
 #include "AI/ScriptDevAI/include/sc_common.h"
 #include "naxxramas.h"
 
