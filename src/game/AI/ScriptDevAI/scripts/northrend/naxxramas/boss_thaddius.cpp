78d9bda14fa2097c732035d359f6ec7a900c6105
insunaa <dev.insuna@gmail.com>
2022-08-29 19:07:49 +0200

    BossAI: Fix Thaddius Emotes and Ball Lightning

--- src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/boss_thaddius.cpp
@@ -28,7 +28,6 @@ boss_stalagg
 boss_feugen
 EndContentData */
 
-#include "AI/ScriptDevAI/ScriptDevAIMgr.h"
 #include "AI/ScriptDevAI/base/BossAI.h"
 #include "AI/ScriptDevAI/include/sc_common.h"
 #include "naxxramas.h"
@@ -120,7 +119,7 @@ struct boss_thaddiusAI : public BossAI
         AddOnAggroText(SAY_AGGRO_1, SAY_AGGRO_2, SAY_AGGRO_3);
         AddCombatAction(THADDIUS_POLARITY_SHIFT, 15s);
         AddCombatAction(THADDIUS_CHAIN_LIGHTNING, 8s);
-        AddCombatAction(THADDIUS_BALL_LIGHTNING, true);
+        AddCombatAction(THADDIUS_BALL_LIGHTNING, 1s);
         AddCombatAction(THADDIUS_BERSERK, 6min);
         Reset();
     }
@@ -183,6 +182,15 @@ struct boss_thaddiusAI : public BossAI
         DoCastSpellIfCan(nullptr, SPELL_CLEAR_CHARGES);
     }
 
+    void OnSpellCooldownAdded(SpellEntry const* spellInfo) override
+    {
+        if (spellInfo->Id == SPELL_POLARITY_SHIFT)
+        {
+            DoBroadcastText(SAY_ELECT, m_creature);
+            DoBroadcastText(EMOTE_POLARITY_SHIFT, m_creature);
+        }
+    }
+
     std::chrono::milliseconds GetSubsequentActionTimer(uint32 action)
     {
         switch (action)
@@ -214,11 +222,7 @@ struct boss_thaddiusAI : public BossAI
             case THADDIUS_POLARITY_SHIFT:
             {
                 if (DoCastSpellIfCan(m_creature, SPELL_POLARITY_SHIFT, CAST_INTERRUPT_PREVIOUS) == CAST_OK)
-                {
-                    DoBroadcastText(SAY_ELECT, m_creature);
-                    DoBroadcastText(EMOTE_POLARITY_SHIFT, m_creature);
                     break;
-                }
                 return;
             }
             case THADDIUS_BALL_LIGHTNING:
