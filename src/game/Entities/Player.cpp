258d81457db8a55ed7e8dc644c051578160ffec8
killerwife <killerwife@gmail.com>
2022-10-02 15:57:43 +0200

    Player: Add SMSG_PLAYER_SKINNED packet fill

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -8776,11 +8776,13 @@ void Player::RemovedInsignia(Player* looterPlr)
         return;
 
     // If not released spirit, do it !
+    bool repop = false;
     if (m_deathTimer > 0)
     {
         m_deathTimer = 0;
         BuildPlayerRepop();
         RepopAtGraveyard();
+        repop = true;
     }
 
     Corpse* corpse = GetCorpse();
@@ -8788,6 +8790,7 @@ void Player::RemovedInsignia(Player* looterPlr)
         return;
 
     WorldPacket data(SMSG_PLAYER_SKINNED, 0);
+    data << uint8(repop);
     GetSession()->SendPacket(data);
 
     // We have to convert player corpse to bones, not to be able to resurrect there
