3223183b62d80c3327dfeb72aa6eedce6c22cfe7
insunaa <dev.insuna@gmail.com>
2022-12-29 09:32:10 +0100

    BossAI: Add Resettable Values

--- src/game/AI/ScriptDevAI/base/BossAI.h
+++ src/game/AI/ScriptDevAI/base/BossAI.h
@@ -143,6 +143,17 @@ class BossAI : public CombatAI
             DespawnSubordinateOnEvade(fargs...);
         }
 
+        template<typename T, typename W>
+        void ResetValueTo(T& var, W val)
+        {
+            static_assert(std::is_convertible<W, T>::value, "Value must be of castable type to the variable!");
+            T* ptr = &var;
+            m_resetValues.emplace_back([&]()
+            {
+                *ptr = static_cast<T>(val);
+            });
+        }
+
         std::chrono::seconds TimeSinceEncounterStart()
         {
             return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - m_combatStartTimestamp);
@@ -161,6 +172,8 @@ class BossAI : public CombatAI
         std::vector<uint32> m_exitObjects;
         std::chrono::milliseconds m_gateDelay = 3s;
         std::vector<QueuedCast> m_castOnDeath;
+
+        std::vector<std::function<void()>> m_resetValues;
 
         uint32 m_instanceDataType = -1;
 
