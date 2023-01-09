4266a7628b932f8d1f1fd305d3b0698b52013d37
insunaa <dev.insuna@gmail.com>
2023-01-09 09:55:49 +0100

    Firebolt: Spellscript SQL

--- sql/scriptdev2/spell.sql
+++ sql/scriptdev2/spell.sql
@@ -965,6 +965,7 @@ INSERT INTO spell_scripts(Id, ScriptName) VALUES
 (62707,'spell_ignis_grab'),
 (62717,'spell_slag_pot'),
 (63474,'spell_ignis_scorch'),
+(62669,'spell_firebolt_razorscale'),
 (63236,'spell_devouring_flame_razorscale'),
 (63482,'spell_lightning_whirl_heroic'),
 (63845,'spell_create_lance'),
