35de982a0c5b349c6f4bbfc4b38fe62eebc4b8bb
insunaa <dev.insuna@gmail.com>
2023-01-30 23:02:24 +0100

    Use correct path

--- src/game/Models/M2Stores.cpp
+++ src/game/Models/M2Stores.cpp
@@ -272,10 +272,11 @@ void LoadM2Attachments(std::string const& dataPath)
         if (CreatureModelDataEntry const* dbcentry = sCreatureModelDataStore.LookupEntry(i))
         {
             std::string filename = dataPath;
+            filename.append("CreatureModels/");
             filename.append(dbcentry->ModelPath);
 
             // Replace slashes
-            std::replace(filename.begin(), filename.end(), '\\', '/');
+            //std::replace(filename.begin(), filename.end(), '\\', '/');
 
             // Replace mdx to .m2
             size_t loc = filename.find(".mdx");
