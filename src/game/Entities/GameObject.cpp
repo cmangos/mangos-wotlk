19c86ce01bff7a01f5cf05b0aa89e7fbf07be1ae
insunaa <dev.insuna@gmail.com>
2023-02-17 14:55:39 +0100

    Ulduar: Increase Bubble visbility

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -305,6 +305,8 @@ bool GameObject::Create(uint32 dbGuid, uint32 guidlow, uint32 name_id, Map* map,
 
     if (GetGoType() == GAMEOBJECT_TYPE_DESTRUCTIBLE_BUILDING)
         GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Infinite);
+    if (GetGOInfo()->id == 194484)
+        GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Gigantic);
 
     return true;
 }
