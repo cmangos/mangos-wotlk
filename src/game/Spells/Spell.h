491ee34d098cdcf05ba6c1fdf959eb85c37493e1
insunaa <dev.insuna@gmail.com>
2023-02-17 23:00:23 +0100

    Fix spells not chaining anymore

--- src/game/Spells/Spell.h
+++ src/game/Spells/Spell.h
@@ -1238,6 +1238,7 @@ namespace MaNGOS
                         if (!i_originalCaster->CanAssistSpell(itr->getSource(), i_spell.m_spellInfo))
                             continue;
                         break;
+                    case SPELL_TARGETS_CHAIN_ATTACKABLE:
                     case SPELL_TARGETS_AOE_ATTACKABLE:
                     {
                         if (!i_originalCaster->CanAttackSpell(itr->getSource(), i_spell.m_spellInfo, !i_spell.m_spellInfo->HasAttribute(SPELL_ATTR_EX5_IGNORE_AREA_EFFECT_PVP_CHECK)))
