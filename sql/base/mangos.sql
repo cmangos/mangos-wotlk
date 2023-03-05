b011878249835ccbe49b352acc5159aa01dda9ec
killerwife <killerwife@gmail.com>
2023-03-05 11:21:16 +0100

    [14072] Add creature_spawn_data_template Name

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -24,7 +24,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_14071_01_mangos_spawn_data_equip` bit(1) DEFAULT NULL
+  `required_14072_01_mangos_string_id` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
