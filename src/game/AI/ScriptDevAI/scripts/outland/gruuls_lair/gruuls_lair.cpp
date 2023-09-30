c73b7aaea90d3fe3c18cd238ee77a24f43c95b7e
evil-at-wow <evil.at.wow@gmail.com>
2023-09-30 17:41:02 +0200

    Remove unused member from instance_gruuls_lair.

--- src/game/AI/ScriptDevAI/scripts/outland/gruuls_lair/gruuls_lair.cpp
+++ src/game/AI/ScriptDevAI/scripts/outland/gruuls_lair/gruuls_lair.cpp
@@ -29,8 +29,7 @@ EndScriptData */
 2 - Gruul event
 */
 
-instance_gruuls_lair::instance_gruuls_lair(Map* pMap) : ScriptedInstance(pMap),
-    m_uiCouncilMembersDied(0)
+instance_gruuls_lair::instance_gruuls_lair(Map* pMap) : ScriptedInstance(pMap)
 {
     Initialize();
 }
--- src/game/AI/ScriptDevAI/scripts/outland/gruuls_lair/gruuls_lair.h
+++ src/game/AI/ScriptDevAI/scripts/outland/gruuls_lair/gruuls_lair.h
@@ -45,8 +45,6 @@ class instance_gruuls_lair : public ScriptedInstance
     private:
         uint32 m_auiEncounter[MAX_ENCOUNTER];
         std::string m_strSaveData;
-
-        uint8 m_uiCouncilMembersDied;
 };
 
 #endif
