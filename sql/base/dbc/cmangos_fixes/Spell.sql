0093bc80ae5f9c6aa2abd00986ae0f1777d6fa79
AnonXS <anonxsdev@gmail.com>
2023-09-30 12:41:03 +0200

    [Spell] 16577 Disease Cloud should not be removed on Evade

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -2303,6 +2303,7 @@ UPDATE `spell_template` SET `AttributesServerSide` = `AttributesServerSide`|0x00
 12896, -- Helboar
 12898, -- Smoke Aura Visual
 13879, -- Magma Splash
+16577, -- Disease Cloud
 17205, -- Winterfall Firewater
 18163, -- Strength of Arko'narin
 18167, -- Holy Fire
