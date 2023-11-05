26f5bd242cff34f53826dac9bdd7d3955b657022
XandrNiko <starnetkhv@gmail.com>
2023-11-05 03:33:40 +0300

    Magtheridon: Fix door to boss magtheridon Closes https://github.com/cmangos/mangos-tbc/pull/638

--- src/game/AI/ScriptDevAI/scripts/outland/hellfire_citadel/magtheridons_lair/magtheridons_lair.cpp
+++ src/game/AI/ScriptDevAI/scripts/outland/hellfire_citadel/magtheridons_lair/magtheridons_lair.cpp
@@ -127,11 +127,10 @@ void instance_magtheridons_lair::SetData(uint32 uiType, uint32 uiData)
                     m_uiCageBreakTimer = 0;
                     m_uiCageBreakStage = 0;
 
-                // no break;
+                    [[fallthrough]];
                 case DONE:
                     // Reset door on Fail or Done
-                    if (GameObject* pDoor = GetSingleGameObjectFromStorage(GO_DOODAD_HF_MAG_DOOR01))
-                        pDoor->ResetDoorOrButton();
+                    DoUseOpenableObject(GO_DOODAD_HF_MAG_DOOR01, true);
 
                     SetData(TYPE_CHANNELER_EVENT, DONE);
                     break;
@@ -164,8 +163,7 @@ void instance_magtheridons_lair::SetData(uint32 uiType, uint32 uiData)
                 m_uiCageBreakStage = 0;
 
                 // Reset door on Fail
-                if (GameObject* pDoor = GetSingleGameObjectFromStorage(GO_DOODAD_HF_MAG_DOOR01))
-                    pDoor->ResetDoorOrButton();
+                DoUseOpenableObject(GO_DOODAD_HF_MAG_DOOR01, true);
 
                 // Reset Magtheridon
                 if (Creature* pMagtheridon = GetSingleCreatureFromStorage(NPC_MAGTHERIDON))
@@ -187,7 +185,7 @@ void instance_magtheridons_lair::SetData(uint32 uiType, uint32 uiData)
                 }
 
                 // combat door
-                DoUseDoorOrButton(GO_DOODAD_HF_MAG_DOOR01);
+                DoUseOpenableObject(GO_DOODAD_HF_MAG_DOOR01, false);
             }
             m_auiEncounter[uiType] = uiData;
             break;
