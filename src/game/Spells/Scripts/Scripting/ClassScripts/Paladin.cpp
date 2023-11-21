cmangos/mangos-tbc

Spell: Add missing comment naming to all existing class spell scripts

src/game/Spells/Scripts/Scripting/ClassScripts/Paladin.cpp
@@ -90,6 +90,7 @@ struct JudgementOfWisdomIntermediate : public SpellScript
     }
 };
 
+// 20271 - Judgement
 struct spell_judgement : public SpellScript
 {
     void OnEffectExecute(Spell* spell, SpellEffectIndex /*effIdx*/) const override
@@ -146,13 +147,14 @@ struct spell_judgement : public SpellScript
         if (caster->HasAura(37188)) // improved judgement
             caster->CastSpell(nullptr, 43838, TRIGGERED_OLD_TRIGGERED);
 
-        if (caster->HasAura(40470)) // spell_paladin_tier_6_trinket
+        if (caster->HasAura(40470)) // PaladinTier6Trinket
             if (roll_chance_f(50.f))
                 caster->CastSpell(unitTarget, 40472, TRIGGERED_OLD_TRIGGERED);
     }
 };
 
-struct spell_paladin_tier_6_trinket : public AuraScript
+// 40470 - Paladin Tier 6 Trinket
+struct PaladinTier6Trinket : public AuraScript
 {
     SpellAuraProcResult OnProc(Aura* /*aura*/, ProcExecutionData& procData) const override
     {
@@ -176,6 +178,7 @@ struct spell_paladin_tier_6_trinket : public AuraScript
     }
 };
 
+// 31789 - Righteous Defense
 struct RighteousDefense : public SpellScript
 {
     bool OnCheckTarget(const Spell* spell, Unit* target, SpellEffectIndex /*eff*/) const override
@@ -226,6 +229,7 @@ enum
     SPELL_SEAL_OF_BLOOD_SELF_DAMAGE         = 32221
 };
 
+// 31893 - Seal of Blood, 31898 - Judgement of Blood
 struct SealOfBloodSelfDamage : public SpellScript
 {
     void OnAfterHit(Spell* spell) const override
@@ -265,6 +269,6 @@ void LoadPaladinScripts()
     RegisterSpellScript<RighteousDefense>("spell_righteous_defense");
     RegisterSpellScript<SealOfTheCrusader>("spell_seal_of_the_crusader");
     RegisterSpellScript<SealOfBloodSelfDamage>("spell_seal_of_blood_self_damage");
-    RegisterSpellScript<spell_paladin_tier_6_trinket>("spell_paladin_tier_6_trinket");
+    RegisterSpellScript<PaladinTier6Trinket>("PaladinTier6Trinket");
     RegisterSpellScript<BlessingOfLight>("spell_blessing_of_light");
 }

cmangos/mangos-wotlk

src/game/Spells/Scripts/Scripting/ClassScripts/Paladin.cpp
@@ -20,6 +20,7 @@
 #include "Spells/SpellAuras.h"
 #include "Spells/SpellMgr.h"
 
+// 20271 - Judgement
 struct spell_judgement : public SpellScript
 {
     void OnEffectExecute(Spell* spell, SpellEffectIndex /*effIdx*/) const override
@@ -81,13 +82,14 @@ struct spell_judgement : public SpellScript
         if (caster->HasAura(37188)) // improved judgement
             caster->CastSpell(nullptr, 43838, TRIGGERED_OLD_TRIGGERED);
 
-        if (caster->HasAura(40470)) // spell_paladin_tier_6_trinket
+        if (caster->HasAura(40470)) // PaladinTier6Trinket
             if (roll_chance_f(50.f))
                 caster->CastSpell(unitTarget, 40472, TRIGGERED_OLD_TRIGGERED);
     }
 };
 
-struct spell_paladin_tier_6_trinket : public AuraScript
+// 40470 - Paladin Tier 6 Trinket
+struct PaladinTier6Trinket : public AuraScript
 {
     SpellAuraProcResult OnProc(Aura* /*aura*/, ProcExecutionData& procData) const override
     {
@@ -124,6 +126,7 @@ struct IncreasedHolyLightHealing : public AuraScript
     }
 };
 
+// 31789 - Righteous Defense
 struct RighteousDefense : public SpellScript
 {
     bool OnCheckTarget(const Spell* /*spell*/, Unit* target, SpellEffectIndex /*eff*/) const override
@@ -319,7 +322,7 @@ void LoadPaladinScripts()
     RegisterSpellScript<IncreasedHolyLightHealing>("spell_increased_holy_light_healing");
     RegisterSpellScript<spell_judgement>("spell_judgement");
     RegisterSpellScript<RighteousDefense>("spell_righteous_defense");
-    RegisterSpellScript<spell_paladin_tier_6_trinket>("spell_paladin_tier_6_trinket");
+    RegisterSpellScript<PaladinTier6Trinket>("spell_paladin_tier_6_trinket");
     RegisterSpellScript<DivineStorm>("spell_divine_storm");
     RegisterSpellScript<DivineStormHeal>("spell_divine_storm_heal");
     RegisterSpellScript<DivineStormCooldown>("spell_divine_storm_cooldown");