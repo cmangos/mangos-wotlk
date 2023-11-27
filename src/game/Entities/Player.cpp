8da0aaf140ab3fa3d13872bf2c498d5d65d75081
insunaa <insuna@no.capfr.fr>
2023-11-27 15:52:53 +0100

    Pets: change flying-mount-detection logic

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -1925,8 +1925,8 @@ bool Player::Mount(uint32 displayid, const Aura* aura/* = nullptr*/)
         return false;
 
     // Custom mount (non-aura such as taxi or command) or in flight: unsummon any pet
-    if (!aura && !sWorld.getConfig(CONFIG_BOOL_KEEP_PET_WHEN_MOUNTED) &&
-        (IsFreeFlying() || IsSpellHaveAura(aura->GetSpellProto(), SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED)))
+    if (!sWorld.getConfig(CONFIG_BOOL_KEEP_PET_WHEN_MOUNTED) &&
+        (!aura || IsFreeFlying() || IsSpellHaveAura(aura->GetSpellProto(), SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED)))
     {
         UnsummonPetTemporaryIfAny();
     }
