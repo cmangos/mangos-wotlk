5b3ed92e4f60889a276379f285f70b6ddb8fdd3d
killerwife <killerwife@gmail.com>
2024-02-13 09:07:39 +0100

    Network: Rework reconnection case to fix slow cpu race conditions https://github.com/cmangos/issues/issues/3676

--- src/game/Server/WorldSession.h
+++ src/game/Server/WorldSession.h
@@ -313,8 +313,7 @@ class WorldSession
         void SetExpansion(uint8 expansion);
 
         void InitializeAnticheat(const BigNumber& K);
-        void AssignAnticheat();
-        void SetDelayedAnticheat(std::unique_ptr<SessionAnticheatInterface>&& anticheat);
+        void AssignAnticheat(std::unique_ptr<SessionAnticheatInterface>&& anticheat);
         SessionAnticheatInterface* GetAnticheat() const { return m_anticheat.get(); }
 
 #ifdef BUILD_DEPRECATED_PLAYERBOT
@@ -1050,7 +1049,6 @@ class WorldSession
         uint32 m_accountMaxLevel;
         uint32 m_orderCounter;
         uint32 m_lastAnticheatUpdate;
-        std::unique_ptr<SessionAnticheatInterface> m_delayedAnticheat;
         std::unique_ptr<SessionAnticheatInterface> m_anticheat;
 
         time_t _logoutTime;                                 // when logout will be processed after a logout request
