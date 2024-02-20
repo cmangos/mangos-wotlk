e581a52dbd7682677f1a608b08a636bc92c5b975
Grz3s <Grz3s@users.noreply.github.com>
2024-02-21 00:05:13 +0100

    Icecrown: support for random groups
    Part of DB update

--- src/game/AI/ScriptDevAI/scripts/northrend/world_northrend.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/world_northrend.cpp
@@ -16,6 +16,7 @@
 
 #include "AI/ScriptDevAI/include/sc_common.h"
 #include "world_northrend.h"
+#include "World/WorldStateDefines.h"
 #include "AI/ScriptDevAI/scripts/world/world_map_scripts.h"
 
 world_map_northrend::world_map_northrend(Map* map) : ScriptedMap(map)
@@ -25,10 +26,45 @@ world_map_northrend::world_map_northrend(Map* map) : ScriptedMap(map)
 
 void world_map_northrend::Initialize()
 {
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_01, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_02, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_03, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_04, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_05, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_06, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_07, urand(0, 1));
+    instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_08, urand(0, 1));
 }
 
 void world_map_northrend::OnCreatureGroupDespawn(CreatureGroup* creatureGroup, Creature* creature)
 {
+    switch (creatureGroup->GetGroupEntry().WorldStateCondition)
+    {
+    case 25000: case 25001: // TODO: Propagate respawn info so they do not instant respawn
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_01, urand(0, 1));
+        break;
+    case 25002: case 25003:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_02, urand(0, 1));
+        break;
+    case 25004: case 25005:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_03, urand(0, 1));
+        break;
+    case 25006: case 25007:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_04, urand(0, 1));
+        break;
+    case 25008: case 25009:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_05, urand(0, 1));
+        break;
+    case 25010: case 25011:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_06, urand(0, 1));
+        break;
+    case 25012: case 25013:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_07, urand(0, 1));
+        break;
+    case 25014: case 25015:
+        instance->GetVariableManager().SetVariable(WORLD_STATE_CUSTOM_ICC_GRP_08, urand(0, 1));
+        break;
+    }
 }
 
 void world_map_northrend::OnCreatureCreate(Creature* creature)
