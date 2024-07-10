29dcff7b413d33b15e35c23c77756f16057e7a23
insunaa <insuna@no.capfr.fr>
2024-07-10 10:21:35 +0200

    Ulduar: Remove unneeded Leviathan changes

--- src/game/AI/ScriptDevAI/scripts/northrend/ulduar/ulduar/ulduar.cpp
+++ src/game/AI/ScriptDevAI/scripts/northrend/ulduar/ulduar/ulduar.cpp
@@ -319,9 +319,6 @@ void instance_ulduar::OnCreatureCreate(Creature* pCreature)
         case NPC_PRIEST_ALLIANCE_H:
             pCreature->SetImmuneToNPC(false);
             return;
-        case NPC_LEVIATHAN_SEAT:
-            m_leviathanSeatGuids.insert(pCreature->GetObjectGuid());
-            return;
         default:
             return;
     }
@@ -1399,9 +1396,6 @@ void instance_ulduar::OnCreatureDeath(Creature* pCreature)
             SetData(TYPE_FREYA_CONSPEEDATORY, DONE);
         }
         break;
-        case NPC_LEVIATHAN_SEAT:
-            m_leviathanSeatGuids.erase(pCreature->GetObjectGuid());
-            break;
     }
 }
 
