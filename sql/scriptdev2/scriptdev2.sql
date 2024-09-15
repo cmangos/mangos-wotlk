651790a01e0916095aa136ccbdbfb8c3f3ffb350
Grz3s <Grz3s@users.noreply.github.com>
2024-09-15 23:42:03 +0200

    SD2: Loramus Thalipedes 7783 update
    Gossip moved to DB

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -413,7 +413,6 @@ UPDATE creature_template SET ScriptName='npc_rizzle_sprysprocket' WHERE entry=23
 UPDATE creature_template SET ScriptName='npc_depth_charge' WHERE entry=23025;
 UPDATE gameobject_template SET ScriptName='go_southfury_moonstone' WHERE entry=185566;
 UPDATE creature_template SET ScriptName='mobs_spitelashes' WHERE entry IN (6190,6193,6194,6195,6196);
-UPDATE creature_template SET ScriptName='npc_loramus_thalipedes' WHERE entry=7783;
 UPDATE creature_template SET ScriptName='npc_felhound_tracker' WHERE entry=8668;
 UPDATE creature_template SET ScriptName='boss_maws' WHERE entry=15571;
 UPDATE gameobject_template SET ScriptName='go_lightning' WHERE entry=183356;
