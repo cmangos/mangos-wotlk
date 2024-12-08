9e16a16ab61ae29003d637a7ac0094b96729bfdc
AnonXS <anonxs@web.de>
2024-12-08 23:35:33 +0100

    [Spell] Net Guard 11920 should reduce threat on target hit
    Might be different by entry that uses the parent spell
    
    Confirmed for:
    c.16912 145200 -> 72600
    c.1034 19400 -> 9700
    c.485 seemed to work different
    
    Based on https://github.com/vmangos/core/commit/619b0d7278dcf084b98876a4c597d8deb635ea4c
    
    Closes https://github.com/cmangos/mangos-tbc/pull/714

--- src/game/AI/ScriptDevAI/scripts/world/spell_scripts.cpp
+++ src/game/AI/ScriptDevAI/scripts/world/spell_scripts.cpp
@@ -1398,6 +1398,18 @@ struct RetaliationCreature : public SpellScript
     }
 };
 
+// 11920 - Net Guard
+struct NetGuard : public SpellScript
+{
+    void OnEffectExecute(Spell* spell, SpellEffectIndex effIdx) const override
+    {
+        if (effIdx != EFFECT_INDEX_0)
+            return;
+
+        spell->GetCaster()->getThreatManager().modifyThreatPercent(spell->GetUnitTarget(), -50);
+    }
+};
+
 struct HateToHalf : public SpellScript
 {
     void OnEffectExecute(Spell* spell, SpellEffectIndex effIdx) const override
@@ -1707,6 +1719,7 @@ void AddSC_spell_scripts()
     RegisterSpellScript<PreventSpellIfSameAuraOnCaster>("spell_prevent_spell_if_same_aura_on_caster");
     RegisterSpellScript<InstillLordValthalaksSpirit>("spell_instill_lord_valthalaks_spirit");
     RegisterSpellScript<RetaliationCreature>("spell_retaliation_creature");
+    RegisterSpellScript<NetGuard>("spell_net_guard");
     RegisterSpellScript<HateToHalf>("spell_hate_to_half");
     RegisterSpellScript<HateToZero>("spell_hate_to_zero");
     RegisterSpellScript<Stoned>("spell_stoned");
