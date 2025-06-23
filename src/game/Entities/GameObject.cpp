a23eb3f27eb3e64d46053eefe1fa5a718ef54c63
killerwife <killerwife@gmail.com>
2025-06-23 12:08:58 +0200

    GameObject: Fix crash due to empty functor

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -2086,7 +2086,7 @@ void GameObject::Use(Unit* user, SpellEntry const* spellInfo)
     targets.setUnitTarget(user);
 
     SpellCastResult result = spell->SpellStart(&targets);
-    if (result == SPELL_CAST_OK)
+    if (result == SPELL_CAST_OK && onSuccess)
         onSuccess();
 }
 
