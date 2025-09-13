652617e1f274ba6244c42f45000f6da87ef9ff0c
AnonXS <anonxs@web.de>
2025-09-14 01:00:10 +0200

    [Spell] Fel Fire 32008 should not be removed on Evade

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -3109,6 +3109,7 @@ UPDATE `spell_template` SET `EffectMiscValueB1`=64 WHERE `Id` IN(38107);
 -- SPELL_ATTR_SS_IGNORE_EVADE - IsSpellRemovedOnEvade()
 -- ============================================================
 UPDATE `spell_template` SET `AttributesServerSide` = `AttributesServerSide`|0x00000004 WHERE `Id` IN (
+32008, -- Fel Fire - c.17152,18944,19298
 36630, -- Resistances (c.22000)
 37248, -- Power Converters: Electromental Visual
 38471, -- Spore Burst
