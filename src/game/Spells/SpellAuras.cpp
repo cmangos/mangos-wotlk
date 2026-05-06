28ab317fed04f8fb23eb9265501148ab5f18aebe
killerwife <killerwife@gmail.com>
2026-05-06 08:13:25 +0200

    Spell: Fill amount for aura shapeshift in order to have data for displayid restore

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -4025,7 +4025,10 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
         target->RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT, GetHolder());
 
         if (displayId > 0)
+        {
+            GetModifier()->m_amount = displayId;
             target->SetDisplayId(displayId);
+        }
 
         // now only powertype must be set
         switch (form)
