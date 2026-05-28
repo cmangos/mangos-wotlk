9780438c12ad52c1db872fd801014293785da432
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-05-28 01:05:21 -0400

    Network: fix nullptr in WorldSession::HandleMoveWorldportAckOpcode (#820)

--- src/game/MotionGenerators/MovementHandler.cpp
+++ src/game/MotionGenerators/MovementHandler.cpp
@@ -47,6 +47,9 @@ void WorldSession::HandleMoveWorldportAckOpcode(WorldPacket& /*recv_data*/)
 
 void WorldSession::HandleMoveWorldportAckOpcode()
 {
+    if (!GetPlayer())
+        return;
+
     // ignore unexpected far teleports
     if (!GetPlayer()->IsBeingTeleportedFar())
         return;
