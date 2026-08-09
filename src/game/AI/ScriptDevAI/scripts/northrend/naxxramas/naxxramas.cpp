Crocolisk/mangos-tbc

Fix compilation warnings. Change Location to Position. SpawnLocation, WaveLocation single def.

src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/naxxramas/naxxramas.cpp
@@ -747,9 +747,9 @@ void instance_naxxramas::Update(uint32 diff)
             {
                 // Spawn 3 living poisons every 5 secs and make them cross the corridor and then despawn, for ever and ever
                 for (uint8 i = 0; i < 3; i++)
-                    if (Creature* poison = trigger->SummonCreature(NPC_LIVING_POISON, livingPoisonPositions[i].m_fX, livingPoisonPositions[i].m_fY, livingPoisonPositions[i].m_fZ, livingPoisonPositions[i].m_fO, TEMPSPAWN_DEAD_DESPAWN, 0))
+                    if (Creature* poison = trigger->SummonCreature(NPC_LIVING_POISON, livingPoisonPositions[i].x, livingPoisonPositions[i].y, livingPoisonPositions[i].z, livingPoisonPositions[i].o, TEMPSPAWN_DEAD_DESPAWN, 0))
                     {
-                        poison->GetMotionMaster()->MovePoint(0, livingPoisonPositions[i + 3].m_fX, livingPoisonPositions[i + 3].m_fY, livingPoisonPositions[i + 3].m_fZ);
+                        poison->GetMotionMaster()->MovePoint(0, livingPoisonPositions[i + 3].x, livingPoisonPositions[i + 3].y, livingPoisonPositions[i + 3].z);
                         poison->ForcedDespawn(15000);
                     }
             }
@@ -948,12 +948,7 @@ bool instance_naxxramas::DoHandleEvent(uint32 eventId)
     return false;
 }
 
-struct Location3DPoint
-{
-    float x, y, z;
-};
-
-static const Location3DPoint gargoyleResetCoords = {2963.f, -3476.f, 297.6f};
+static const Position gargoyleResetCoords = {2963.f, -3476.f, 297.6f, 0.0};
 
 enum
 {

src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/naxxramas/naxxramas.h
@@ -242,13 +242,8 @@ struct GothTrigger
 
 static const float sapphironPositions[4] = {3521.48f, -5234.87f, 137.626f, 4.53329f};
 
-struct SpawnLocation
-{
-    float m_fX, m_fY, m_fZ, m_fO;
-};
-
 // Used in Construct Quarter for the deadly blobs continuously spawning in Patchwerk corridor
-static const SpawnLocation livingPoisonPositions[6] =
+static const Position livingPoisonPositions[6] =
 {
     {3128.692f, -3119.211f, 293.346f, 4.725505f},
     {3154.432f, -3125.669f, 293.408f, 4.456693f},

cmangos/mangos-wotlk

src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/naxxramas.cpp
@@ -663,9 +663,9 @@ void instance_naxxramas::Update(uint32 uiDiff)
             {
                 // Spawn 3 living poisons every 5 secs and make them cross the corridor and then despawn, for ever and ever
                 for (uint8 i = 0; i < 3; i++)
-                    if (Creature* pPoison = pPlayer->SummonCreature(NPC_LIVING_POISON, aLivingPoisonPositions[i].m_fX, aLivingPoisonPositions[i].m_fY, aLivingPoisonPositions[i].m_fZ, aLivingPoisonPositions[i].m_fO, TEMPSPAWN_DEAD_DESPAWN, 0))
+                    if (Creature* pPoison = pPlayer->SummonCreature(NPC_LIVING_POISON, aLivingPoisonPositions[i].x, aLivingPoisonPositions[i].y, aLivingPoisonPositions[i].z, aLivingPoisonPositions[i].o, TEMPSPAWN_DEAD_DESPAWN, 0))
                     {
-                        pPoison->GetMotionMaster()->MovePoint(0, aLivingPoisonPositions[i + 3].m_fX, aLivingPoisonPositions[i + 3].m_fY, aLivingPoisonPositions[i + 3].m_fZ);
+                        pPoison->GetMotionMaster()->MovePoint(0, aLivingPoisonPositions[i + 3].x, aLivingPoisonPositions[i + 3].y, aLivingPoisonPositions[i + 3].z);
                         pPoison->ForcedDespawn(15000);
                     }
             }

src/game/AI/ScriptDevAI/scripts/northrend/naxxramas/naxxramas.h
@@ -238,13 +238,8 @@ struct GothTrigger
 
 static const float aSapphPositions[4] = {3521.48f, -5234.87f, 137.626f, 4.53329f};
 
-struct SpawnLocation
-{
-    float m_fX, m_fY, m_fZ, m_fO;
-};
-
 // Used in Construct Quarter for the deadly blobs continuously spawning in Patcherk corridor
-static const SpawnLocation aLivingPoisonPositions[6] =
+static const Position aLivingPoisonPositions[6] =
 {
     {3128.692f, -3119.211f, 293.346f, 4.725505f},
     {3154.432f, -3125.669f, 293.408f, 4.456693f},