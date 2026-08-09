cmangos/mangos-tbc

Refactor: Make function for generating Bitmasks

src/game/AI/BaseAI/UnitAI.cpp
@@ -730,7 +730,7 @@ CreatureList UnitAI::DoFindFriendlyEligibleDispel(SpellEntry const* spellInfo, b
         if (spellInfo->Effect[i] == SPELL_EFFECT_DISPEL)
             dispelMask |= GetDispellMask(DispelType(spellInfo->EffectMiscValue[i]));
         else if (spellInfo->Effect[i] == SPELL_EFFECT_DISPEL_MECHANIC)
-            mechanicMask |= (1 << (spellInfo->EffectMiscValue[i] - 1));
+            mechanicMask |= getBitmask(spellInfo->EffectMiscValue[i]);
     }            
     float maxRange = CalculateSpellRange(spellInfo);
     return DoFindFriendlyEligibleDispel(maxRange, dispelMask, mechanicMask, self);

src/game/Entities/Corpse.h
@@ -68,7 +68,7 @@ class Corpse : public WorldObject
         void SetOwnerGuid(ObjectGuid guid) override { SetGuidValue(CORPSE_FIELD_OWNER, guid); }
 
         uint8 getRace() const { return GetByteValue(CORPSE_FIELD_BYTES_1, 1); }
-        uint32 getRaceMask() const { return 1 << (getRace() - 1); }
+        uint32 getRaceMask() const { return getBitmask(getRace()); }
         uint8 getGender() const { return GetByteValue(CORPSE_FIELD_BYTES_1, 2); }
 
         // faction template id

src/game/Entities/Creature.cpp
@@ -2019,7 +2019,7 @@ bool Creature::IsImmuneToSpell(SpellEntry const* spellInfo, bool castOnSelf, uin
 
     if (!castOnSelf)
     {
-        if (GetCreatureInfo()->MechanicImmuneMask & (1 << (spellInfo->Mechanic - 1)))
+        if (GetCreatureInfo()->MechanicImmuneMask & getBitmask(spellInfo->Mechanic))
             return true;
 
         if (GetCreatureInfo()->SchoolImmuneMask & spellInfo->SchoolMask)
@@ -2039,7 +2039,7 @@ bool Creature::IsImmuneToDamage(SpellSchoolMask meleeSchoolMask)
 
 bool Creature::IsImmuneToSpellEffect(SpellEntry const* spellInfo, SpellEffectIndex index, bool castOnSelf) const
 {
-    if (!castOnSelf && GetCreatureInfo()->MechanicImmuneMask & (1 << (spellInfo->EffectMechanic[index] - 1)))
+    if (!castOnSelf && GetCreatureInfo()->MechanicImmuneMask & getBitmask(spellInfo->EffectMechanic[index]))
         return true;
 
     return Unit::IsImmuneToSpellEffect(spellInfo, index, castOnSelf);

src/game/Entities/Pet.cpp
@@ -1531,7 +1531,7 @@ bool Pet::HaveInDiet(ItemPrototype const* item) const
         return false;
 
     const uint32 diet = cFamily->petFoodMask;
-    const uint32 FoodMask = 1 << (item->FoodType - 1);
+    const uint32 FoodMask = getBitmask(item->FoodType);
     return (diet & FoodMask) != 0;
 }
 

src/game/Globals/ObjectMgr.cpp
@@ -603,7 +603,7 @@ void ObjectMgr::LoadCreatureTemplates()
             const_cast<CreatureInfo*>(cInfo)->Expansion = -1;
         }
 
-        if (!cInfo->UnitClass || (((1 << (cInfo->UnitClass - 1)) & CLASSMASK_ALL_CREATURES) == 0))
+        if (!cInfo->UnitClass || ((getBitmask(cInfo->UnitClass) & CLASSMASK_ALL_CREATURES) == 0))
         {
             ERROR_DB_STRICT_LOG("Creature (Entry: %u) does not have proper `UnitClass` (%u) in creature_template", cInfo->Entry, cInfo->UnitClass);
             // Mark NPC as having improper data by his expansion
@@ -896,7 +896,7 @@ void ObjectMgr::LoadCreatureClassLvlStats()
             continue;
         }
 
-        if (((1 << (creatureClass - 1)) & CLASSMASK_ALL_CREATURES) == 0)
+        if ((getBitmask(creatureClass) & CLASSMASK_ALL_CREATURES) == 0)
         {
             sLog.outErrorDb("Found stats for creature class [%u], incorrect class for this core. Skip!", creatureClass);
             continue;
@@ -1870,7 +1870,7 @@ void ObjectMgr::LoadCreatureModelInfo()
         if (!raceEntry)
             continue;
 
-        if (!((1 << (race - 1)) & RACEMASK_ALL_PLAYABLE))
+        if (!(getBitmask(race) & RACEMASK_ALL_PLAYABLE))
             continue;
 
         if (CreatureModelInfo const* minfo = GetCreatureModelInfo(raceEntry->model_f))
@@ -3662,14 +3662,14 @@ void ObjectMgr::LoadPlayerInfo()
             float  orientation   = fields[7].GetFloat();
 
             ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-            if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+            if (!rEntry || !(getBitmask(current_race) & RACEMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong race %u in `playercreateinfo` table, ignoring.", current_race);
                 continue;
             }
 
             ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-            if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+            if (!cEntry || !(getBitmask(current_class) & CLASSMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong class %u in `playercreateinfo` table, ignoring.", current_class);
                 continue;
@@ -3737,14 +3737,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(getBitmask(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_item` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !(getBitmask(current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_item` table, ignoring.", current_class);
                     continue;
@@ -3836,12 +3836,12 @@ void ObjectMgr::LoadPlayerInfo()
 
                 for (uint32 raceIndex = RACE_HUMAN; raceIndex < MAX_RACES; ++raceIndex)
                 {
-                    const uint32 raceIndexMask = (1 << (raceIndex - 1));
+                    const uint32 raceIndexMask = getBitmask(raceIndex);
                     if (!raceMask || (raceMask & raceIndexMask))
                     {
                         for (uint32 classIndex = CLASS_WARRIOR; classIndex < MAX_CLASSES; ++classIndex)
                         {
-                            const uint32 classIndexMask = (1 << (classIndex - 1));
+                            const uint32 classIndexMask = getBitmask(classIndex);
                             if (!classMask || (classMask & classIndexMask))
                             {
                                 bool obtainable = false;
@@ -3915,14 +3915,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(getBitmask(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_spell` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !(getBitmask(current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_spell` table, ignoring.", current_class);
                     continue;
@@ -3975,14 +3975,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(getBitmask(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_action` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !getBitmask((current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_action` table, ignoring.", current_class);
                     continue;
@@ -4132,14 +4132,14 @@ void ObjectMgr::LoadPlayerInfo()
             uint32 current_class = fields[1].GetUInt32();
 
             ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-            if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+            if (!rEntry || !(getBitmask(current_race) & RACEMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong race %u in `player_levelstats` table, ignoring.", current_race);
                 continue;
             }
 
             ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-            if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+            if (!cEntry || !(getBitmask(current_class) & CLASSMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong class %u in `player_levelstats` table, ignoring.", current_class);
                 continue;
@@ -4181,13 +4181,13 @@ void ObjectMgr::LoadPlayerInfo()
     for (int race = 1; race < MAX_RACES; ++race)
     {
         // skip nonexistent races
-        if (!((1 << (race - 1)) & RACEMASK_ALL_PLAYABLE) || !sChrRacesStore.LookupEntry(race))
+        if (!(getBitmask(race) & RACEMASK_ALL_PLAYABLE) || !sChrRacesStore.LookupEntry(race))
             continue;
 
         for (int class_ = 1; class_ < MAX_CLASSES; ++class_)
         {
             // skip nonexistent classes
-            if (!((1 << (class_ - 1)) & CLASSMASK_ALL_PLAYABLE) || !sChrClassesStore.LookupEntry(class_))
+            if (!(getBitmask(class_) & CLASSMASK_ALL_PLAYABLE) || !sChrClassesStore.LookupEntry(class_))
                 continue;
 
             PlayerInfo* pInfo = &playerInfo[race][class_];

src/game/Guilds/Guild.cpp
@@ -225,7 +225,7 @@ bool Guild::AddMember(ObjectGuid plGuid, uint32 plRank)
         newmember.accountId = fields[5].GetInt32();
 
         if (newmember.Level < 1 || newmember.Class < 1 ||
-                !((1 << (newmember.Class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                !(getBitmask(newmember.Class) & CLASSMASK_ALL_PLAYABLE))
         {
             sLog.outError("%s has a broken data in field `characters` table, cannot add him to guild.", plGuid.GetString().c_str());
             return false;
@@ -484,7 +484,7 @@ bool Guild::LoadMembersFromDB(QueryResult* guildMembersResult)
             // the zone through xy coords .. this is a bit redundant, but shouldn't be called often
             newmember.ZoneId = Player::GetZoneIdFromDB(newmember.guid);
         }
-        if (!((1 << (newmember.Class - 1)) & CLASSMASK_ALL_PLAYABLE)) // can be at broken `class` field
+        if (!(getBitmask(newmember.Class) & CLASSMASK_ALL_PLAYABLE)) // can be at broken `class` field
         {
             sLog.outError("%s has a broken data in field `characters`.`class`, deleting him from guild!", newmember.guid.GetString().c_str());
             CharacterDatabase.PExecute("DELETE FROM guild_member WHERE guid = '%u'", lowguid);

src/game/Server/DBCStores.cpp
@@ -298,13 +298,13 @@ void LoadDBCStores(const std::string& dataPath)
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sCharacterFacialHairStylesStore, dbcPath, "CharacterFacialHairStyles.dbc");
     for (uint32 i = 0; i < sCharacterFacialHairStylesStore.GetNumRows(); ++i)
         if (CharacterFacialHairStylesEntry const* entry = sCharacterFacialHairStylesStore.LookupEntry(i))
-            if (entry->RaceID && ((1 << (entry->RaceID - 1)) & RACEMASK_ALL_PLAYABLE) != 0) // ignore nonplayable races
+            if (entry->RaceID && (getBitmask(entry->RaceID) & RACEMASK_ALL_PLAYABLE) != 0) // ignore nonplayable races
                 sCharFacialHairMap.insert({ entry->RaceID | (entry->SexID << 8) | (entry->VariationID << 16), entry });
 
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sCharSectionsStore, dbcPath, "CharSections.dbc");
     for (uint32 i = 0; i < sCharSectionsStore.GetNumRows(); ++i)
         if (CharSectionsEntry const* entry = sCharSectionsStore.LookupEntry(i))
-            if (entry->Race && ((1 << (entry->Race - 1)) & RACEMASK_ALL_PLAYABLE) != 0) //ignore Nonplayable races
+            if (entry->Race && (getBitmask(entry->Race) & RACEMASK_ALL_PLAYABLE) != 0) //ignore Nonplayable races
                 sCharSectionMap.emplace(uint8(entry->BaseSection) | (uint8(entry->Gender) << 8) | (uint8(entry->Race) << 16), entry);
 
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sChrClassesStore,          dbcPath, "ChrClasses.dbc");

src/game/Spells/SpellStacking.cpp
@@ -145,7 +145,7 @@ bool SpellStacker::IsStackableAuraEffect(SpellEntry const* entry, SpellEntry con
     auto secondItr = m_spellGroupSpellData.find(entry2->Id);
     if (firstItr != m_spellGroupSpellData.end() && secondItr != m_spellGroupSpellData.end())
     {
-        int wellFedIndex = 1 << (((int)SpellGroupId::WELL_FED) - 1);
+        int wellFedIndex = getBitmask(static_cast<uint32>(SpellGroupId::WELL_FED));
         if ((firstItr->second.mask & wellFedIndex) != 0 && (secondItr->second.mask & wellFedIndex) == 0)
             return true;
     }

cmangos/mangos-wotlk

src/game/AI/BaseAI/UnitAI.cpp
@@ -737,7 +737,7 @@ CreatureList UnitAI::DoFindFriendlyEligibleDispel(SpellEntry const* spellInfo, b
         if (spellInfo->Effect[i] == SPELL_EFFECT_DISPEL)
             dispelMask |= GetDispellMask(DispelType(spellInfo->EffectMiscValue[i]));
         else if (spellInfo->Effect[i] == SPELL_EFFECT_DISPEL_MECHANIC)
-            mechanicMask |= (1 << (spellInfo->EffectMiscValue[i] - 1));
+            mechanicMask |= convertEnumToFlag(spellInfo->EffectMiscValue[i]);
     }            
     float maxRange = CalculateSpellRange(spellInfo);
     return DoFindFriendlyEligibleDispel(maxRange, dispelMask, mechanicMask, self);

src/game/Entities/Corpse.h
@@ -68,7 +68,7 @@ class Corpse : public WorldObject
         void SetOwnerGuid(ObjectGuid guid) override { SetGuidValue(CORPSE_FIELD_OWNER, guid); }
 
         uint8 getRace() const { return GetByteValue(CORPSE_FIELD_BYTES_1, 1); }
-        uint32 getRaceMask() const { return 1 << (getRace() - 1); }
+        uint32 getRaceMask() const { return convertEnumToFlag(getRace()); }
         uint8 getGender() const { return GetByteValue(CORPSE_FIELD_BYTES_1, 2); }
 
         // faction template id

src/game/Entities/Creature.cpp
@@ -2159,7 +2159,7 @@ bool Creature::IsImmuneToSpell(SpellEntry const* spellInfo, bool castOnSelf, uin
 
     if (!castOnSelf)
     {
-        if (GetCreatureInfo()->MechanicImmuneMask & (1 << (spellInfo->Mechanic - 1)))
+        if (GetCreatureInfo()->MechanicImmuneMask & convertEnumToFlag(spellInfo->Mechanic))
             return true;
 
         if (GetCreatureInfo()->SchoolImmuneMask & spellInfo->SchoolMask)
@@ -2179,7 +2179,7 @@ bool Creature::IsImmuneToDamage(SpellSchoolMask meleeSchoolMask)
 
 bool Creature::IsImmuneToSpellEffect(SpellEntry const* spellInfo, SpellEffectIndex index, bool castOnSelf) const
 {
-    if (!castOnSelf && GetCreatureInfo()->MechanicImmuneMask & (1 << (spellInfo->EffectMechanic[index] - 1)))
+    if (!castOnSelf && GetCreatureInfo()->MechanicImmuneMask & convertEnumToFlag(spellInfo->EffectMechanic[index]))
         return true;
 
     return Unit::IsImmuneToSpellEffect(spellInfo, index, castOnSelf);

src/game/Entities/Pet.cpp
@@ -1294,7 +1294,7 @@ bool Pet::HaveInDiet(ItemPrototype const* item) const
         return false;
 
     const uint32 diet = cFamily->petFoodMask;
-    const uint32 FoodMask = 1 << (item->FoodType - 1);
+    const uint32 FoodMask = convertEnumToFlag(item->FoodType);
     return (diet & FoodMask) != 0;
 }
 

src/game/Globals/ObjectMgr.cpp
@@ -648,7 +648,7 @@ void ObjectMgr::LoadCreatureTemplates()
             const_cast<CreatureInfo*>(cInfo)->Expansion = -1;
         }
 
-        if (!cInfo->UnitClass || (((1 << (cInfo->UnitClass - 1)) & CLASSMASK_ALL_CREATURES) == 0))
+        if (!cInfo->UnitClass || ((convertEnumToFlag(cInfo->UnitClass) & CLASSMASK_ALL_CREATURES) == 0))
         {
             ERROR_DB_STRICT_LOG("Creature (Entry: %u) does not have proper `UnitClass` (%u) in creature_template", cInfo->Entry, cInfo->UnitClass);
             // Mark NPC as having improper data by his expansion
@@ -959,7 +959,7 @@ void ObjectMgr::LoadCreatureClassLvlStats()
             continue;
         }
 
-        if (((1 << (creatureClass - 1)) & CLASSMASK_ALL_CREATURES) == 0)
+        if ((convertEnumToFlag(creatureClass) & CLASSMASK_ALL_CREATURES) == 0)
         {
             sLog.outErrorDb("Found stats for creature class [%u], incorrect class for this core. Skip!", creatureClass);
             continue;
@@ -1985,7 +1985,7 @@ void ObjectMgr::LoadCreatureModelInfo()
         if (!raceEntry)
             continue;
 
-        if (!((1 << (race - 1)) & RACEMASK_ALL_PLAYABLE))
+        if (!(convertEnumToFlag(race) & RACEMASK_ALL_PLAYABLE))
             continue;
 
         if (CreatureModelInfo const* minfo = GetCreatureModelInfo(raceEntry->model_f))
@@ -4067,14 +4067,14 @@ void ObjectMgr::LoadPlayerInfo()
             float  orientation   = fields[7].GetFloat();
 
             ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-            if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+            if (!rEntry || !(convertEnumToFlag(current_race) & RACEMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong race %u in `playercreateinfo` table, ignoring.", current_race);
                 continue;
             }
 
             ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-            if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+            if (!cEntry || !(convertEnumToFlag(current_class) & CLASSMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong class %u in `playercreateinfo` table, ignoring.", current_class);
                 continue;
@@ -4142,14 +4142,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(convertEnumToFlag(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_item` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !(convertEnumToFlag(current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_item` table, ignoring.", current_class);
                     continue;
@@ -4241,12 +4241,12 @@ void ObjectMgr::LoadPlayerInfo()
 
                 for (uint32 raceIndex = RACE_HUMAN; raceIndex < MAX_RACES; ++raceIndex)
                 {
-                    const uint32 raceIndexMask = (1 << (raceIndex - 1));
+                    const uint32 raceIndexMask = convertEnumToFlag(raceIndex);
                     if (!raceMask || (raceMask & raceIndexMask))
                     {
                         for (uint32 classIndex = CLASS_WARRIOR; classIndex < MAX_CLASSES; ++classIndex)
                         {
-                            const uint32 classIndexMask = (1 << (classIndex - 1));
+                            const uint32 classIndexMask = convertEnumToFlag(classIndex);
                             if (!classMask || (classMask & classIndexMask))
                             {
                                 bool obtainable = false;
@@ -4320,14 +4320,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(convertEnumToFlag(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_spell` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !(convertEnumToFlag(current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_spell` table, ignoring.", current_class);
                     continue;
@@ -4380,14 +4380,14 @@ void ObjectMgr::LoadPlayerInfo()
                 uint32 current_class = fields[1].GetUInt32();
 
                 ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-                if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+                if (!rEntry || !(convertEnumToFlag(current_race) & RACEMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong race %u in `playercreateinfo_action` table, ignoring.", current_race);
                     continue;
                 }
 
                 ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-                if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                if (!cEntry || !convertEnumToFlag((current_class) & CLASSMASK_ALL_PLAYABLE))
                 {
                     sLog.outErrorDb("Wrong class %u in `playercreateinfo_action` table, ignoring.", current_class);
                     continue;
@@ -4537,14 +4537,14 @@ void ObjectMgr::LoadPlayerInfo()
             uint32 current_class = fields[1].GetUInt32();
 
             ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(current_race);
-            if (!rEntry || !((1 << (current_race - 1)) & RACEMASK_ALL_PLAYABLE))
+            if (!rEntry || !(convertEnumToFlag(current_race) & RACEMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong race %u in `player_levelstats` table, ignoring.", current_race);
                 continue;
             }
 
             ChrClassesEntry const* cEntry = sChrClassesStore.LookupEntry(current_class);
-            if (!cEntry || !((1 << (current_class - 1)) & CLASSMASK_ALL_PLAYABLE))
+            if (!cEntry || !(convertEnumToFlag(current_class) & CLASSMASK_ALL_PLAYABLE))
             {
                 sLog.outErrorDb("Wrong class %u in `player_levelstats` table, ignoring.", current_class);
                 continue;
@@ -4586,13 +4586,13 @@ void ObjectMgr::LoadPlayerInfo()
     for (int race = 1; race < MAX_RACES; ++race)
     {
         // skip nonexistent races
-        if (!((1 << (race - 1)) & RACEMASK_ALL_PLAYABLE) || !sChrRacesStore.LookupEntry(race))
+        if (!(convertEnumToFlag(race) & RACEMASK_ALL_PLAYABLE) || !sChrRacesStore.LookupEntry(race))
             continue;
 
         for (int class_ = 1; class_ < MAX_CLASSES; ++class_)
         {
             // skip nonexistent classes
-            if (!((1 << (class_ - 1)) & CLASSMASK_ALL_PLAYABLE) || !sChrClassesStore.LookupEntry(class_))
+            if (!(convertEnumToFlag(class_) & CLASSMASK_ALL_PLAYABLE) || !sChrClassesStore.LookupEntry(class_))
                 continue;
 
             PlayerInfo* pInfo = &playerInfo[race][class_];

src/game/Guilds/Guild.cpp
@@ -218,7 +218,7 @@ bool Guild::AddMember(ObjectGuid plGuid, uint32 plRank)
         newmember.accountId = fields[5].GetInt32();
 
         if (newmember.Level < 1 || newmember.Class < 1 ||
-                !((1 << (newmember.Class - 1)) & CLASSMASK_ALL_PLAYABLE))
+                !(convertEnumToFlag(newmember.Class) & CLASSMASK_ALL_PLAYABLE))
         {
             sLog.outError("%s has a broken data in field `characters` table, cannot add him to guild.", plGuid.GetString().c_str());
             return false;
@@ -469,7 +469,7 @@ bool Guild::LoadMembersFromDB(QueryResult* guildMembersResult)
             // the zone through xy coords .. this is a bit redundant, but shouldn't be called often
             newmember.ZoneId = Player::GetZoneIdFromDB(newmember.guid);
         }
-        if (!((1 << (newmember.Class - 1)) & CLASSMASK_ALL_PLAYABLE)) // can be at broken `class` field
+        if (!(convertEnumToFlag(newmember.Class) & CLASSMASK_ALL_PLAYABLE)) // can be at broken `class` field
         {
             sLog.outError("%s has a broken data in field `characters`.`class`, deleting him from guild!", newmember.guid.GetString().c_str());
             CharacterDatabase.PExecute("DELETE FROM guild_member WHERE guid = '%u'", lowguid);

src/game/Server/DBCStores.cpp
@@ -445,13 +445,13 @@ void LoadDBCStores(const std::string& dataPath)
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sCharacterFacialHairStylesStore, dbcPath, "CharacterFacialHairStyles.dbc");
     for (uint32 i = 0; i < sCharacterFacialHairStylesStore.GetNumRows(); ++i)
         if (CharacterFacialHairStylesEntry const* entry = sCharacterFacialHairStylesStore.LookupEntry(i))
-            if (entry->RaceID && ((1 << (entry->RaceID - 1)) & RACEMASK_ALL_PLAYABLE) != 0) // ignore nonplayable races
+            if (entry->RaceID && (convertEnumToFlag(entry->RaceID) & RACEMASK_ALL_PLAYABLE) != 0) // ignore nonplayable races
                 sCharFacialHairMap.insert({ entry->RaceID | (entry->SexID << 8) | (entry->VariationID << 16), entry });
 
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sCharSectionsStore, dbcPath, "CharSections.dbc");
     for (uint32 i = 0; i < sCharSectionsStore.GetNumRows(); ++i)
         if (CharSectionsEntry const* entry = sCharSectionsStore.LookupEntry(i))
-            if (entry->Race && ((1 << (entry->Race - 1)) & RACEMASK_ALL_PLAYABLE) != 0) //ignore Nonplayable races
+            if (entry->Race && (convertEnumToFlag(entry->Race) & RACEMASK_ALL_PLAYABLE) != 0) //ignore Nonplayable races
                 sCharSectionMap.emplace(uint8(entry->BaseSection) | (uint8(entry->Gender) << 8) | (uint8(entry->Race) << 16), entry);
 
     LoadDBC(availableDbcLocales, bar, bad_dbc_files, sChrClassesStore,          dbcPath, "ChrClasses.dbc");

src/game/Spells/SpellStacking.cpp
@@ -146,7 +146,7 @@ bool SpellStacker::IsStackableAuraEffect(SpellEntry const* entry, SpellEntry con
     auto secondItr = m_spellGroupSpellData.find(entry2->Id);
     if (firstItr != m_spellGroupSpellData.end() && secondItr != m_spellGroupSpellData.end())
     {
-        int wellFedIndex = 1 << (((int)SpellGroupId::WELL_FED) - 1);
+        int wellFedIndex = convertEnumToFlag(static_cast<uint32>(SpellGroupId::WELL_FED));
         if ((firstItr->second.mask & wellFedIndex) != 0 && (secondItr->second.mask & wellFedIndex) == 0)
             return true;
     }